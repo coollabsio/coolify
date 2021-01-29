740bed06b474839ed72d0967507fc3addbb6d9bb
Andras Bacsai <andras.bacsai@gmail.com>
2021-01-29 22:18:04 +0100

    Fixed deployment progress

--- api/v1/dashboard/index.js
+++ api/v1/dashboard/index.js
@@ -6,20 +6,34 @@ module.exports = async function (fastify) {
     socketPath: fastify.config.DOCKER_ENGINE,
   });
   fastify.get("/", async (request, reply) => {
-    let configOnly = await Config.find()
-    // const latestDeployments = await Deployment.find().sort({ createdAt: '-1' })
-    // const progress = latestDeployments.map(r => {
-    //   return {
-    //     repoId: r.repoId,
-    //     progress: r.progress
-    //   }
-    // })
-  
-    const running = await (await dockerEngine.listServices()).filter(r => r.Spec.Labels.managedBy === 'coolify')
-    running.forEach(r => {
-      configOnly = configOnly.filter(c => c.publish.domain !== r.Spec.Labels.domain)
-    })
+    let configOnly = await Config.find().select('-__v -_id')
+    const latestDeployments = await Deployment.aggregate([
+      {
+        $sort:{ updatedAt: -1 }
+      },
+      {
+        $group:
+        {
+          _id: '$repoId',
+          createdAt: { $last: '$updatedAt' },
+          progress : { $first: '$progress' },
+        }
+      }
+    ])
 
+    let running = await (await dockerEngine.listServices()).filter(r => r.Spec.Labels.managedBy === 'coolify')
+    running = running.map(r => {
+      configOnly = configOnly.filter(c => { 
+        if (c.publish.domain !== r.Spec.Labels.domain) {
+          let status = latestDeployments.find(l => r.Spec.Labels.repoId == l._id)
+          if (status && status.progress) c.progress = status.progress
+          return c
+        }
+      })
+      let status = latestDeployments.find(l => r.Spec.Labels.repoId == l._id)
+      if (status && status.progress) r.progress = status.progress
+      return r
+    })
     return {
       running,
       configOnly
