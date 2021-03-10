40072de9f2a0941e98da61b59dc46a346689c0a3
Andras Bacsai <andras.bacsai@gmail.com>
2021-03-10 15:47:50 +0100

    lint

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -1,8 +1,11 @@
 <script>
-    import { fade } from "svelte/transition";
+  import { fade } from "svelte/transition";
   export let message = "Loading...";
 </script>
 
-<p in:fade={{duration: 500}} class="text-xl font-bold tracking-tighter text-center text-gray-700">
+<p
+  in:fade="{{ duration: 500 }}"
+  class="text-xl font-bold tracking-tighter text-center text-gray-700"
+>
   {message}
 </p>
