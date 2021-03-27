63835e8b20b1e7c1e6fa4e43f3dd03157d17249f
Andras Bacsai <andras.bacsai@gmail.com>
2021-03-27 16:14:04 +0100

    Ok

--- api/routes/v1/upgrade/index.js
+++ api/routes/v1/upgrade/index.js
@@ -4,9 +4,9 @@ const { saveServerLog } = require('../../../libs/logging')
 module.exports = async function (fastify) {
   fastify.get('/', async (request, reply) => {
     const upgradeP1 = await execShellAsync('bash ./upgrade.sh upgrade-p1')
-    await saveServerLog({ event: upgradeP1, type: 'UPGRADE-P1' })
+    await saveServerLog({ event: upgradeP1, type: 'UPGRADE-P-1' })
     reply.code(200).send('I\'m trying, okay?')
     const upgradeP2 = await execShellAsync('bash ./upgrade.sh upgrade-p2')
-    await saveServerLog({ event: upgradeP2, type: 'UPGRADE-P2' })
+    await saveServerLog({ event: upgradeP2, type: 'UPGRADE-P-2' })
   })
 }
