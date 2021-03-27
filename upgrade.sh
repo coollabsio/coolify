e13f4bf92956b83484ea0c4aad56a533b1342956
Andras Bacsai <andras.bacsai@gmail.com>
2021-03-27 15:58:37 +0100

    Fix

--- upgrade.sh
+++ upgrade.sh
@@ -4,56 +4,56 @@
 case "$1" in
     "upgrade-p1")
             echo '
-        ##############################
-        #### Pulling Git Updates #####
-        ##############################'
+##############################
+#### Pulling Git Updates #####
+##############################'
         GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull
 
         if [ $? -ne 0 ]; then
             echo '
-        ####################################
-        #### Ooops something not okay! #####
-        ####################################'
+####################################
+#### Ooops something not okay! #####
+####################################'
             exit 1
         fi
 
         echo '
-        ##############################
-        #### Building Base Image #####
-        ##############################'
+##############################
+#### Building Base Image #####
+##############################'
         docker build --label coolify-reserve=true -t coolify-base -f install/Dockerfile-base .
 
         if [ $? -ne 0 ]; then
             echo '
-        ####################################
-        #### Ooops something not okay! #####
-        ####################################'
+####################################
+#### Ooops something not okay! #####
+####################################'
             exit 1
         fi
 
         echo '
-        ##################################
-        #### Checking configuration. #####
-        ##################################'
+##################################
+#### Checking configuration. #####
+##################################'
         docker run --rm -w /usr/src/app coolify-base node install/install.js --check
         if [ $? -ne 0 ]; then
         echo '
-        #################################
-        #### Missing configuration! #####
-        #################################'
+#################################
+#### Missing configuration! #####
+#################################'
             exit 1
         fi
         echo '
-        ################################
-        #### Upgrading Coolify P1. #####
-        ################################'
+################################
+#### Upgrading Coolify P1. #####
+################################'
         docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/upgrade.js --type upgrade-p1
     ;;
     "upgrade-p2")
         echo '
-        ################################
-        #### Upgrading Coolify P2. #####
-        ################################'
+################################
+#### Upgrading Coolify P2. #####
+################################'
         docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/upgrade.js --type upgrade-p2
     ;;
     *)
