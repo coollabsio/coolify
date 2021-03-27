2d5667840868354e976f44c11d931f9c656775e0
Andras Bacsai <andras.bacsai@gmail.com>
2021-03-27 14:19:10 +0100

    Test new upgrade process

--- api/routes/v1/upgrade/index.js
+++ api/routes/v1/upgrade/index.js
@@ -3,10 +3,8 @@ const { saveServerLog } = require('../../../libs/logging')
 
 module.exports = async function (fastify) {
   fastify.get('/', async (request, reply) => {
-    const upgradeP1 = await execShellAsync('bash ./install.sh upgrade-phase-1')
-    await saveServerLog({ event: upgradeP1, type: 'UPGRADE-P-1' })
     reply.code(200).send('I\'m trying, okay?')
-    const upgradeP2 = await execShellAsync('bash ./install.sh upgrade-phase-2')
-    await saveServerLog({ event: upgradeP2, type: 'UPGRADE-P-2' })
+    const upgradeP1 = await execShellAsync('bash ./install.sh upgrade')
+    await saveServerLog({ event: upgradeP1, type: 'UPGRADE' })
   })
 }
