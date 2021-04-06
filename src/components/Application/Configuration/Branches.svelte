046cea54ee15d99839cd35b4521eba4a6435a2cd
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-06 22:13:12 +0200

    lol

--- src/components/Application/Configuration/Branches.svelte
+++ src/components/Application/Configuration/Branches.svelte
@@ -1,24 +1,42 @@
 <script>
   export let loading, branches;
   import { application } from "@store";
+  import Select from "svelte-select";
+  let selectedValue;
+
+  function handleSelect(event) {
+    $application.repository.branch = null;
+    setTimeout(() => {
+      $application.repository.branch = event.detail.value;
+    }, 1);
+  }
 </script>
 
 {#if loading}
   <div class="grid grid-cols-1">
     <label for="branch">Branch</label>
-    <select disabled>
-      <option selected>Loading branches</option>
-    </select>
+    <div class="repository-select-search col-span-2">
+      <Select
+        containerClasses="w-full border-none bg-transparent"
+        placeholder="Loading branches..."
+        isDisabled
+      />
+    </div>
   </div>
 {:else}
   <div class="grid grid-cols-1">
     <label for="branch">Branch</label>
-    <!-- svelte-ignore a11y-no-onchange -->
-    <select id="branch" bind:value="{$application.repository.branch}">
-      <option disabled selected>Select a branch</option>
-      {#each branches as branch}
-        <option value="{branch.name}" class="font-bold">{branch.name}</option>
-      {/each}
-    </select>
+    <div class="repository-select-search col-span-2">
+      <Select
+        containerClasses="w-full border-none bg-transparent"
+        on:select="{handleSelect}"
+        selectedValue="{selectedValue}"
+        isClearable="{false}"
+        items="{branches.map(b => ({ label: b.name, value: b.name }))}"
+        showIndicator
+        noOptionsMessage="No branches found"
+        placeholder="Select a branch"
+      />
+    </div>
   </div>
 {/if}
