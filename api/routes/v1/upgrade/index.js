7c9d0b9e1f61ab09f9c5cf3445078151c159f1d6
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 23:06:19 +0200

    asd

--- api/routes/v1/upgrade/index.js
+++ api/routes/v1/upgrade/index.js
@@ -6,7 +6,7 @@ module.exports = async function (fastify) {
     const upgradeP1 = await execShellAsync('bash -c "$(curl -fsSL https://get.coollabs.io/coolify/upgrade-p1.sh)"')
     await saveServerLog({ event: upgradeP1, type: 'UPGRADE-P-1' })
     reply.code(200).send('I\'m trying, okay?')
-    const upgradeP2 = await execShellAsync('bash -c "$(curl -fsSL https://get.coollabs.io/coolify/upgrade-p2.sh)"')
+    const upgradeP2 = await execShellAsync('bash -c "$(curl -fsSL https://get.coollabs.io/coolify/upgrade-p2.sh)" >/dev/null 2>&1 &')
     await saveServerLog({ event: upgradeP2, type: 'UPGRADE-P-2' })
   })
 }
