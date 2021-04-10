d47d3d1069d9be83d95f466c8cc3c8c1c3956878
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 18:02:47 +0200

    wtf

--- scripts/upgrade-p1.sh
+++ scripts/upgrade-p1.sh
@@ -4,10 +4,10 @@ if [ $WHO != 'root' ]; then
     exit
 fi
 
-docker build --label coolify-reserve=true -t coolify-base -f scripts/Dockerfile-base .
-docker run --rm -w /usr/src/app coolify-base node scripts/check.js
+docker build --label coolify-reserve=true -t coolify-base -f /usr/src/app/scripts/Dockerfile-base .
+docker run --rm -w /usr/src/app coolify-base node /usr/src/app/scripts/check.js
 
 set -a && source .env && set +a
 
 docker network create $DOCKER_NETWORK --driver overlay
-docker build -t coolify -f scripts/Dockerfile .
+docker build -t coolify -f /usr/src/app/scripts/Dockerfile .
