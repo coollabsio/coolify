505a97710377e7a128784f5d53ba4451cfc68cb2
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 14:20:30 +0200

    upgrade p1

--- scripts/upgrade-p1.sh
+++ scripts/upgrade-p1.sh
@@ -3,6 +3,10 @@ if [ $WHO != 'root' ]; then
     echo 'You are not root. Ooops!'
     exit
 fi
+
+docker build --label coolify-reserve=true -t coolify-base -f scripts/Dockerfile-base .
+docker run --rm -w /usr/src/app coolify-base node scripts/check.js
+
 export $(egrep -v '^#' .env | grep -v 'GITHUB_APP_PRIVATE_KEY'| xargs)
 
 docker network create $DOCKER_NETWORK --driver overlay
