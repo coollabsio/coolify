0882ccd9cd77aaadb1b0f5faefb78e3bd7e157fc
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 18:38:37 +0200

    asd

--- install.sh
+++ install.sh
@@ -57,16 +57,16 @@ case "$1" in
 #### Upgrading Coolify P1. #####
 ################################'
         # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base bash -x /usr/src/app/scripts/upgrade-p1.sh
-        preTasks
-        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/install.js --type upgrade
+        # preTasks
+        # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/install.js --type upgrade
     ;;
     "upgrade-phase-2")
         echo '
 ################################
 #### Upgrading Coolify P2. #####
 ################################'
-        # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base bash -x /usr/src/app/scripts/upgrade-p2.sh
-        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/update.js --type upgrade
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base bash -x /usr/src/app/scripts/upgrade-p2.sh
+        # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/update.js --type upgrade
     ;;
     *)
         exit 1
