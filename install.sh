d0af5b5af15a8bfb5c0eda4861e2ed6669d77544
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 14:55:17 +0200

    test upgrade

--- install.sh
+++ install.sh
@@ -56,17 +56,17 @@ case "$1" in
 ################################
 #### Upgrading Coolify P1. #####
 ################################'
-        #bash -x scripts/upgrade-p1.sh
-        preTasks
-        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/install.js --type upgrade
+        bash -x scripts/upgrade-p1.sh
+        # preTasks
+        # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/install.js --type upgrade
     ;;
     "upgrade-phase-2")
         echo '
 ################################
 #### Upgrading Coolify P2. #####
 ################################'
-        #bash -x scripts/upgrade-p2.sh
-        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/update.js --type upgrade
+        bash -x scripts/upgrade-p2.sh
+        # docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify:/data/coolify -u root -w /usr/src/app coolify-base node install/update.js --type upgrade
     ;;
     *)
         exit 1
