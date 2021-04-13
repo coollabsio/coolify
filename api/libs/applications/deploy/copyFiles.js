7295ee87061c45bf99cd2c1a247512ad68fba9a7
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-13 10:46:32 +0200

    asd

--- api/libs/applications/deploy/copyFiles.js
+++ api/libs/applications/deploy/copyFiles.js
@@ -2,58 +2,62 @@ const fs = require('fs').promises
 module.exports = async function (configuration) {
   try {
     // TODO: Write full .dockerignore for all deployments!!
-    await fs.writeFile(`${configuration.general.workdir}/.htaccess`, `
-    RewriteEngine On
-    RewriteBase /
-    RewriteCond %{REQUEST_FILENAME} !-d
-    RewriteCond %{REQUEST_FILENAME} !-f
-    RewriteRule ^(.+)$ index.php [QSA,L]
-    `)
+    if (configuration.build.pack === 'php') {
+      await fs.writeFile(`${configuration.general.workdir}/.htaccess`, `
+      RewriteEngine on
+      RewriteCond %{REQUEST_FILENAME} !-f
+      RewriteCond %{REQUEST_FILENAME} !-d
+      RewriteCond $1 !^(index.php|public|css|js|robots.txt|test) 
+      RewriteRule ^(.*)$ index.php/$1 [L]
+      `)
+    }
     await fs.writeFile(`${configuration.general.workdir}/.dockerignore`, 'node_modules')
-    await fs.writeFile(
-      `${configuration.general.workdir}/nginx.conf`,
-      `user  nginx;
-        worker_processes  auto;
-        
-        error_log  /var/log/nginx/error.log warn;
-        pid        /var/run/nginx.pid;
-        
-        events {
-            worker_connections  1024;
-        }
-        
-        http {
-            include       /etc/nginx/mime.types;
-        
-            access_log      off;
-            sendfile        on;
-            #tcp_nopush     on;
-            keepalive_timeout  65;
-
-            server {
-                listen       80;
-                server_name  localhost;
-                
-                location / {
-                    root   /usr/share/nginx/html;
-                    index  index.html;
-                    try_files $uri $uri/index.html $uri/ /index.html =404;
-                }
-        
-                error_page  404              /50x.html;
-        
-                # redirect server error pages to the static page /50x.html
-                #
-                error_page   500 502 503 504  /50x.html;
-                location = /50x.html {
-                    root   /usr/share/nginx/html;
-                }  
-        
-            }
-        
-        }
-        `
-    )
+    if (configuration.build.pack === 'static') {
+      await fs.writeFile(
+        `${configuration.general.workdir}/nginx.conf`,
+        `user  nginx;
+          worker_processes  auto;
+          
+          error_log  /var/log/nginx/error.log warn;
+          pid        /var/run/nginx.pid;
+          
+          events {
+              worker_connections  1024;
+          }
+          
+          http {
+              include       /etc/nginx/mime.types;
+          
+              access_log      off;
+              sendfile        on;
+              #tcp_nopush     on;
+              keepalive_timeout  65;
+  
+              server {
+                  listen       80;
+                  server_name  localhost;
+                  
+                  location / {
+                      root   /usr/share/nginx/html;
+                      index  index.html;
+                      try_files $uri $uri/index.html $uri/ /index.html =404;
+                  }
+          
+                  error_page  404              /50x.html;
+          
+                  # redirect server error pages to the static page /50x.html
+                  #
+                  error_page   500 502 503 504  /50x.html;
+                  location = /50x.html {
+                      root   /usr/share/nginx/html;
+                  }  
+          
+              }
+          
+          }
+          `
+      )
+    }
   } catch (error) {
     throw { error, type: 'server' }
   }
