778099c13c1a72bc7e8ac5a8fedebb7a190ca950
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-14 23:28:30 +0200

    okay

--- api/routes/v1/upgrade/index.js
+++ api/routes/v1/upgrade/index.js
@@ -6,7 +6,7 @@ module.exports = async function (fastify) {
     const upgradeP1 = await execShellAsync('bash -c "$(curl -fsSL https://get.coollabs.io/coolify/upgrade-p1.sh)"')
     await saveServerLog({ event: upgradeP1, type: 'UPGRADE-P-1' })
     reply.code(200).send('I\'m trying, okay?')
-    const upgradeP2 = await execShellAsync('docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -u root coolify-base bash -c "$(curl -fsSL https://get.coollabs.io/coolify/upgrade-p2.sh)"')
+    const upgradeP2 = await execShellAsync('docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -u root coolify bash -c "$(curl -fsSL https://get.coollabs.io/coolify/upgrade-p2.sh)"')
     await saveServerLog({ event: upgradeP2, type: 'UPGRADE-P-2' })
   })
 }
