406b170991d271a3b7c43586767e8bcecf27b53e
Andras Bacsai <andras.bacsai@gmail.com>
2021-05-09 23:51:26 +0200

    okay

--- src/routes/service/[name]/configuration.svelte
+++ src/routes/service/[name]/configuration.svelte
@@ -38,22 +38,39 @@
 {:then}
 	<div class="min-h-full text-white">
 		<div class="py-5 text-left px-6 text-3xl tracking-tight font-bold flex items-center">
+			<div>{$page.params.name === 'plausible' ? 'Plausible Analytics' : $page.params.name}</div>
+			<div class="px-4">
+				{#if $page.params.name === 'plausible'}
+					<img
+						alt="plausible logo"
+						class="w-6 mx-auto"
+						src="https://cdn.coollabs.io/assets/coolify/services/plausible/logo_sm.png"
+					/>
+				{/if}
+			</div>
 			<a
-				href={service.config.baseURL}
-				target="_blank"
-				class="inline-flex hover:underline cursor-pointer px-2"
+			target="_blank"
+			class="icon mx-2"
+			href={service.config.baseURL}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
 			>
-				<div>{$page.params.name === 'plausible' ? 'Plausible Analytics' : $page.params.name}</div>
-				<div class="px-4">
-					{#if $page.params.name === 'plausible'}
-						<img
-							alt="plausible logo"
-							class="w-6 mx-auto"
-							src="https://cdn.coollabs.io/assets/coolify/services/plausible/logo_sm.png"
-						/>
-					{/if}
-				</div>
-			</a>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+				/>
+			</svg></a
+		>
+
+
+
 		</div>
 	</div>
 	<div class="space-y-2 max-w-4xl mx-auto px-6" in:fade={{ duration: 100 }}>
