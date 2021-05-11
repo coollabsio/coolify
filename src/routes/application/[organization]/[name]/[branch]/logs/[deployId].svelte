5abfd4070601f9aad4e6d3d2f23d458d9976eb34
Andras Bacsai <andras.bacsai@gmail.com>
2021-05-11 11:38:40 +0200

    show link on deployment logs

--- src/routes/application/[organization]/[name]/[branch]/logs/[deployId].svelte
+++ src/routes/application/[organization]/[name]/[branch]/logs/[deployId].svelte
@@ -5,7 +5,8 @@
 	import { request } from '$lib/api/request';
 	import { page, session } from '$app/stores';
 	import { goto } from '$app/navigation';
-import { browser } from '$app/env';
+	import { browser } from '$app/env';
+	import { application } from '$store';
 
 	let loadLogsInterval;
 	let logs = [];
@@ -33,6 +34,7 @@ import { browser } from '$app/env';
 	onDestroy(() => {
 		clearInterval(loadLogsInterval);
 	});
+
 </script>
 
 <div
@@ -40,6 +42,26 @@ import { browser } from '$app/env';
 	in:fade={{ duration: 100 }}
 >
 	<div>Deployment log</div>
+	<a
+		target="_blank"
+		class="icon mx-2"
+		href={'https://' + $application.publish.domain + $application.publish.path}
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+			/>
+		</svg></a
+	>
 </div>
 {#await loadLogs()}
 	<Loading />
