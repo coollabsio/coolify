881a4fd2098d06d2ceb095c52aabc2e4b12dc6f1
Andras Bacsai <andras.bacsai@gmail.com>
2021-07-13 14:24:30 +0200

    Do not show build logs in case of build envs set?!

--- src/lib/api/applications/packs/helpers.ts
+++ src/lib/api/applications/packs/helpers.ts
@@ -1,11 +1,7 @@
 import { docker, streamEvents } from '$lib/api/docker';
 import { promises as fs } from 'fs';
 
-const buildImageNodeDocker = (configuration, prodBuild) => {
-	const generateEnvs = [];
-	for (const secret of configuration.publish.secrets) {
-		if (secret.isBuild) generateEnvs.push(`ENV ${secret.name}=${secret.value}`)
-	}
+const buildImageNodeDocker = (configuration, prodBuild, generateEnvs) => {
 	return [
 		'FROM node:lts',
 		...generateEnvs,
@@ -20,9 +16,13 @@ const buildImageNodeDocker = (configuration, prodBuild) => {
 export async function buildImage(configuration, cacheBuild?: boolean, prodBuild?: boolean) {
 	// TODO: Edit secrets
 	// TODO: Add secret from .env file / json
+	const generateEnvs = [];
+	for (const secret of configuration.publish.secrets) {
+		if (secret.isBuild) generateEnvs.push(`ENV ${secret.name}=${secret.value}`)
+	}
 	await fs.writeFile(
 		`${configuration.general.workdir}/Dockerfile`,
-		buildImageNodeDocker(configuration, prodBuild)
+		buildImageNodeDocker(configuration, prodBuild, generateEnvs)
 	);
 	const stream = await docker.engine.buildImage(
 		{ src: ['.'], context: configuration.general.workdir },
@@ -33,5 +33,5 @@ export async function buildImage(configuration, cacheBuild?: boolean, prodBuild?
 				}`
 		}
 	);
-	await streamEvents(stream, configuration);
+	if (generateEnvs.length === 0) await streamEvents(stream, configuration);
 }
