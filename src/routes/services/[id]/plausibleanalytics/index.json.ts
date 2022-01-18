a3a51a47251d32ccff842ede24531a47ff5adfe0
Andras Bacsai <andras.bacsai@gmail.com>
2022-01-19 00:41:45 +0100

    Always restart coolify

--- src/routes/services/[id]/plausibleanalytics/index.json.ts
+++ src/routes/services/[id]/plausibleanalytics/index.json.ts
@@ -1,10 +1,9 @@
-import { asyncExecShell, getEngine, getUserDetails } from '$lib/common';
+import {  getUserDetails } from '$lib/common';
 import * as db from '$lib/database';
-import { generateDatabaseConfiguration, getVersions } from '$lib/database';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const post: RequestHandler<Locals, FormData> = async (request) => {
-    const { teamId, status, body } = await getUserDetails(request);
+    const { status, body } = await getUserDetails(request);
     if (status === 401) return { status, body }
     const { id } = request.params
 
