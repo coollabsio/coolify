60e3868530d4db7082b36fd5b5c01ce98e4859ae
Andras Bacsai <andras.bacsai@gmail.com>
2022-02-01 22:23:56 +0100

    fix: See database type

--- src/routes/databases/[id]/index.svelte
+++ src/routes/databases/[id]/index.svelte
@@ -31,6 +31,13 @@
 </script>
 
 <script lang="ts">
+	import Clickhouse from '$lib/components/svg/databases/Clickhouse.svelte';
+	import CouchDb from '$lib/components/svg/databases/CouchDB.svelte';
+	import MongoDb from '$lib/components/svg/databases/MongoDB.svelte';
+	import MySql from '$lib/components/svg/databases/MySQL.svelte';
+	import PostgreSql from '$lib/components/svg/databases/PostgreSQL.svelte';
+	import Redis from '$lib/components/svg/databases/Redis.svelte';
+
 	export let database;
 	export let settings;
 	export let privatePort;
@@ -40,8 +47,22 @@
 	<div class="md:max-w-64 hidden truncate tracking-tight md:block">
 		{database.name}
 	</div>
-	<span class="arrow-right-applications hidden px-1 md:block">></span>
-	<span class="pr-2">{database.type}</span>
+	<span class="arrow-right-applications hidden px-1 pr-5 md:block">></span>
+	<span class="relative">
+		{#if database.type === 'clickhouse'}
+			<Clickhouse />
+		{:else if database.type === 'couchdb'}
+			<CouchDb />
+		{:else if database.type === 'mongodb'}
+			<MongoDb />
+		{:else if database.type === 'mysql'}
+			<MySql />
+		{:else if database.type === 'postgresql'}
+			<PostgreSql />
+		{:else if database.type === 'redis'}
+			<Redis />
+		{/if}
+	</span>
 </div>
 
 <Databases bind:database {privatePort} {settings} />
