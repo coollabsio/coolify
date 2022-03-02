892d8cd5c1fe529fa8af02dddb42f748016dd28a
Andras Bacsai <andras.bacsai@gmail.com>
2022-03-02 21:04:42 +0100

    Reload proxy after ssl renewal

--- src/lib/queues/sslrenewal.ts
+++ src/lib/queues/sslrenewal.ts
@@ -1,10 +1,12 @@
 import { asyncExecShell } from '$lib/common';
+import { reloadHaproxy } from '$lib/haproxy';
 
 export default async function () {
 	try {
-		return await asyncExecShell(
+		await asyncExecShell(
 			`docker run --rm --name certbot-renewal -v "coolify-letsencrypt:/etc/letsencrypt" certbot/certbot --logs-dir /etc/letsencrypt/logs renew`
 		);
+		await reloadHaproxy('unix:///var/run/docker.sock');
 	} catch (error) {
 		throw error;
 	}
