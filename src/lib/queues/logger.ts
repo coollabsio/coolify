456b1b8074f23cb6a6480e3c34195331ebe3dbb2
dominicbachmann <dominic.bachmann4@gmail.com>
2022-04-06 21:04:14 +0200

    Added types for queues/logger

--- src/lib/queues/logger.ts
+++ src/lib/queues/logger.ts
@@ -1,7 +1,8 @@
 import { prisma } from '$lib/database';
 import { dev } from '$app/env';
+import type { Job } from 'bullmq';
 
-export default async function (job) {
+export default async function (job: Job): Promise<void> {
 	const { line, applicationId, buildId } = job.data;
 	if (dev) console.debug(`[${applicationId}] ${line}`);
 	await prisma.buildLog.create({ data: { line, buildId, time: Number(job.id), applicationId } });
