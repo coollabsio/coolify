3449e0f8fc02ced1ce13f8b911f04d4a92bc2b1d
Andras Bacsai <andras.bacsai@gmail.com>
2022-04-08 14:16:14 +0200

    ui: fix

--- src/routes/sources/index.svelte
+++ src/routes/sources/index.svelte
@@ -61,7 +61,7 @@
 		</button>
 	{/if}
 </div>
-<div class="flex justify-center">
+<div class="flex flex-wrap justify-center">
 	{#if !sources || ownSources.length === 0}
 		<div class="flex-col">
 			<div class="text-center text-xl font-bold">No git sources found</div>
