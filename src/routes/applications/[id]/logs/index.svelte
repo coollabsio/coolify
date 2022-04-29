dc4a63ef929b9d047a16cfb7405bdda7383c8efa
Andras Bacsai <andras.bacsai@gmail.com>
2022-04-29 23:36:10 +0200

    fix: UI

--- src/routes/applications/[id]/logs/index.svelte
+++ src/routes/applications/[id]/logs/index.svelte
@@ -98,7 +98,7 @@
 	}
 </script>
 
-<div class="flex items-center space-x-2 p-6 text-2xl font-bold">
+<div class="flex h-20 items-center space-x-2 p-5 px-6 font-bold">
 	<div class="-mb-5 flex-col">
 		<div class="md:max-w-64 truncate text-base tracking-tight md:text-2xl lg:block">
 			Application Logs
