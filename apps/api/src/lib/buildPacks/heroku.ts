3dd2a059bf8b182b65645d1b66a8098473234721
Andras Bacsai <andras.bacsai@protonmail.com>
2022-08-15 11:23:21 +0000

    fix: gitpod conf and heroku buildpacks

--- apps/api/src/lib/buildPacks/heroku.ts
+++ apps/api/src/lib/buildPacks/heroku.ts
@@ -2,14 +2,14 @@ import { executeDockerCmd, prisma } from "../common"
 import { saveBuildLog } from "./common";
 
 export default async function (data: any): Promise<void> {
+    const { buildId, applicationId, tag, dockerId, debug, workdir } = data
     try {
-        const { buildId, applicationId, tag, dockerId, debug, workdir } = data
+
         await saveBuildLog({ line: `Building image started.`, buildId, applicationId });
         const { stdout } = await executeDockerCmd({
             dockerId,
             command: `pack build -p ${workdir} ${applicationId}:${tag} --builder heroku/buildpacks:20`
         })
-
         if (debug) {
             const array = stdout.split('\n')
             for (const line of array) {
@@ -24,6 +24,16 @@ export default async function (data: any): Promise<void> {
         }
         await saveBuildLog({ line: `Building image successful.`, buildId, applicationId });
     } catch (error) {
+        const array = error.stdout.split('\n')
+        for (const line of array) {
+            if (line !== '\n') {
+                await saveBuildLog({
+                    line: `${line.replace('\n', '')}`,
+                    buildId,
+                    applicationId
+                });
+            }
+        }
         throw error;
     }
 }
