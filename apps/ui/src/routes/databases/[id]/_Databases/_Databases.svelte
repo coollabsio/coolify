03bf93eb12e3f756d862f70956755241bde84feb
Andras Bacsai <andras.bacsai@protonmail.com>
2022-08-15 09:17:31 +0000

    fix: loading indicator

--- apps/ui/src/routes/databases/[id]/_Databases/_Databases.svelte
+++ apps/ui/src/routes/databases/[id]/_Databases/_Databases.svelte
@@ -247,6 +247,7 @@
 		{#if database.type === 'redis'}
 			<div class="grid grid-cols-2 items-center">
 				<Setting
+					loading={publicLoading}
 					bind:setting={appendOnly}
 					on:click={() => changeSettings('appendOnly')}
 					title={$t('database.change_append_only_mode')}
