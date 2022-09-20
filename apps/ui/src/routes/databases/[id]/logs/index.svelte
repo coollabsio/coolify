034e86e2cb82a8dcb990029a736555fa3ca7a0b2
Andras Bacsai <andras.bacsai@gmail.com>
2022-09-20 13:07:49 +0200

    ui: small logs on mobile

--- apps/ui/src/routes/databases/[id]/logs/index.svelte
+++ apps/ui/src/routes/databases/[id]/logs/index.svelte
@@ -126,22 +126,11 @@
 				</button>
 				<Tooltip triggeredBy="#follow">Follow Logs</Tooltip>
 			</div>
-			<div class="font-mono w-full rounder bg-coolgray-200 p-5 overflow-x-auto overflox-y-auto max-h-[80vh] rounded-md mb-20 flex flex-col whitespace-nowrap -mt-12 scrollbar-thumb-coollabs scrollbar-track-coolgray-200 scrollbar-w-1">
+			<div class="font-mono w-full rounder bg-coolgray-200 p-5 overflow-x-auto overflox-y-auto max-h-[80vh] rounded-md mb-20 flex flex-col whitespace-nowrap -mt-12 scrollbar-thumb-coollabs scrollbar-track-coolgray-200 scrollbar-w-1 lg:text-base text-[10px]">
 				{#each logs as log}
 					<p>{log + '\n'}</p>
 				{/each}
 			</div>
-			<!-- <div
-				class="font-mono w-full leading-6 text-left text-md tracking-tighter rounded bg-coolgray-200 py-5 px-6 whitespace-pre-wrap break-words overflow-auto max-h-[80vh] -mt-12 overflow-y-scroll scrollbar-w-1 scrollbar-thumb-coollabs scrollbar-track-coolgray-200"
-				bind:this={logsEl}
-				on:scroll={detect}
-			>
-				<div class="px-2 pr-14">
-					{#each logs as log}
-						{log + '\n'}
-					{/each}
-				</div>
-			</div> -->
 		</div>
 	{/if}
 </div>
