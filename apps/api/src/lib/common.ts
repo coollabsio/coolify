4cd7af7a748c3edda8937cd3c7e97cc72a8640c6
Andras Bacsai <andras.bacsai@protonmail.com>
2022-09-25 07:58:52 +0000

    fix: stream logs for heroku bp

--- apps/api/src/lib/common.ts
+++ apps/api/src/lib/common.ts
@@ -585,7 +585,7 @@ export async function executeDockerCmd({ debug, buildId, applicationId, dockerId
 			command = command.replace(/docker compose/gi, 'docker-compose');
 		}
 	}
-	if (command.startsWith(`docker build --progress plain`)) {
+	if (command.startsWith(`docker build --progress plain`) || command.startsWith(`pack build`)) {
 		return await asyncExecShellStream({ debug, buildId, applicationId, command, engine });
 	}
 	return await execaCommand(command, { env: { DOCKER_BUILDKIT: "1", DOCKER_HOST: engine }, shell: true })
