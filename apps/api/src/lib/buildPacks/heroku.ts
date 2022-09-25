17f82109b661f1ef6b6920969a34fd5079f94a3c
Andras Bacsai <andras.bacsai@protonmail.com>
2022-09-25 07:47:37 +0000

    fix: consider base directory in heroku bp

--- apps/api/src/lib/buildPacks/heroku.ts
+++ apps/api/src/lib/buildPacks/heroku.ts
@@ -2,13 +2,12 @@ import { executeDockerCmd, prisma } from "../common"
 import { saveBuildLog } from "./common";
 
 export default async function (data: any): Promise<void> {
-    const { buildId, applicationId, tag, dockerId, debug, workdir } = data
+    const { buildId, applicationId, tag, dockerId, debug, workdir, baseDirectory } = data
     try {
-
         await saveBuildLog({ line: `Building image started.`, buildId, applicationId });
         const { stdout } = await executeDockerCmd({
             dockerId,
-            command: `pack build -p ${workdir} ${applicationId}:${tag} --builder heroku/buildpacks:20`
+            command: `pack build -p ${workdir}${baseDirectory} ${applicationId}:${tag} --builder heroku/buildpacks:20`
         })
         if (debug) {
             const array = stdout.split('\n')
