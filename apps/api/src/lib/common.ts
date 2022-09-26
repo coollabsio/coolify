9d15d2be77c28c86f767c9f4846ec066c165519a
Andras Bacsai <andras.bacsai@protonmail.com>
2022-09-26 20:22:08 +0000

    chore: version++

--- apps/api/src/lib/common.ts
+++ apps/api/src/lib/common.ts
@@ -21,7 +21,7 @@ import { scheduler } from './scheduler';
 import { supportedServiceTypesAndVersions } from './services/supportedVersions';
 import { includeServices } from './services/common';
 
-export const version = '3.10.8';
+export const version = '3.10.9';
 export const isDev = process.env.NODE_ENV === 'development';
 
 const algorithm = 'aes-256-ctr';
