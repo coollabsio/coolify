02c42a7e3a4260aded73e7706833f995607780da
Andras Bacsai <andras.bacsai@protonmail.com>
2022-10-05 09:01:17 +0000

    fix: pure docker based development

--- apps/api/src/lib/common.ts
+++ apps/api/src/lib/common.ts
@@ -20,7 +20,7 @@ import { scheduler } from './scheduler';
 import { supportedServiceTypesAndVersions } from './services/supportedVersions';
 import { includeServices } from './services/common';
 
-export const version = '3.10.14';
+export const version = '3.10.15';
 export const isDev = process.env.NODE_ENV === 'development';
 
 const algorithm = 'aes-256-ctr';
