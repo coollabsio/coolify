cca58ece03d278016b0544c6a55a174932bbd972
Gabriel Engel <gabrielfengel@gmail.com>
2022-11-13 14:01:26 -0300

    Status badge autorefreshing every 5 seconds

---
+++ apps/ui/src/lib/components/buttons/OpenButton.svelte
@@ -0,0 +1,20 @@
+<script type="ts">
+  export let location:string;
+</script>
+<a href={location} target="_blank noreferrer" class="btn btn-sm gap-2 text-sm bg-primary"
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+    <line x1="10" y1="14" x2="20" y2="4" />
+    <polyline points="15 4 20 4 20 9" />
+  </svg>Open</a
+>
--- apps/ui/src/routes/applications/[id]/__layout.svelte
+++ apps/ui/src/routes/applications/[id]/__layout.svelte
@@ -73,6 +73,8 @@
 	import Tooltip from '$lib/components/Tooltip.svelte';
 	import Menu from './_Menu.svelte';
 	import StopApplicationButton from '$lib/components/buttons/StopApplicationButton.svelte';
+	import OpenButton from '$lib/components/buttons/OpenButton.svelte';
+	import StatusBadge from '$lib/components/badges/StatusBadge.svelte';
 
 	let statusInterval: any;
 	let forceDelete = false;
@@ -229,6 +231,7 @@
 		{:else}
 			<div class="flex justify-center items-center space-x-2">
 				<div>Configurations</div>
+				<!-- <StatusBadge {id} /> -->
 				<div
 					class="badge badge-lg rounded uppercase"
 					class:text-green-500={$status.application.overallStatus === 'healthy'}
@@ -462,23 +465,7 @@
 		</button>
 	{/if}
 	{#if $location && $status.application.overallStatus === 'healthy'}
-		<a href={$location} target="_blank noreferrer" class="btn btn-sm gap-2 text-sm bg-primary"
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				fill="none"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
-				<line x1="10" y1="14" x2="20" y2="4" />
-				<polyline points="15 4 20 4 20 9" />
-			</svg>Open</a
-		>
+		<OpenButton location={$location}/>
 	{/if}
 </div>
 
