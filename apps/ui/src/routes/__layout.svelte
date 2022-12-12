ce3750c51c3884576f397bd1b0790fa3df52d195
hyddeos <ericsageby@hotmail.com>
2022-12-12 17:38:45 +0100

    Add link document to Mobile-menu

--- apps/ui/src/routes/__layout.svelte
+++ apps/ui/src/routes/__layout.svelte
@@ -495,6 +495,30 @@
 					Settings
 				</a>
 			</li>
+			<li>
+				<a
+					class="no-underline icons hover:text-white hover:bg-info"
+					href="https://docs.coollabs.io/coolify/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-8 h-8"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+					/>
+				</svg>
+					Documentation
+				</a>
+			</li>
 			<li class="flex-1 bg-transparent" />
 			<div class="block lg:hidden">
 				<UpdateAvailable />
