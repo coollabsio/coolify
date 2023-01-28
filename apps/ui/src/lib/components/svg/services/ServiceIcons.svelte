e8790a4d4c71683bdd8bc523a3afe86a11aa00cf
Alex <tetrisiq@tuta.io>
2023-01-28 21:00:57 +0000

    feat: remove svg support

--- apps/ui/src/lib/components/svg/services/ServiceIcons.svelte
+++ apps/ui/src/lib/components/svg/services/ServiceIcons.svelte
@@ -2,18 +2,12 @@
 	export let type: string;
 	export let isAbsolute = false;
 	let githubRawIconUrl =
-		'https://raw.githubusercontent.com/coollabsio/coolify-community-templates/main/services/icons';
+		'https://raw.githubusercontent.com/tetrisiq/coolify-community-templates/main/services/icons';
 
 	let fallback = '/icons/default.png';
-	let useFallback: boolean = false;
 
 	const handleError = (ev: { target: { src: string } }) => {
-		if (useFallback) {
-			ev.target.src = fallback;
-		} else {
-			ev.target.src = `${githubRawIconUrl}/${name}.svg`;
-			useFallback = true;
-		}
+		ev.target.src = fallback;
 	};
 
 	const name: any =
