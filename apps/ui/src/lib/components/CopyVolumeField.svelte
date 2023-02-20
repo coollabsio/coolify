a8f24fd1b7a49c62091d0140273946f52f465f26
Andras Bacsai <andras.bacsai@gmail.com>
2023-02-20 09:11:05 +0100

    update icon

--- apps/ui/src/lib/components/CopyVolumeField.svelte
+++ apps/ui/src/lib/components/CopyVolumeField.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { addToast } from '$lib/store';
 
-	export let value = "";
+	export let value = '';
 	let isHttps = window.location.protocol === 'https:';
 
 	function copyToClipboard() {
@@ -14,6 +14,33 @@
 		}
 	}
 </script>
+
+<div class="w-full relative box">
+	<p class="text-white  p-2">{value}</p>
+	<div
+		class="absolute top-0 right-0 flex justify-center items-center h-full cursor-pointer text-stone-600 mr-3"
+	>
+		{#if isHttps}
+			<div on:click={copyToClipboard}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<rect x="8" y="8" width="12" height="12" rx="2" />
+					<path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+				</svg>
+			</div>
+		{/if}
+	</div>
+</div>
+
 <style>
 	.box {
 		position: relative;
@@ -21,18 +48,4 @@
 		border-radius: 5px;
 		padding: 5px;
 	}
-	
 </style>
-
-<div class="w-full relative box">
-	<p class="text-white  p-2">{value}</p>
-    <div class="absolute top-0 right-0 flex justify-center items-center h-full cursor-pointer text-stone-600 mr-3">
-		<div on:click={copyToClipboard}>
-            
-            <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="stroke-gray-700 hover:stroke-white">
-                <path fill="none" stroke-width="2" d="M9,15 L17,15 L9,15 Z M9,11 L19,11 L9,11 Z M9,7 L13,7 L9,7 Z M16,1 L16,7 L22,7 M6,5 L2,5 L2,23 L18,23 L18,19 M22,19 L6,19 L6,1 L17,1 L22,6 L22,19 L22,19 Z"/>
-              </svg>
-              
-		</div>
-	</div>
-</div>
