232e907363f701eb0634a086d0603d4b084350c4
Joao Patricio <ijpatricio@gmail.com>
2023-03-20 14:40:14 +0000

    Have Sail and SSU. Runs a command on a remote host.

--- app/Http/Livewire/RunCommand.php
+++ app/Http/Livewire/RunCommand.php
@@ -22,22 +22,7 @@ class RunCommand extends Component
     public function runCommand()
     {
         $this->isKeepAliveOn = true;
-
-        // Override manual to experiment
-        $override = 0;
-
-        if ($override) {
-            // Good to play with the throttle feature
-            $sleepingBeauty = 'x=1; while  [ $x -le 40 ]; do sleep 0.1 && echo "Welcome $x times" $(( x++ )); done';
-
-            $this->activity = coolifyProcess(<<<EOT
-            cd projects/dummy-project
-            # ~/.docker/cli-plugins/docker-compose build --no-cache
-            $sleepingBeauty
-            EOT, 'testing-host');
-
-            return;
-        }
+        
 
         $this->activity = coolifyProcess($this->command, 'testing-host');
     }
