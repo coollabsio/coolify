2a8d603f987a2826d3349fa330d8a8de354e6421
Andras Bacsai <andras.bacsai@gmail.com>
2023-04-03 13:39:33 +0200

    fix: poll activity

--- resources/views/livewire/poll-activity.blade.php
+++ resources/views/livewire/poll-activity.blade.php
@@ -1,7 +1,3 @@
 <div>
-    <pre style="width: 100%;overflow-y: scroll;" @if ($isKeepAliveOn) wire:poll.750ms="polling" @endif>
-        @isset($activity)
-        {{ (new App\Actions\RemoteProcess\TidyOutput($activity))() }}
-        @endisset
-    </pre>
+    <pre style="width: 100%;overflow-y: scroll;" @if ($isKeepAliveOn) wire:poll.750ms="polling" @endif>{{ data_get($activity, 'description') }}</pre>
 </div>
