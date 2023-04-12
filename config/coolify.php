7954b892bc9f3a27f025e573f05dd4ab5446b8ea
Andras Bacsai <andras.bacsai@gmail.com>
2023-04-12 14:08:48 +0200

    fix: version

--- config/coolify.php
+++ config/coolify.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'version' => '4.0.0-rc.1',
+    'version' => '4.0.0-nightly.0',
 ];
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,5 @@
 <nav>
+    <div>v{{ config('coolify.version') }}</div>
     @guest
         <a href="/login">Login</a>
         <a href="/register">Register</a>
