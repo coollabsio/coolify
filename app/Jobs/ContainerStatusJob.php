4ecf1edb2504555ff7251293c21649c46f88052a
Andras Bacsai <andras.bacsai@gmail.com>
2023-04-14 11:11:22 +0200

    remove unnecessary imports

--- app/Jobs/ContainerStatusJob.php
+++ app/Jobs/ContainerStatusJob.php
@@ -2,19 +2,14 @@
 
 namespace App\Jobs;
 
-use App\Actions\RemoteProcess\RunRemoteProcess;
 use App\Models\Application;
 use App\Models\Server;
-use App\Traits\Docker;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Process;
-use Tests\Support\Output;
 
 class ContainerStatusJob implements ShouldQueue
 {
