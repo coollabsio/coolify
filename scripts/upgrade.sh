91ba7a5704499312884410739570d303bdd69c4a
Andras Bacsai <andras.bacsai@gmail.com>
2023-04-27 13:57:01 +0200

    wip

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ## Do not modify this file. You will lost the ability to autoupdate!
 
-docker run --pull always -v /data/coolify/source:/data/coolify/source -v /var/run/docker.sock:/var/run/docker.sock -ti --rm ghcr.io/coollabsio/coolify-builder bash -c "APP_TAG=$1 docker compose --env-file /data/coolify/source/.env -f /data/coolify/source/docker-compose.yml -f /data/coolify/source/docker-compose.prod.yml up -d --pull always --remove-orphans --force-recreate"
+docker run --pull always -v /data/coolify/source:/data/coolify/source -v /var/run/docker.sock:/var/run/docker.sock -ti --rm ghcr.io/coollabsio/coolify-builder bash -c "APP_TAG=${1:-} docker compose --env-file /data/coolify/source/.env -f /data/coolify/source/docker-compose.yml -f /data/coolify/source/docker-compose.prod.yml up -d --pull always --remove-orphans --force-recreate"
