a7c4e06bc3d30904d141daecc24b2682207569c2
Andras Bacsai <andras.bacsai@gmail.com>
2023-04-28 09:00:47 +0200

    remove dangling images

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Jobs\ContainerStatusJob;
+use App\Jobs\DockerCleanupDanglingImagesJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new ContainerStatusJob)->everyMinute();
+        $schedule->job(new ContainerStatusJob)->everyFiveMinutes();
+        $schedule->job(new DockerCleanupDanglingImagesJob)->everyMinute();
     }
 
     /**
@@ -21,7 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
