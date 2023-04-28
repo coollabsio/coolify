2fb8d928e5985934c9ea5ee9c84d4fbe2e22163b
Andras Bacsai <andras.bacsai@gmail.com>
2023-04-28 11:06:55 +0200

    add hc for coolify

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/health', function () {
+    return 'OK';
 });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
