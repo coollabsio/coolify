4706414e269702ee501fe4eca87b661666784ab7
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-02 14:20:41 +0200

    show private key

--- resources/views/server/show.blade.php
+++ resources/views/server/show.blade.php
@@ -1,6 +1,8 @@
 <x-layout>
     <h1>Server</h1>
     <livewire:server.form :server_id="$server->id" />
+    <h2>Private Key</h2>
+    <p>{{ $server->privateKey->private_key }}</p>
     <h2>Destinations</h2>
     @if ($server->standaloneDockers)
         @foreach ($server->standaloneDockers as $docker)
