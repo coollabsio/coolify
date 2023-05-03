d5c1e1d3f4bdc06d92b4fe524e0bf2f29be75f7d
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-03 14:40:58 +0200

    more css

--- resources/views/components/inputs/button.blade.php
+++ resources/views/components/inputs/button.blade.php
@@ -1,10 +1,11 @@
 @props([
+    'class' => null,
     'defaultClass' => 'text-white bg-violet-500 hover:bg-violet-600',
     'confirm' => null,
     'confirmAction' => null,
 ])
 
-<button {{ $attributes }} {{ $attributes->merge(['class' => $defaultClass]) }}
+<button {{ $attributes }} {{ $attributes->merge(['class' => "$defaultClass $class"]) }}
     @if ($attributes->whereStartsWith('wire:click')) wire:target="{{ $attributes->whereStartsWith('wire:click')->first() }}"
     wire:loading.class="text-black bg-green-500" wire:loading.attr="disabled" wire:loading.class.remove="{{ $defaultClass }} {{ $attributes->whereStartsWith('class')->first() }}" @endif
     @isset($confirm) x-on:click="toggleConfirmModal('{{ $confirm }}')" @endisset
