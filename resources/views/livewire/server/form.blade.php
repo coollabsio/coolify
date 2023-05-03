b69410697d6385b3a7840754c31cc968a6de91db
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-03 10:25:44 +0200

    delete server

--- resources/views/livewire/server/form.blade.php
+++ resources/views/livewire/server/form.blade.php
@@ -22,6 +22,9 @@
                 Submit
             </button>
             <button wire:click.prevent='checkServer'>Check Server</button>
+            <button class="bg-red-500" @confirm.window="$wire.delete()"
+                x-on:click="toggleConfirmModal('Are you sure you would like to delete this application?')">
+                Delete</button>
             {{-- <button wire:click.prevent='installDocker'>Install Docker</button> --}}
         </div>
     </form>
--- resources/views/livewire/server/new/by-ip.blade.php
+++ resources/views/livewire/server/new/by-ip.blade.php
@@ -5,22 +5,29 @@
         <x-form-input id="ip" label="IP Address" required />
         <x-form-input id="user" label="User" />
         <x-form-input type="number" id="port" label="Port" />
+        <x-form-input id="private_key_id" label="Private Key" required hidden />
         <button class="mt-4" type="submit">
             Submit
         </button>
     </form>
-    <h1>Select a private key</h1>
-    @foreach ($private_keys as $key)
-        <button @if ($private_key_id == $key->id) class="bg-green-500" @endif
-            wire:click="setPrivateKey('{{ $key->id }}')">{{ $key->name }}</button>
-    @endforeach
-    <h2>Add a new One</h2>
-    <form wire:submit.prevent='addPrivateKey'>
-        <x-form-input id="new_private_key_name" label="Name" required />
-        <x-form-input id="new_private_key_description" label="Longer Description" />
-        <x-form-input type="textarea" id="new_private_key_value" label="Private Key" required />
-        <button type="submit">
-            Submit
-        </button>
-    </form>
+    <div class="flex gap-4">
+        <div>
+            <h1>Select a private key</h1>
+            @foreach ($private_keys as $key)
+                <button @if ($private_key_id == $key->id) class="bg-green-500" @endif
+                    wire:click.defer="setPrivateKey('{{ $key->id }}')">{{ $key->name }}</button>
+            @endforeach
+        </div>
+        <div>
+            <h2>Add a new One</h2>
+            <form class="flex flex-col gap-2" wire:submit.prevent='addPrivateKey'>
+                <x-form-input id="new_private_key_name" label="Name" required />
+                <x-form-input id="new_private_key_description" label="Longer Description" />
+                <x-form-input type="textarea" id="new_private_key_value" label="Private Key" required />
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    </div>
 </div>
