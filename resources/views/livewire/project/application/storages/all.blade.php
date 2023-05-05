80e915c01596132788dd6a59227b82decf8f12f2
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-05 13:50:29 +0200

    add missing wire:key

--- resources/views/livewire/project/application/storages/all.blade.php
+++ resources/views/livewire/project/application/storages/all.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col gap-2">
     <h3>Persistent Storages</h3>
     @forelse ($application->persistentStorages as $storage)
-        <livewire:project.application.storages.show :storage="$storage" />
+        <livewire:project.application.storages.show wire:key="storage-{{ $storage->id }}" :storage="$storage" />
     @empty
         <p>There are no persistent storage attached for this application.</p>
     @endforelse
