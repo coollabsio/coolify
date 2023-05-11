7b9d1284aaecd9a6131f9d9ac0c601a78163d6e1
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-11 15:29:55 +0200

    again

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import Alpine from "alpinejs";
-import { createApp } from "vue";
+// import { createApp } from "vue";
 // import MagicSearchBar from "./components/MagicSearchBar.vue";
 window.Alpine = Alpine;
 Alpine.start();
 
-const app = createApp({});
+// const app = createApp({});
 // app.component('magic-search-bar', MagicSearchBar);
 // app.mount('#vue');
