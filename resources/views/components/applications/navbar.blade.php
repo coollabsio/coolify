cd5655bd3f1c1af5715c45383d22f1f3ded419c4
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-18 15:19:23 +0200

    save

--- resources/views/components/applications/navbar.blade.php
+++ resources/views/components/applications/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex justify-center gap-4 py-2 border-b-2 border-solid border-coolgray-200">
+<nav class="flex gap-4 py-2 border-b-2 border-solid border-coolgray-200">
     <a
         href="{{ route('project.application.configuration', [
             'project_uuid' => Route::current()->parameters()['project_uuid'],
@@ -15,23 +15,36 @@
         ]) }}">
         Deployments
     </a>
-    <a target="_blank" href="{{ $application->gitBranchLocation }}">
-        Open on Git
-        <x-external-link />
-    </a>
-    @if (data_get($application, 'ports_mappings_array'))
-        @foreach ($application->ports_mappings_array as $port)
-            @if (config('app.env') === 'local')
-                <a target="_blank" href="http://localhost:{{ explode(':', $port)[0] }}">Open
-                    {{ explode(':', $port)[0] }}
+    <div class="flex-1"></div>
+    <div class="dropdown dropdown-hover">
+        <x-inputs.button>Links👇 </x-inputs.button>
+        <ul tabindex="0"
+            class="p-2 text-xs text-white normal-case rounded min-w-max dropdown-content menu bg-coolgray-200">
+            <li>
+                <a class="text-xs"target="_blank" href="{{ $application->gitBranchLocation }}">
+                    Open on Git
                     <x-external-link />
                 </a>
-            @else
-                <a target="_blank"
-                    href="http://{{ $application->destination->server->ip }}:{{ explode(':', $port)[0] }}">Open
-                    {{ $port }}</a>
+            </li>
+            @if (data_get($application, 'ports_mappings_array'))
+                @foreach ($application->ports_mappings_array as $port)
+                    @if (config('app.env') === 'local')
+                        <li>
+                            <a class="text-xs" target="_blank" href="http://localhost:{{ explode(':', $port)[0] }}">Open
+                                {{ explode(':', $port)[0] }}
+                                <x-external-link />
+                            </a>
+                        </li>
+                    @else
+                        <li>
+                            <a class="text-xs" target="_blank"
+                                href="http://{{ $application->destination->server->ip }}:{{ explode(':', $port)[0] }}">Open
+                                {{ $port }}</a>
+                        </li>
+                    @endif
+                @endforeach
             @endif
-        @endforeach
-    @endif
+        </ul>
+    </div>
     <livewire:project.application.deploy :applicationId="$application->id" />
 </nav>
