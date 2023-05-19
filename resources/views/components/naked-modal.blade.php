c4397adfce887057e514a5cae89e48867e79819a
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-19 14:15:03 +0200

    fix

--- resources/views/components/naked-modal.blade.php
+++ resources/views/components/naked-modal.blade.php
@@ -5,7 +5,7 @@
 ])
 <div x-cloak x-show="{{ $show }}" x-transition class="modal modal-open">
     <div class="relative modal-box">
-        <div class="pb-8 text-base font-bold">{{ $message }}</div>
+        <div class="pb-8 text-base font-bold text-white">{{ $message }}</div>
         <div class="flex justify-end gap-4 text-xs">
             <x-inputs.button isWarning wire:click='{{ $action }}' x-on:click="{{ $show }} = false">
                 Yes
