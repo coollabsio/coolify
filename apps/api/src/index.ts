d4798a3b2270bcde01559802e08f0ace37463d86
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-24 20:34:40 +0200

    fix: more aggressive cleanup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -604,53 +604,54 @@ async function cleanupStorage() {
 			if (!destination.remoteVerified) continue;
 			enginesDone.add(destination.remoteIpAddress);
 		}
-		let lowDiskSpace = false;
-		try {
-			let stdout = null;
-			if (!isDev) {
-				const output = await executeCommand({
-					dockerId: destination.id,
-					command: `CONTAINER=$(docker ps -lq | head -1) && docker exec $CONTAINER sh -c 'df -kPT /'`,
-					shell: true
-				});
-				stdout = output.stdout;
-			} else {
-				const output = await executeCommand({
-					command: `df -kPT /`
-				});
-				stdout = output.stdout;
-			}
-			let lines = stdout.trim().split('\n');
-			let header = lines[0];
-			let regex =
-				/^Filesystem\s+|Type\s+|1024-blocks|\s+Used|\s+Available|\s+Capacity|\s+Mounted on\s*$/g;
-			const boundaries = [];
-			let match;
+		await cleanupDockerStorage(destination.id);
+		// let lowDiskSpace = false;
+		// try {
+		// 	let stdout = null;
+		// 	if (!isDev) {
+		// 		const output = await executeCommand({
+		// 			dockerId: destination.id,
+		// 			command: `CONTAINER=$(docker ps -lq | head -1) && docker exec $CONTAINER sh -c 'df -kPT /'`,
+		// 			shell: true
+		// 		});
+		// 		stdout = output.stdout;
+		// 	} else {
+		// 		const output = await executeCommand({
+		// 			command: `df -kPT /`
+		// 		});
+		// 		stdout = output.stdout;
+		// 	}
+		// 	let lines = stdout.trim().split('\n');
+		// 	let header = lines[0];
+		// 	let regex =
+		// 		/^Filesystem\s+|Type\s+|1024-blocks|\s+Used|\s+Available|\s+Capacity|\s+Mounted on\s*$/g;
+		// 	const boundaries = [];
+		// 	let match;
 
-			while ((match = regex.exec(header))) {
-				boundaries.push(match[0].length);
-			}
+		// 	while ((match = regex.exec(header))) {
+		// 		boundaries.push(match[0].length);
+		// 	}
 
-			boundaries[boundaries.length - 1] = -1;
-			const data = lines.slice(1).map((line) => {
-				const cl = boundaries.map((boundary) => {
-					const column = boundary > 0 ? line.slice(0, boundary) : line;
-					line = line.slice(boundary);
-					return column.trim();
-				});
-				return {
-					capacity: Number.parseInt(cl[5], 10) / 100
-				};
-			});
-			if (data.length > 0) {
-				const { capacity } = data[0];
-				if (capacity > 0.8) {
-					lowDiskSpace = true;
-				}
-			}
-		} catch (error) {}
-		if (lowDiskSpace) {
-			await cleanupDockerStorage(destination.id);
-		}
+		// 	boundaries[boundaries.length - 1] = -1;
+		// 	const data = lines.slice(1).map((line) => {
+		// 		const cl = boundaries.map((boundary) => {
+		// 			const column = boundary > 0 ? line.slice(0, boundary) : line;
+		// 			line = line.slice(boundary);
+		// 			return column.trim();
+		// 		});
+		// 		return {
+		// 			capacity: Number.parseInt(cl[5], 10) / 100
+		// 		};
+		// 	});
+		// 	if (data.length > 0) {
+		// 		const { capacity } = data[0];
+		// 		if (capacity > 0.8) {
+		// 			lowDiskSpace = true;
+		// 		}
+		// 	}
+		// } catch (error) {}
+		// if (lowDiskSpace) {
+			// await cleanupDockerStorage(destination.id);
+		// }
 	}
 }
