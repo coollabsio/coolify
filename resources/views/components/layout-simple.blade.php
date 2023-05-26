e59cad82651b20ce3d2a8ddaca47e55d8b5a2268
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-26 12:13:42 +0200

    asd

--- resources/views/components/layout-simple.blade.php
+++ resources/views/components/layout-simple.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://api.fonts.coollabs.io" crossorigin>
     <link href="https://api.fonts.coollabs.io/css2?family=Inter&display=swap" rel="stylesheet">
-    g
+    {{ dd(request()->schemeAndHttpHost(), str_starts_with(request()->schemeAndHttpHost(), 'https')) }}
     @if (str_starts_with(request()->schemeAndHttpHost(), 'https'))
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @else
