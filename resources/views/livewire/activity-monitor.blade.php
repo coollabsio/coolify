820ae61c2020bb4433977c7fa5ca9de558df6454
Andras Bacsai <andras.bacsai@gmail.com>
2023-05-26 15:19:01 +0200

    scrollbar

--- resources/views/livewire/activity-monitor.blade.php
+++ resources/views/livewire/activity-monitor.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if ($this->activity)
         <div
-            class="flex flex-col-reverse w-full overflow-y-auto border border-solid rounded border-coolgray-300 max-h-[32rem] p-4 text-xs text-white">
+            class="scrollbar flex flex-col-reverse w-full overflow-y-auto border border-solid rounded border-coolgray-300 max-h-[32rem] p-4 text-xs text-white">
             <pre class="font-mono whitespace-pre-wrap" @if ($isPollingActive) wire:poll.2000ms="polling" @endif>{{ \App\Actions\CoolifyTask\RunRemoteProcess::decodeOutput($this->activity) }}</pre>
             {{-- @else
             <pre class="whitespace-pre-wrap">Output will be here...</pre> --}}
