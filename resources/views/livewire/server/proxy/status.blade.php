9c055f2149b8224969f396bce8d0f91f3bc4aaf2
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-07 15:30:10 +0200

    update

--- resources/views/livewire/server/proxy/status.blade.php
+++ resources/views/livewire/server/proxy/status.blade.php
@@ -3,13 +3,15 @@
         <div wire:poll.5000ms="proxyStatus">
             @if ($server->extra_attributes->proxy_status === 'running')
                 <x-loading wire:loading.delay.longer />
-                <div class="flex items-center gap-2 text-sm" wire:loading.remove.delay.longer>
-                    <div class="text-xs font-medium tracking-wide text-white badge border-success">Running</div>
+                <div class="flex items-center gap-2" wire:loading.remove.delay.longer>
+                    <div class="badge badge-success badge-xs"></div>
+                    <div class="text-xs font-medium tracking-wide">Running</div>
                 </div>
             @else
                 <x-loading wire:loading.delay.longer />
-                <div class="flex items-center gap-2 text-sm" wire:loading.remove.delay.longer>
-                    <div class="text-xs font-medium tracking-wide text-white badge border-error">Stopped</div>
+                <div class="flex items-center gap-2" wire:loading.remove.delay.longer>
+                    <div class="badge badge-error badge-xs"></div>
+                    <div class="text-xs font-medium tracking-wide">Stopped</div>
                 </div>
             @endif
         </div>
