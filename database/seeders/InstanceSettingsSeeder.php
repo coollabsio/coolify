4df928a66082ca8a3934c56d06eead32fd8af6b2
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-14 09:03:25 +0200

    fix

--- database/seeders/InstanceSettingsSeeder.php
+++ database/seeders/InstanceSettingsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\InstanceSettings;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Process;
 
 class InstanceSettingsSeeder extends Seeder
 {
@@ -23,5 +24,23 @@ class InstanceSettingsSeeder extends Seeder
                 'smtp_from_name' => 'Coolify',
             ]
         ]);
+        try {
+            $ipv4 = Process::run('curl -4s https://ifconfig.io')->output();
+            $ipv4 = trim($ipv4);
+            $ipv4 = filter_var($ipv4, FILTER_VALIDATE_IP);
+            $settings = InstanceSettings::get();
+            if (is_null($settings->public_ipv4) && $ipv4) {
+                $settings->update(['public_ipv4' => $ipv4]);
+            }
+            $ipv6 = Process::run('curl -6s https://ifconfig.io')->output();
+            $ipv6 = trim($ipv6);
+            $ipv6 = filter_var($ipv6, FILTER_VALIDATE_IP);
+            $settings = InstanceSettings::get();
+            if (is_null($settings->public_ipv6) && $ipv6) {
+                $settings->update(['public_ipv6' => $ipv6]);
+            }
+        } catch (\Exception $e) {
+            echo "Error: {$e->getMessage()}\n";
+        }
     }
 }
