6067e5118f11ae1f428978a4e7a8ee35bb82d2e2
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-16 13:57:36 +0200

    fix

--- resources/views/components/applications/navbar.blade.php
+++ resources/views/components/applications/navbar.blade.php
@@ -60,7 +60,7 @@
     </a>
     <div class="flex-1"></div>
     <div class="group">
-        <label tabindex="0" class="flex items-center gap-2  cursor-pointer hover:text-white"> Links
+        <label tabindex="0" class="flex items-center gap-2 cursor-pointer hover:text-white"> Links
             <x-chevron-down />
         </label>
         <div class="absolute hidden group-hover:block">
@@ -74,20 +74,22 @@
                     </a>
                 </li>
                 @if (data_get($application, 'fqdn'))
-                    <li>
-                        <a class="text-xs text-white rounded-none hover:no-underline hover:bg-coollabs" target="_blank"
-                            href="{{ $application->fqdn }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M9 15l6 -6" />
-                                <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
-                                <path
-                                    d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
-                            </svg>{{ $application->fqdn }}
-                        </a>
-                    </li>
+                    @foreach (Str::of(data_get($application, 'fqdn'))->explode(',') as $fqdn)
+                        <li>
+                            <a class="text-xs text-white rounded-none hover:no-underline hover:bg-coollabs"
+                                target="_blank" href="{{ $fqdn }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M9 15l6 -6" />
+                                    <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
+                                    <path
+                                        d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
+                                </svg>{{ $fqdn }}
+                            </a>
+                        </li>
+                    @endforeach
                 @endif
                 @if (data_get($application, 'ports_mappings_array'))
                     @foreach ($application->ports_mappings_array as $port)
--- resources/views/components/naked-modal.blade.php
+++ resources/views/components/naked-modal.blade.php
@@ -5,30 +5,9 @@
 ])
 <div x-cloak x-show="{{ $show }}" x-transition class="relative z-10" aria-labelledby="modal-title" role="dialog"
     aria-modal="true">
-    <!--
-    Background backdrop, show/hide based on modal state.
-
-    Entering: "ease-out duration-300"
-      From: "opacity-0"
-      To: "opacity-100"
-    Leaving: "ease-in duration-200"
-      From: "opacity-100"
-      To: "opacity-0"
-  -->
     <div class="fixed inset-0 transition-opacity bg-coolgray-100/75"></div>
-
     <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
-            <!--
-        Modal panel, show/hide based on modal state.
-
-        Entering: "ease-out duration-300"
-          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          To: "opacity-100 translate-y-0 sm:scale-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100 translate-y-0 sm:scale-100"
-          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      -->
             <div
                 class="relative px-4 pt-5 pb-4 overflow-hidden text-left text-white transition-all transform rounded-lg shadow-xl bg-coolgray-200 sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div class="sm:flex sm:items-start">
