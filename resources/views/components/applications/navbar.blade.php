a4c5105d78f9910b0ab8b8f584278fea6ca11f5d
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-16 16:22:17 +0200

    fix

--- resources/views/components/applications/navbar.blade.php
+++ resources/views/components/applications/navbar.blade.php
@@ -91,6 +91,27 @@
                         </li>
                     @endforeach
                 @endif
+                @if (data_get($application, 'previews')->count() > 0)
+                    @foreach (data_get($application, 'previews') as $preview)
+                        @if (data_get($preview, 'fqdn'))
+                            <li>
+                                <a class="text-xs text-white rounded-none hover:no-underline hover:bg-coollabs"
+                                    target="_blank" href="{{ data_get($preview, 'fqdn') }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M9 15l6 -6" />
+                                        <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
+                                        <path
+                                            d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
+                                    </svg>PR{{ data_get($preview, 'pull_request_id') }} |
+                                    {{ data_get($preview, 'fqdn') }}
+                                </a>
+                            </li>
+                        @endif
+                    @endforeach
+                @endif
                 @if (data_get($application, 'ports_mappings_array'))
                     @foreach ($application->ports_mappings_array as $port)
                         @if (isDev())
