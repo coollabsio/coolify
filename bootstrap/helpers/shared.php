45c8b331de9a5076bb0c68bc6c1efea5330c0490
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-16 13:37:02 +0200

    typo

--- bootstrap/helpers/shared.php
+++ bootstrap/helpers/shared.php
@@ -81,9 +81,10 @@ function set_transanctional_email_settings()
         "local_domain" => null,
     ]);
 }
-function base_ip() {
+function base_ip()
+{
     $settings = InstanceSettings::get();
-    return "http://{$settings->public_ipv4}"
+    return "http://{$settings->public_ipv4}";
 }
 function base_url(bool $withPort = true)
 {
