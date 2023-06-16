7456fc1ac70c5b7560d0ee1ea9f14b734a4ff5ac
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-16 12:05:52 +0200

    fix

--- app/Models/PrivateKey.php
+++ app/Models/PrivateKey.php
@@ -14,10 +14,12 @@ class PrivateKey extends BaseModel
     protected $hidden = [
         'private_key',
     ];
-    static public function ownedByCurrentTeam()
+    static public function ownedByCurrentTeam(array $select = ['*'])
     {
-        return PrivateKey::whereTeamId(session('currentTeam')->id);
+        $selectArray = collect($select)->concat(['id']);
+        return PrivateKey::whereTeamId(session('currentTeam')->id)->where('id', '>', 0)->select($selectArray->all());
     }
+
     public function servers()
     {
         return $this->hasMany(Server::class);
