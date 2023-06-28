47d37c6047c58b31a3fbca496f0516438320db4d
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-28 13:27:05 +0200

    fix: cancel deployment button

--- config/version.php
+++ config/version.php
@@ -1,3 +1,3 @@
 <?php
 
-return '4.0.0-beta.14';
+return '4.0.0-beta.15';
--- resources/views/livewire/project/application/deployment-navbar.blade.php
+++ resources/views/livewire/project/application/deployment-navbar.blade.php
@@ -2,7 +2,5 @@
     <h2>Logs</h2>
     @if (data_get($activity, 'properties.status') === 'in_progress')
         <x-forms.button wire:click.prevent="cancel">Cancel deployment</x-forms.button>
-    @else
-        <x-forms.button disabled>Cancel deployment</x-forms.button>
     @endif
 </div>
