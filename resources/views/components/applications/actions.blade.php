a44b1544b4f17a6de9d9ad5afe09207cbdec59ce
Andras Bacsai <andras.bacsai@gmail.com>
2023-06-30 23:42:23 +0200

    remove debug menu item

--- resources/views/components/applications/actions.blade.php
+++ resources/views/components/applications/actions.blade.php
@@ -19,25 +19,6 @@
                             </svg>Restart</div>
                     </li>
                     <li>
-                        <div class="rounded-none hover:bg-coollabs" wire:click='force_deploy_with_debug'><svg
-                                xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M9 9v-1a3 3 0 0 1 6 0v1" />
-                                <path d="M8 9h8a6 6 0 0 1 1 3v3a5 5 0 0 1 -10 0v-3a6 6 0 0 1 1 -3" />
-                                <path d="M3 13l4 0" />
-                                <path d="M17 13l4 0" />
-                                <path d="M12 20l0 -6" />
-                                <path d="M4 19l3.35 -2" />
-                                <path d="M20 19l-3.35 -2" />
-                                <path d="M4 7l3.75 2.4" />
-                                <path d="M20 7l-3.75 2.4" />
-                            </svg>Force deploy (with
-                            debug)
-                        </div>
-                    </li>
-                    <li>
                         <div class="rounded-none hover:bg-coollabs" wire:click='force_deploy_without_cache'><svg
                                 xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
                                 stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
@@ -77,23 +58,6 @@
                             </svg>Deploy</div>
                     </li>
                     <li>
-                        <div class="rounded-none hover:bg-coollabs" wire:click='deploy(true, true)'><svg
-                                xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M9 9v-1a3 3 0 0 1 6 0v1" />
-                                <path d="M8 9h8a6 6 0 0 1 1 3v3a5 5 0 0 1 -10 0v-3a6 6 0 0 1 1 -3" />
-                                <path d="M3 13l4 0" />
-                                <path d="M17 13l4 0" />
-                                <path d="M12 20l0 -6" />
-                                <path d="M4 19l3.35 -2" />
-                                <path d="M20 19l-3.35 -2" />
-                                <path d="M4 7l3.75 2.4" />
-                                <path d="M20 7l-3.75 2.4" />
-                            </svg>Force deploy (with
-                            debug)
-                        </div>
                     </li>
                     <li>
                         <div class="rounded-none hover:bg-coollabs" wire:click='deploy(true)'><svg
