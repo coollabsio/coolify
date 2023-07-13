d93fa7f752f1af5321646b92e1c246d62425dfa8
Andras Bacsai <andras.bacsai@gmail.com>
2023-07-13 13:19:54 +0200

    typo

--- resources/views/livewire/project/application/storages/add.blade.php
+++ resources/views/livewire/project/application/storages/add.blade.php
@@ -1,6 +1,6 @@
 <dialog id="newStorage" class="modal">
     <form method="dialog" class="flex flex-col gap-2 rounded modal-box" wire:submit.prevent='submit'>
-        <h3 class="text-lg font-bold">Add Environment Variable</h3>
+        <h3 class="text-lg font-bold">Add Storage Volume</h3>
         <x-forms.input placeholder="pv-name" id="name" label="Name" required />
         <x-forms.input placeholder="/root" id="host_path" label="Source Path" />
         <x-forms.input placeholder="/tmp/root" id="mount_path" label="Destination Path" required />
