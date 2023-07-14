48ad40dea221d0d844a78e7fad42b76c845a92c4
Andras Bacsai <andras.bacsai@gmail.com>
2023-07-14 12:14:32 +0200

    fix: loading after button text

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -8,11 +8,11 @@
     @endisset
     @if ($isModal) onclick="{{ $modalId }}.showModal()" @endif>
 
+    {{ $slot }}
     @if ($attributes->get('type') === 'submit')
         <span wire:target="submit" wire:loading.delay class="loading loading-xs text-warning loading-spinner"></span>
     @else
         <span wire:target="{{ explode('(', $attributes->whereStartsWith('wire:click')->first())[0] }}" wire:loading.delay
             class="loading loading-xs loading-spinner"></span>
     @endif
-    {{ $slot }}
 </button>
