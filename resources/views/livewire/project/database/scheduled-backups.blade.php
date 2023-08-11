f097b80c35ad2ef536fc6662d1ae04eb164a3f28
Andras Bacsai <andras.bacsai@gmail.com>
2023-08-11 10:43:44 +0200

    refactor

--- resources/views/livewire/project/database/scheduled-backups.blade.php
+++ resources/views/livewire/project/database/scheduled-backups.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-wrap gap-2">
     @forelse($database->scheduledBackups as $backup)
         <a class="box flex flex-col"
-           href="{{ route('project.database.backups.logs', [...$parameters,'backup_uuid'=> $backup->uuid]) }}">
+           href="{{ route('project.database.backups.executions', [...$parameters,'backup_uuid'=> $backup->uuid]) }}">
             <div>Frequency: {{$backup->frequency}}</div>
             <div>Last backup: {{data_get($backup->latest_log, 'status','No backup yet')}}</div>
             <div>Number of backups to keep (locally): {{$backup->number_of_backups_locally}}</div>
