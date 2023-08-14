f8506f9d20ab8a7205c69279b06ae5dd0d1acb3c
Andras Bacsai <andras.bacsai@gmail.com>
2023-08-14 15:25:03 +0200

    fix: self-hosted

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -12,10 +12,17 @@ class ServerController extends Controller
 
     public function new_server()
     {
+        if (!is_cloud()) {
+            return view('server.create', [
+                'limit_reached' => false,
+                'private_keys' => PrivateKey::ownedByCurrentTeam()->get(),
+            ]);
+        }
         $servers = auth()->user()->currentTeam()->servers->count();
-        $subscription = auth()->user()->currentTeam()->subscription->type();
+        $subscription = auth()->user()->currentTeam()?->subscription->type();
         $limits = config('constants.limits.server')[strtolower($subscription)];
         $limit_reached = true ?? $servers >= $limits[$subscription];
+
         return view('server.create', [
             'limit_reached' => $limit_reached,
             'private_keys' => PrivateKey::ownedByCurrentTeam()->get(),
