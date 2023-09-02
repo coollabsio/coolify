ab3055150f6439d4a9408819d386482699177e01
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-02 15:53:12 +0200

    test distributed application check

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,15 +2,19 @@
 
 namespace App\Console;
 
+use App\Jobs\ApplicationContainerStatusJob;
 use App\Jobs\CheckResaleLicenseJob;
 use App\Jobs\CleanupInstanceStuffsJob;
 use App\Jobs\DatabaseBackupJob;
+use App\Jobs\DatabaseContainerStatusJob;
 use App\Jobs\DockerCleanupJob;
 use App\Jobs\InstanceAutoUpdateJob;
 use App\Jobs\ProxyCheckJob;
 use App\Jobs\ResourceStatusJob;
+use App\Models\Application;
 use App\Models\InstanceSettings;
 use App\Models\ScheduledDatabaseBackup;
+use App\Models\StandalonePostgresql;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -20,7 +24,7 @@ class Kernel extends ConsoleKernel
     {
         if (isDev()) {
             $schedule->command('horizon:snapshot')->everyMinute();
-            $schedule->job(new ResourceStatusJob)->everyMinute();
+            // $schedule->job(new ResourceStatusJob)->everyMinute();
             $schedule->job(new ProxyCheckJob)->everyFiveMinutes();
             $schedule->job(new CleanupInstanceStuffsJob)->everyMinute();
             // $schedule->job(new CheckResaleLicenseJob)->hourly();
@@ -28,13 +32,26 @@ class Kernel extends ConsoleKernel
         } else {
             $schedule->command('horizon:snapshot')->everyFiveMinutes();
             $schedule->job(new CleanupInstanceStuffsJob)->everyTenMinutes()->onOneServer();
-            $schedule->job(new ResourceStatusJob)->everyMinute()->onOneServer();
+            // $schedule->job(new ResourceStatusJob)->everyMinute()->onOneServer();
             $schedule->job(new CheckResaleLicenseJob)->hourly()->onOneServer();
             $schedule->job(new ProxyCheckJob)->everyFiveMinutes()->onOneServer();
             $schedule->job(new DockerCleanupJob)->everyTenMinutes()->onOneServer();
         }
         $this->instance_auto_update($schedule);
         $this->check_scheduled_backups($schedule);
+        $this->check_resources($schedule);
+    }
+    private function check_resources($schedule)
+    {
+        $applications = Application::all();
+        foreach ($applications as $application) {
+            $schedule->job(new ApplicationContainerStatusJob($application))->everyMinute()->onOneServer();
+        }
+
+        $postgresqls = StandalonePostgresql::all();
+        foreach ($postgresqls as $postgresql) {
+            $schedule->job(new DatabaseContainerStatusJob($postgresql))->everyMinute()->onOneServer();
+        }
     }
     private function instance_auto_update($schedule){
         if (isDev()) {
