040f47b59c43d6c6de4b4ab4281d7d9b16a086f3
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-02 13:41:42 +0200

    fix: show hosted email service, just disable for non pro subs

--- resources/views/livewire/notifications/email-settings.blade.php
+++ resources/views/livewire/notifications/email-settings.blade.php
@@ -35,7 +35,13 @@
             <x-forms.checkbox instantSave="instantSaveInstance" id="team.use_instance_email_settings"
                 label="Use hosted email service" />
         </div>
+    @else
+    <div class="pb-4 w-96">
+        <x-forms.checkbox disabled id="team.use_instance_email_settings"
+            label="Use hosted email service (Pro+ subscription required)" />
+    </div>
     @endif
+    <h3 class="pb-4">Custom Email Service</h3>
     @if (!$team->use_instance_email_settings)
         <form class="flex flex-col items-end gap-2 pb-4 xl:flex-row" wire:submit.prevent='submitFromFields'>
             <x-forms.input required id="team.smtp_from_name" helper="Name used in emails." label="From Name" />
