acd78ae19679a6e19dacdbf14582a61eff3c8624
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-08 14:15:28 +0200

    feat: Telegram topics separation

--- app/Notifications/Channels/TelegramChannel.php
+++ app/Notifications/Channels/TelegramChannel.php
@@ -10,16 +10,30 @@ class TelegramChannel
     {
         $data = $notification->toTelegram($notifiable);
         $telegramData = $notifiable->routeNotificationForTelegram();
-
         $message = data_get($data, 'message');
         $buttons = data_get($data, 'buttons', []);
-        ray($message, $buttons);
         $telegramToken = data_get($telegramData, 'token');
         $chatId = data_get($telegramData, 'chat_id');
+        $topicId  = null;
+        $topicsInstance = get_class($notification);
 
+        switch ($topicsInstance) {
+            case 'App\Notifications\StatusChange':
+                $topicId = data_get($notifiable, 'telegram_notifications_status_changes_message_thread_id');
+                break;
+            case 'App\Notifications\Test':
+                $topicId = data_get($notifiable, 'telegram_notifications_test_message_thread_id');
+                break;
+            case 'App\Notifications\Deployment':
+                $topicId = data_get($notifiable, 'telegram_notifications_deployments_message_thread_id');
+                break;
+            case 'App\Notifications\DatabaseBackup':
+                $topicId = data_get($notifiable, 'telegram_notifications_database_backups_message_thread_id');
+                break;
+        }
         if (!$telegramToken || !$chatId || !$message) {
             throw new \Exception('Telegram token, chat id and message are required');
         }
-        dispatch(new SendMessageToTelegramJob($message, $buttons, $telegramToken, $chatId));
+        dispatch(new SendMessageToTelegramJob($message, $buttons, $telegramToken, $chatId, $topicId));
     }
 }
