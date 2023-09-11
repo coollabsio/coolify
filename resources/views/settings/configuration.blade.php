5747a87f66e6d4cc9f9ba48c365e9deb31ec4797
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-11 22:55:23 +0200

    ui: fix

--- resources/views/settings/configuration.blade.php
+++ resources/views/settings/configuration.blade.php
@@ -7,7 +7,7 @@
             <a :class="activeTab === 'backup' && 'text-white'"
                 @click.prevent="activeTab = 'backup'; window.location.hash = 'backup'" href="#">Instance Backup</a>
             <a :class="activeTab === 'smtp' && 'text-white'"
-                @click.prevent="activeTab = 'smtp'; window.location.hash = 'smtp'" href="#">Transactional/Shared Email</a>
+                @click.prevent="activeTab = 'smtp'; window.location.hash = 'smtp'" href="#">Transactional Email</a>
         </div>
         <div class="w-full pl-8">
             <div x-cloak x-show="activeTab === 'general'" class="h-full">
