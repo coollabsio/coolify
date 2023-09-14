c287276d0edd0bab538e58a481dfce73e3eeba02
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-14 17:10:37 +0200

    temporary fix for proxy

--- app/Http/Livewire/Server/Proxy/Deploy.php
+++ app/Http/Livewire/Server/Proxy/Deploy.php
@@ -22,7 +22,7 @@ class Deploy extends Component
             $this->server->proxy->last_applied_settings &&
             $this->server->proxy->last_saved_settings !== $this->server->proxy->last_applied_settings
         ) {
-            resolve(SaveConfigurationSync::class)($this->server, $this->proxy_settings);
+            resolve(SaveConfigurationSync::class)($this->server);
         }
 
         $activity = resolve(StartProxy::class)($this->server);
