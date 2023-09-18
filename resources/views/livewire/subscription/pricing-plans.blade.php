af57b2aa732fd2301908e7602969233d617dc281
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-18 13:42:35 +0200

    pricing change

--- resources/views/livewire/subscription/pricing-plans.blade.php
+++ resources/views/livewire/subscription/pricing-plans.blade.php
@@ -2,29 +2,34 @@
     @if (config('subscription.provider') === 'stripe')
         <x-slot:basic>
             <x-forms.button x-show="selected === 'monthly'" x-cloak aria-describedby="tier-basic"
-                class="w-full h-10 buyme" wire:click="subscribeStripe('basic-monthly')"> {{$isTrial ? 'Start Trial' : 'Subscribe' }}
+                class="w-full h-10 buyme" wire:click="subscribeStripe('basic-monthly')">
+                {{ $isTrial ? 'Start Trial' : 'Subscribe' }}
             </x-forms.button>
 
             <x-forms.button x-show="selected === 'yearly'" x-cloak aria-describedby="tier-basic"
-                class="w-full h-10 buyme" wire:click="subscribeStripe('basic-yearly')"> {{$isTrial ? 'Start Trial' : 'Subscribe' }}
+                class="w-full h-10 buyme" wire:click="subscribeStripe('basic-yearly')">
+                {{ $isTrial ? 'Start Trial' : 'Subscribe' }}
             </x-forms.button>
         </x-slot:basic>
         <x-slot:pro>
             <x-forms.button x-show="selected === 'monthly'" x-cloak aria-describedby="tier-pro"
-                class="w-full h-10 buyme" wire:click="subscribeStripe('pro-monthly')"> {{$isTrial ? 'Start Trial' : 'Subscribe' }}
+                class="w-full h-10 buyme" wire:click="subscribeStripe('pro-monthly')">
+                {{ $isTrial ? 'Start Trial' : 'Subscribe' }}
             </x-forms.button>
 
             <x-forms.button x-show="selected === 'yearly'" x-cloak aria-describedby="tier-pro" class="w-full h-10 buyme"
-                wire:click="subscribeStripe('pro-yearly')"> {{$isTrial ? 'Start Trial' : 'Subscribe' }}
+                wire:click="subscribeStripe('pro-yearly')"> {{ $isTrial ? 'Start Trial' : 'Subscribe' }}
             </x-forms.button>
         </x-slot:pro>
         <x-slot:ultimate>
             <x-forms.button x-show="selected === 'monthly'" x-cloak aria-describedby="tier-ultimate"
-                class="w-full h-10 buyme" wire:click="subscribeStripe('ultimate-monthly')"> {{$isTrial ? 'Start Trial' : 'Subscribe' }}
+                class="w-full h-10 buyme"><a class="text-white hover:no-underline" href="https://docs.coollabs.io/contact" target="_blank">
+                    Contact Us</a>
             </x-forms.button>
 
             <x-forms.button x-show="selected === 'yearly'" x-cloak aria-describedby="tier-ultimate"
-                class="w-full h-10 buyme" wire:click="subscribeStripe('ultimate-yearly')"> {{$isTrial ? 'Start Trial' : 'Subscribe' }}
+                class="w-full h-10 buyme"><a class="text-white hover:no-underline" href="https://docs.coollabs.io/contact" target="_blank">
+                    Contact Us</a>
             </x-forms.button>
         </x-slot:ultimate>
     @endif
