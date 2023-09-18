157da798ddc3b0a074b14da470963e910228cf72
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-18 09:58:13 +0200

    fix: proxy start (if not proxy defined, use Traefik)

--- resources/views/livewire/project/shared/destination.blade.php
+++ resources/views/livewire/project/shared/destination.blade.php
@@ -3,6 +3,6 @@
     <div class="">The destination server / network where your application will be deployed to.</div>
     <div class="py-4 ">
         <p>Server: {{ data_get($destination, 'server.name') }}</p>
-        <p>Destination Network: {{ $destination->network }}</p>
+        <p>Destination Network: {{ data_get($destination, 'server.network') }}</p>
     </div>
 </div>
