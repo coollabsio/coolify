cbf9bc99ea65cd2c349746b9d948dd34afc5ac50
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-25 18:07:20 +0200

    ui: help

--- resources/views/livewire/help.blade.php
+++ resources/views/livewire/help.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col w-11/12 max-w-5xl gap-2 modal-box">
     <h3>How can we help?</h3>
-    <div>Please report bugs you found or send us general feedback. It helps us a lot! Thank you! 💜</div>
+    <div>Your feedback helps us to improve Coolify. Thank you! 💜</div>
     <form wire:submit.prevent="submit" class="flex flex-col gap-4 pt-4">
         <x-forms.input id="subject" label="Subject" placeholder="Summary of your problem."></x-forms.input>
         <x-forms.textarea rows="10" id="description" label="Description"
