356394c03d230ae54f0358d537a7896b6d99c070
Andras Bacsai <andras.bacsai@gmail.com>
2023-09-25 11:15:35 +0200

    ui: update help modal

--- resources/views/livewire/help.blade.php
+++ resources/views/livewire/help.blade.php
@@ -1,11 +1,11 @@
 <div class="flex flex-col w-11/12 max-w-5xl gap-2 modal-box">
     <h3>How can we help?</h3>
-    <div>You can report bugs, or send us general feedback.</div>
+    <div>Please report bugs you found or send us general feedback. It helps us a lot! Thank you! 💜</div>
     <form wire:submit.prevent="submit" class="flex flex-col gap-4 pt-4">
         <x-forms.input id="subject" label="Subject" placeholder="Summary of your problem."></x-forms.input>
         <x-forms.textarea rows="10" id="description" label="Description"
             placeholder="Please provide as much information as possible."></x-forms.textarea>
-        <div>Thank you for your feedback! 💜</div>
+        <div></div>
         <x-forms.button class="w-full mt-4" type="submit" onclick="help.close()">Send Email</x-forms.button>
     </form>
 </div>
