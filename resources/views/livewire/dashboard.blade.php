8abfaa1967da919a36b8d44b1e2160f31e6c3974
Andras Bacsai <andras.bacsai@gmail.com>
2023-10-10 10:57:56 +0200

    fix: no env goto envs from dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -44,12 +44,21 @@
     @foreach ($projects as $project)
         <div class="gap-2 border border-transparent cursor-pointer box group" x-data
             x-on:click="goto('{{ $project->uuid }}')">
-            <a class="flex flex-col flex-1 mx-6 hover:no-underline"
-                href="{{ route('project.resources', ['project_uuid' => data_get($project, 'uuid'), 'environment_name' => data_get($project, 'environments.0.name', 'production')]) }}">
-                <div class="font-bold text-white">{{ $project->name }}</div>
-                <div class="text-xs group-hover:text-white hover:no-underline">
-                    {{ $project->description }}</div>
-            </a>
+            @if (data_get($project, 'environments.0.name'))
+                <a class="flex flex-col flex-1 mx-6 hover:no-underline"
+                    href="{{ route('project.resources', ['project_uuid' => data_get($project, 'uuid'), 'environment_name' => data_get($project, 'environments.0.name', 'production')]) }}">
+                    <div class="font-bold text-white">{{ $project->name }}</div>
+                    <div class="text-xs group-hover:text-white hover:no-underline">
+                        {{ $project->description }}</div>
+                </a>
+            @else
+                <a class="flex flex-col flex-1 mx-6 hover:no-underline"
+                    href="{{ route('project.show', ['project_uuid' => data_get($project, 'uuid')]) }}">
+                    <div class="font-bold text-white">{{ $project->name }}</div>
+                    <div class="text-xs group-hover:text-white hover:no-underline">
+                        {{ $project->description }}</div>
+                </a>
+            @endif
             <a class="mx-4 rounded group-hover:text-white"
                 href="{{ route('project.edit', ['project_uuid' => data_get($project, 'uuid')]) }}">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon hover:text-warning" viewBox="0 0 24 24"
@@ -66,38 +75,37 @@
 </div>
 <h3 class="py-4">Servers</h3>
 @if ($servers->count() === 1)
-<div class="grid grid-cols-1 gap-2">
-@else
-    <div class="grid grid-cols-3 gap-2">
+    <div class="grid grid-cols-1 gap-2">
+    @else
+        <div class="grid grid-cols-3 gap-2">
 @endif
-    @foreach ($servers as $server)
-        <a href="{{ route('server.show', ['server_uuid' => data_get($server, 'uuid')]) }}"
-            @class([
-                'gap-2 border cursor-pointer box group',
-                'border-transparent' => $server->settings->is_reachable,
-                'border-red-500' => !$server->settings->is_reachable,
-            ])>
-            <div class="flex flex-col mx-6">
-                <div class="font-bold text-white">
-                    {{ $server->name }}
-                </div>
-                <div class="text-xs group-hover:text-white">
-                    {{ $server->description }}</div>
-                <div class="flex gap-1 text-xs text-error">
-                    @if (!$server->settings->is_reachable)
-                        <span>Not reachable</span>
-                    @endif
-                    @if (!$server->settings->is_reachable && !$server->settings->is_usable)
-                        &
-                    @endif
-                    @if (!$server->settings->is_usable)
-                        <span>Not usable by Coolify</span>
-                    @endif
-                </div>
+@foreach ($servers as $server)
+    <a href="{{ route('server.show', ['server_uuid' => data_get($server, 'uuid')]) }}" @class([
+        'gap-2 border cursor-pointer box group',
+        'border-transparent' => $server->settings->is_reachable,
+        'border-red-500' => !$server->settings->is_reachable,
+    ])>
+        <div class="flex flex-col mx-6">
+            <div class="font-bold text-white">
+                {{ $server->name }}
             </div>
-            <div class="flex-1"></div>
-        </a>
-    @endforeach
+            <div class="text-xs group-hover:text-white">
+                {{ $server->description }}</div>
+            <div class="flex gap-1 text-xs text-error">
+                @if (!$server->settings->is_reachable)
+                    <span>Not reachable</span>
+                @endif
+                @if (!$server->settings->is_reachable && !$server->settings->is_usable)
+                    &
+                @endif
+                @if (!$server->settings->is_usable)
+                    <span>Not usable by Coolify</span>
+                @endif
+            </div>
+        </div>
+        <div class="flex-1"></div>
+    </a>
+@endforeach
 </div>
 {{-- <x-forms.button wire:click='getIptables'>Get IPTABLES</x-forms.button> --}}
 </div>
