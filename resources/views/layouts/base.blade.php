96660994089fc8f818c35128e654c116b5b30ecf
Andras Bacsai <andras.bacsai@gmail.com>
2023-10-11 14:43:34 +0200

    commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html data-theme="coollabs" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
