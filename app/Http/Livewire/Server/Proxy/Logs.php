5541c135df734e5ad293ececa4e899416b1c33a4
Andras Bacsai <andras.bacsai@gmail.com>
2023-10-11 11:00:40 +0200

    feat: proxy logs on the ui

--- app/Http/Livewire/Server/Proxy/Logs.php
+++ app/Http/Livewire/Server/Proxy/Logs.php
@@ -5,15 +5,10 @@ namespace App\Http\Livewire\Server\Proxy;
 use App\Models\Server;
 use Livewire\Component;
 
-class Show extends Component
+class Logs extends Component
 {
     public ?Server $server = null;
     public $parameters = [];
-    protected $listeners = ['proxyStatusUpdated'];
-    public function proxyStatusUpdated()
-    {
-        $this->server->refresh();
-    }
     public function mount()
     {
         $this->parameters = get_route_parameters();
@@ -28,6 +23,6 @@ class Show extends Component
     }
     public function render()
     {
-        return view('livewire.server.proxy.show');
+        return view('livewire.server.proxy.logs');
     }
 }
---
+++ app/View/Components/Server/Sidebar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components\Server;
+
+use App\Models\Server;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Sidebar extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public Server $server, public $parameters)
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.server.sidebar');
+    }
+}
