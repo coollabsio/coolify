49c56524e18f34363e5251d3bb8d80fc16697aeb
Andras Bacsai <andras.bacsai@gmail.com>
2023-10-13 15:17:04 +0200

    fix: dev containerjobs

--- app/Jobs/ContainerStatusJob.php
+++ app/Jobs/ContainerStatusJob.php
@@ -39,6 +39,9 @@ class ContainerStatusJob implements ShouldQueue, ShouldBeEncrypted
 
     public function __construct(public Server $server)
     {
+        if (isDev()) {
+            $this->handle();
+        }
     }
 
     public function handle()
