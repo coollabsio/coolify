97fd56b9e49fc47326fdfa9a67de94ee32b5d0a5
Andras Bacsai <andras.bacsai@gmail.com>
2023-11-23 10:57:11 +0100

    Update number of servers in pricing plans

--- resources/views/components/pricing-plans.blade.php
+++ resources/views/components/pricing-plans.blade.php
@@ -81,7 +81,7 @@
                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
                                     clip-rule="evenodd" />
                             </svg>
-                            1 server <x-helper helper="Bring Your Own Server." />
+                            2 servers <x-helper helper="Bring Your Own Server." />
                         </li>
                         <li class="flex gap-x-3">
                             <svg class="flex-none w-5 h-6 text-warning" viewBox="0 0 20 20" fill="currentColor"
