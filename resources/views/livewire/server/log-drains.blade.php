96327af83814d4b3b7d0fc87096181a68c0e7210
Andras Bacsai <andras.bacsai@gmail.com>
2023-11-23 12:44:08 +0100

    Update log-drains.blade.php and add trigger-with-external-database.yaml and service-templates.json

--- resources/views/livewire/server/log-drains.blade.php
+++ resources/views/livewire/server/log-drains.blade.php
@@ -24,24 +24,7 @@
                     </x-forms.button>
                 </div>
             </form>
-            {{-- <h3>Highlight.io</h3>
-            <div class="w-32">
-                <x-forms.checkbox instantSave='instantSave("highlight")'
-                    id="server.settings.is_logdrain_highlight_enabled" label="Enabled" />
-            </div>
-            <form wire:submit.prevent='submit("highlight")' class="flex flex-col">
-                <div class="flex flex-col gap-4">
-                    <div class="flex flex-col w-full gap-2 xl:flex-row">
-                        <x-forms.input type="password" required id="server.settings.logdrain_highlight_project_id"
-                            label="Project Id" />
-                    </div>
-                </div>
-                <div class="flex justify-end gap-4 pt-6">
-                    <x-forms.button type="submit">
-                        Save
-                    </x-forms.button>
-                </div>
-            </form> --}}
+
             <h3>Axiom</h3>
             <div class="w-32">
                 <x-forms.checkbox instantSave='instantSave("axiom")' id="server.settings.is_logdrain_axiom_enabled"
@@ -61,6 +44,24 @@
                     </x-forms.button>
                 </div>
             </form>
+            {{-- <h3>Highlight.io</h3>
+            <div class="w-32">
+                <x-forms.checkbox instantSave='instantSave("highlight")'
+                    id="server.settings.is_logdrain_highlight_enabled" label="Enabled" />
+            </div>
+            <form wire:submit.prevent='submit("highlight")' class="flex flex-col">
+                <div class="flex flex-col gap-4">
+                    <div class="flex flex-col w-full gap-2 xl:flex-row">
+                        <x-forms.input type="password" required id="server.settings.logdrain_highlight_project_id"
+                            label="Project Id" />
+                    </div>
+                </div>
+                <div class="flex justify-end gap-4 pt-6">
+                    <x-forms.button type="submit">
+                        Save
+                    </x-forms.button>
+                </div>
+            </form> --}}
         </div>
     </div>
 </div>
