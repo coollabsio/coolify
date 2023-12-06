852c2df12e453085bd44399344297bbc581c2009
Andras Bacsai <andras.bacsai@gmail.com>
2023-12-06 17:02:07 +0100

    mono

--- resources/views/livewire/project/service/compose-modal.blade.php
+++ resources/views/livewire/project/service/compose-modal.blade.php
@@ -13,11 +13,11 @@
         name collision. <br>To see the actual volume names, check the Deployable Compose file, or go to Storage
         menu.</div>
 
-    <div x-cloak x-show="raw">
+    <div x-cloak x-show="raw" class="font-mono">
         <x-forms.textarea rows="20" id="service.docker_compose_raw">
         </x-forms.textarea>
     </div>
-    <div x-cloak x-show="raw === false">
+    <div x-cloak x-show="raw === false" class="font-mono">
         <x-forms.textarea rows="20" readonly id="service.docker_compose">
         </x-forms.textarea>
     </div>
