61efdfb7c173dfbd6aa56c5f4f7b2b6cd6b9428c
Andras Bacsai <andras.bacsai@gmail.com>
2023-12-06 15:30:04 +0100

    revert

--- resources/views/components/status/stopped.blade.php
+++ resources/views/components/status/stopped.blade.php
@@ -1,7 +1,7 @@
 @props([
     'status' => 'Stopped',
 ])
-{{-- <x-loading wire:loading.delay.longer /> --}}
+<x-loading wire:loading.delay.longer />
 <div class="flex items-center gap-2 " wire:loading.remove.delay.longer>
     <div class="badge badge-error badge-xs"></div>
     <div class="text-xs font-medium tracking-wide text-error">{{ Str::headline($status) }}</div>
