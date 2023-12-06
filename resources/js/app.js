6ccafacc8741e0f505b79b55600cc89736805267
Andras Bacsai <andras.bacsai@gmail.com>
2023-12-06 17:02:27 +0100

    very important commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import MagicBar from "./components/MagicBar.vue";
 import Toaster from "../../vendor/masmerise/livewire-toaster/resources/js";
 import "../../vendor/wire-elements/modal/resources/js/modal";
 
-Alpine.plugin(focus)
+Alpine.plugin(focus);
 Alpine.plugin(Toaster);
 
 window.Alpine = Alpine;
