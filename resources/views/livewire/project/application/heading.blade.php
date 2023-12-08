ba2e4c06f12aab393c26d8263086bd0d9153198d
Andras Bacsai <andras.bacsai@gmail.com>
2023-12-08 21:37:20 +0100

    fix

--- resources/views/livewire/project/application/heading.blade.php
+++ resources/views/livewire/project/application/heading.blade.php
@@ -1,6 +1,4 @@
 <nav x-init="$wire.check_status" wire:poll.10000ms="check_status">
-    @persist($application->uuid)
     <x-resources.breadcrumbs :resource="$application" :parameters="$parameters" />
     <x-applications.navbar :application="$application" :parameters="$parameters" />
-    @endpersist
 </nav>
