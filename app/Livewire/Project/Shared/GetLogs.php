e9920f05f5249de6d90a8f95a6b768bc69466a59
Andras Bacsai <andras.bacsai@gmail.com>
2023-12-11 15:08:40 +0100

    fix: proxy logs

--- app/Livewire/Project/Shared/GetLogs.php
+++ app/Livewire/Project/Shared/GetLogs.php
@@ -70,27 +70,27 @@ class GetLogs extends Component
     }
     public function getLogs($refresh = false)
     {
-        if (!$refresh && $this->resource->getMorphClass() === 'App\Models\Service') return;
-        if ($this->container) {
-            if ($this->showTimeStamps) {
-                $sshCommand = generateSshCommand($this->server, "docker logs -n {$this->numberOfLines} -t {$this->container}");
-            } else {
-                $sshCommand = generateSshCommand($this->server, "docker logs -n {$this->numberOfLines} {$this->container}");
+            if (!$refresh && $this->resource?->getMorphClass() === 'App\Models\Service') return;
+            if ($this->container) {
+                if ($this->showTimeStamps) {
+                    $sshCommand = generateSshCommand($this->server, "docker logs -n {$this->numberOfLines} -t {$this->container}");
+                } else {
+                    $sshCommand = generateSshCommand($this->server, "docker logs -n {$this->numberOfLines} {$this->container}");
+                }
+                if ($refresh) {
+                    $this->outputs = '';
+                }
+                Process::run($sshCommand, function (string $type, string $output) {
+                    $this->doSomethingWithThisChunkOfOutput($output);
+                });
+                if ($this->showTimeStamps) {
+                    $this->outputs = str($this->outputs)->split('/\n/')->sort(function ($a, $b) {
+                        $a = explode(' ', $a);
+                        $b = explode(' ', $b);
+                        return $a[0] <=> $b[0];
+                    })->join("\n");
+                }
             }
-            if ($refresh) {
-                $this->outputs = '';
-            }
-            Process::run($sshCommand, function (string $type, string $output) {
-                $this->doSomethingWithThisChunkOfOutput($output);
-            });
-            if ($this->showTimeStamps) {
-                $this->outputs = str($this->outputs)->split('/\n/')->sort(function ($a, $b) {
-                    $a = explode(' ', $a);
-                    $b = explode(' ', $b);
-                    return $a[0] <=> $b[0];
-                })->join("\n");
-            }
-        }
     }
     public function render()
     {
