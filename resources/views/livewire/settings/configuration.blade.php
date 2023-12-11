41b6df0e6ef874af737e4ba90f9720a9c749e4e9
Andras Bacsai <andras.bacsai@gmail.com>
2023-12-11 21:26:21 +0100

    fix

--- resources/views/livewire/settings/configuration.blade.php
+++ resources/views/livewire/settings/configuration.blade.php
@@ -22,7 +22,8 @@
     <h2 class="pt-6">Advanced</h2>
     <div class="flex flex-col py-6 text-right w-80">
         @isset(env('AUTOUPDATE'))
-            <x-forms.checkbox instantSave helper="AUTOUPDATE is set in .env file, you need to modify it there." disabled id="is_auto_update_enabled" label="Auto Update Coolify" />
+            <x-forms.checkbox instantSave helper="AUTOUPDATE is set in .env file, you need to modify it there." disabled
+                id="is_auto_update_enabled" label="Auto Update Coolify" />
         @else
             <x-forms.checkbox instantSave id="is_auto_update_enabled" label="Auto Update Coolify" />
         @endisset
