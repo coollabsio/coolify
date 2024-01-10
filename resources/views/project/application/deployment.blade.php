225bf0673637a5f9066e25ffcbb803dc211b3d75
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-10 10:50:07 +0100

    Update version numbers and remove unused files

--- resources/views/project/application/deployment.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout>
-    <h1 class="py-0">Deployment</h1>
-    <livewire:project.application.heading :application="$application" />
-    <livewire:project.application.deployment-logs :application_deployment_queue="$application_deployment_queue" />
-</x-layout>
--- resources/views/project/application/deployments.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout>
-    <h1>Deployments</h1>
-    <livewire:project.application.heading :application="$application" />
-    <livewire:project.application.deployments :application="$application" :deployments="$deployments" :deployments_count="$deployments_count" />
-</x-layout>
