8d04fbdb74b55b4fa6a8ea0459e77ea079963291
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-12 11:25:20 +0100

    feat: search between resources

--- resources/views/livewire/project/resource/index.blade.php
+++ resources/views/livewire/project/resource/index.blade.php
@@ -44,54 +44,188 @@
         <a href="{{ route('project.resource.create', ['project_uuid' => request()->route('project_uuid'), 'environment_name' => request()->route('environment_name')]) }}  "
             class="items-center justify-center box">+ Add New Resource</a>
     @endif
-    <div class="grid gap-2 lg:grid-cols-2">
-        @foreach ($environment->applications->sortBy('name') as $application)
-            <a class="relative box group"
-                href="{{ route('project.application.configuration', [$project->uuid, $environment->name, $application->uuid]) }}">
-                <div class="flex flex-col mx-6">
-                    <div class="font-bold text-white">{{ $application->name }}</div>
-                    <div class="description">{{ $application->description }}</div>
-                </div>
-                @if (Str::of(data_get($application, 'status'))->startsWith('running'))
-                    <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
-                @elseif (Str::of(data_get($application, 'status'))->startsWith('exited'))
-                    <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
-                @elseif (Str::of(data_get($application, 'status'))->startsWith('restarting'))
-                    <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
-                @endif
-            </a>
-        @endforeach
-        @foreach ($environment->databases()->sortBy('name') as $database)
-            <a class="relative box group"
-                href="{{ route('project.database.configuration', [$project->uuid, $environment->name, $database->uuid]) }}">
-                <div class="flex flex-col mx-6">
-                    <div class="font-bold text-white">{{ $database->name }}</div>
-                    <div class="description">{{ $database->description }}</div>
-                </div>
-                @if (Str::of(data_get($database, 'status'))->startsWith('running'))
-                    <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
-                @elseif (Str::of(data_get($database, 'status'))->startsWith('exited'))
-                    <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
-                @elseif (Str::of(data_get($database, 'status'))->startsWith('restaring'))
-                    <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
-                @endif
-            </a>
-        @endforeach
-        @foreach ($environment->services->sortBy('name') as $service)
-            <a class="relative box group"
-                href="{{ route('project.service.configuration', [$project->uuid, $environment->name, $service->uuid]) }}">
-                <div class="flex flex-col mx-6">
-                    <div class="font-bold text-white">{{ $service->name }}</div>
-                    <div class="description">{{ $service->description }}</div>
-                </div>
-                @if (Str::of(serviceStatus($service))->startsWith('running'))
-                    <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
-                @elseif (Str::of(serviceStatus($service))->startsWith('degraded'))
-                    <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
-                @elseif (Str::of(serviceStatus($service))->startsWith('exited'))
-                    <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
-                @endif
-            </a>
-        @endforeach
+    <div x-data="searchComponent()">
+        <x-forms.input placeholder="Search for name, fqdn..." class="w-full" x-model="search" />
+        <div class="grid gap-2 pt-4 lg:grid-cols-2">
+            <template x-for="item in filteredApplications" :key="item.id">
+                <a class="relative box group" :href="item.hrefLink">
+                    <div class="flex flex-col mx-6">
+                        <div class="pb-2 font-bold text-white" x-text="item.name"></div>
+                        <div class="description" x-text="item.description"></div>
+                        <div class="description" x-text="item.fqdn"></div>
+                    </div>
+                    <template x-if="item.status.startsWith('running')">
+                        <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('exited')">
+                        <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('restarting')">
+                        <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                </a>
+            </template>
+            <template x-for="item in filteredPostgresqls" :key="item.id">
+                <a class="relative box group" :href="item.hrefLink">
+                    <div class="flex flex-col mx-6">
+                        <div class="font-bold text-white" x-text="item.name"></div>
+                        <div class="description" x-text="item.description"></div>
+                    </div>
+                    <template x-if="item.status.startsWith('running')">
+                        <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('exited')">
+                        <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('restarting')">
+                        <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                </a>
+            </template>
+            <template x-for="item in filteredRedis" :key="item.id">
+                <a class="relative box group" :href="item.hrefLink">
+                    <div class="flex flex-col mx-6">
+                        <div class="font-bold text-white" x-text="item.name"></div>
+                        <div class="description" x-text="item.description"></div>
+                    </div>
+                    <template x-if="item.status.startsWith('running')">
+                        <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('exited')">
+                        <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('restarting')">
+                        <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                </a>
+            </template>
+            <template x-for="item in filteredMongodbs" :key="item.id">
+                <a class="relative box group" :href="item.hrefLink">
+                    <div class="flex flex-col mx-6">
+                        <div class="font-bold text-white" x-text="item.name"></div>
+                        <div class="description" x-text="item.description"></div>
+                    </div>
+                    <template x-if="item.status.startsWith('running')">
+                        <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('exited')">
+                        <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('restarting')">
+                        <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                </a>
+            </template>
+            <template x-for="item in filteredMysqls" :key="item.id">
+                <a class="relative box group" :href="item.hrefLink">
+                    <div class="flex flex-col mx-6">
+                        <div class="font-bold text-white" x-text="item.name"></div>
+                        <div class="description" x-text="item.description"></div>
+                    </div>
+                    <template x-if="item.status.startsWith('running')">
+                        <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('exited')">
+                        <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('restarting')">
+                        <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                </a>
+            </template>
+            <template x-for="item in filteredMariadbs" :key="item.id">
+                <a class="relative box group" :href="item.hrefLink">
+                    <div class="flex flex-col mx-6">
+                        <div class="font-bold text-white" x-text="item.name"></div>
+                        <div class="description" x-text="item.description"></div>
+                    </div>
+                    <template x-if="item.status.startsWith('running')">
+                        <div class="absolute bg-success -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('exited')">
+                        <div class="absolute bg-error -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                    <template x-if="item.status.startsWith('restarting')">
+                        <div class="absolute bg-warning -top-1 -left-1 badge badge-xs"></div>
+                    </template>
+                </a>
+            </template>
+        </div>
     </div>
 </div>
+
+<script>
+    function searchComponent() {
+        return {
+            search: '',
+            applications: @js($applications),
+            postgresqls: @js($postgresqls),
+            redis: @js($redis),
+            mongodbs: @js($mongodbs),
+            mysqls: @js($mysqls),
+            mariadbs: @js($mariadbs),
+            get filteredApplications() {
+                if (this.search === '') {
+                    return this.applications;
+                }
+                this.applications = Object.values(this.applications);
+                return this.applications.filter(item => {
+                    return item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.fqdn?.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.description?.toLowerCase().includes(this.search.toLowerCase());
+                });
+            },
+            get filteredPostgresqls() {
+                if (this.search === '') {
+                    return this.postgresqls;
+                }
+                this.postgresqls = Object.values(this.postgresqls);
+                return this.postgresqls.filter(item => {
+                    return item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.description?.toLowerCase().includes(this.search.toLowerCase());
+                });
+            },
+            get filteredRedis() {
+                if (this.search === '') {
+                    return this.redis;
+                }
+                this.redis = Object.values(this.redis);
+                return this.redis.filter(item => {
+                    return item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.description?.toLowerCase().includes(this.search.toLowerCase());
+                });
+            },
+            get filteredMongodbs() {
+                if (this.search === '') {
+                    return this.mongodbs;
+                }
+                this.mongodbs = Object.values(this.mongodbs);
+                return this.mongodbs.filter(item => {
+                    return item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.description?.toLowerCase().includes(this.search.toLowerCase());
+                });
+            },
+            get filteredMysqls() {
+                if (this.search === '') {
+                    return this.mysqls;
+                }
+                this.mysqls = Object.values(this.mysqls);
+                return this.mysqls.filter(item => {
+                    return item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.description?.toLowerCase().includes(this.search.toLowerCase());
+                });
+            },
+            get filteredMariadbs() {
+                if (this.search === '') {
+                    return this.mariadbs;
+                }
+                this.mariadbs = Object.values(this.mariadbs);
+                return this.mariadbs.filter(item => {
+                    return item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+                        item.description?.toLowerCase().includes(this.search.toLowerCase());
+                });
+            },
+
+        };
+    }
+</script>
