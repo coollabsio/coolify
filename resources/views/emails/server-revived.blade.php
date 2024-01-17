8c60dd5523fb9497fb50cb3c462448ebf0a1eb79
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-17 11:21:47 +0100

    typo

--- resources/views/emails/server-revived.blade.php
+++ resources/views/emails/server-revived.blade.php
@@ -1,3 +1,3 @@
 <x-emails.layout>
-Your server ({{ $name }}) was offline for a while, but it is back online now. All automations & integrationsare turned on again.
+Your server ({{ $name }}) was offline for a while, but it is back online now. All automations & integrations are turned on again.
 </x-emails.layout>
