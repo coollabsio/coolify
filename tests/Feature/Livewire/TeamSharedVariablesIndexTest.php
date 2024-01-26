829e41f93ffc8414992ad415dd3cac23e223388c
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-26 11:12:07 +0100

    Delete TeamSharedVariablesIndexTest.php

--- tests/Feature/Livewire/TeamSharedVariablesIndexTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\TeamSharedVariablesIndex;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(TeamSharedVariablesIndex::class)
-        ->assertStatus(200);
-});
