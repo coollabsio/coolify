e8b539c3bdb41519bcda59f0e6629db6197b8fda
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-30 10:53:12 +0100

    Refactor CleanupUnreachableServers command to update server IP address

--- app/Console/Commands/CleanupUnreachableServers.php
+++ app/Console/Commands/CleanupUnreachableServers.php
@@ -16,7 +16,7 @@ class CleanupUnreachableServers extends Command
         $servers = Server::where('unreachable_count', 3)->where('unreachable_notification_sent', true)->where('updated_at', '<', now()->subDays(3))->get();
         if ($servers->count() > 0) {
             foreach ($servers as $server) {
-                ray("Cleanup unreachable server ($server->id) with name $server->name");
+                echo "Cleanup unreachable server ($server->id) with name $server->name";
                 $server->update([
                     'ip' => '1.2.3.4'
                 ]);
