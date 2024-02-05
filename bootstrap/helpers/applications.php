409ba8a1bb24f11f4efd99ec7dcce2fc9db79712
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-05 14:47:06 +0100

    Refactor application deployment logic

--- bootstrap/helpers/applications.php
+++ bootstrap/helpers/applications.php
@@ -40,21 +40,21 @@ function queue_application_deployment(Application $application, string $deployme
         'git_type' => $git_type
     ]);
 
-    // if ($no_questions_asked) {
-    //     $deployment->update([
-    //         'status' => ApplicationDeploymentStatus::IN_PROGRESS->value,
-    //     ]);
-    //     dispatch(new ApplicationDeploymentJob(
-    //         application_deployment_queue_id: $deployment->id,
-    //     ));
-    // } else if (next_queuable($server_id, $application_id)) {
-    //     $deployment->update([
-    //         'status' => ApplicationDeploymentStatus::IN_PROGRESS->value,
-    //     ]);
-    //     dispatch(new ApplicationDeploymentJob(
-    //         application_deployment_queue_id: $deployment->id,
-    //     ));
-    // }
+    if ($no_questions_asked) {
+        $deployment->update([
+            'status' => ApplicationDeploymentStatus::IN_PROGRESS->value,
+        ]);
+        dispatch(new ApplicationDeploymentJob(
+            application_deployment_queue_id: $deployment->id,
+        ));
+    } else if (next_queuable($server_id, $application_id)) {
+        $deployment->update([
+            'status' => ApplicationDeploymentStatus::IN_PROGRESS->value,
+        ]);
+        dispatch(new ApplicationDeploymentJob(
+            application_deployment_queue_id: $deployment->id,
+        ));
+    }
 }
 function force_start_deployment(ApplicationDeploymentQueue $deployment)
 {
