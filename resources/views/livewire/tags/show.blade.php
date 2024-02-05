d3eb87561e940aacd6aee4a24ba2815cb774e4aa
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-05 10:00:53 +0100

    Fix styling issue in tag links

--- resources/views/livewire/tags/show.blade.php
+++ resources/views/livewire/tags/show.blade.php
@@ -9,7 +9,7 @@
         <div class="flex flex-wrap gap-2 ">
             @forelse ($tags as $oneTag)
                 <a :class="{{ $tag->id == $oneTag->id }} && 'bg-coollabs hover:bg-coollabs-100'"
-                    class="flex items-center justify-center h-6 text-white min-w-14 w-fit hover:no-underline hover:bg-coolgray-200 bg-coolgray-100"
+                    class="flex items-center justify-center h-6 px-2 text-white min-w-14 w-fit hover:no-underline hover:bg-coolgray-200 bg-coolgray-100"
                     href="{{ route('tags.show', ['tag_name' => $oneTag->name]) }}">{{ $oneTag->name }}</a>
             @empty
                 <div>No tags yet defined yet. Go to a resource and add a tag there.</div>
