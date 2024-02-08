59c3cc6ce1ce9cfa2827f557684fc822934e690b
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-08 13:46:43 +0100

    Refactor admin authentication logic in Index component

--- app/Livewire/Admin/Index.php
+++ app/Livewire/Admin/Index.php
@@ -11,7 +11,7 @@ class Index extends Component
     public $users = [];
     public function mount()
     {
-        if (!isInstanceAdmin() && session('adminToken') === null) {
+        if (!isInstanceAdmin()) {
             return redirect()->route('dashboard');
         }
         $this->users = User::whereHas('teams', function ($query) {
