e0b0dda3826e83720c9b97cafc4d250560593fbc
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-16 22:04:26 +0100

    Remove unused code for displaying server resources

--- resources/views/livewire/server/delete.blade.php
+++ resources/views/livewire/server/delete.blade.php
@@ -15,40 +15,5 @@
                 This server will be deleted. It is not reversible. <br>Please think again.
             </x-new-modal>
         @endif
-        <div class="flex flex-col">
-            @forelse ($server->definedResources() as $resource)
-                @if ($loop->first)
-                    <h3 class="pt-4">Resources</h3>
-                @endif
-                @if ($resource->link())
-                    <a class="flex gap-2 p-1 hover:bg-coolgray-100 hover:no-underline" href="{{ $resource->link() }}">
-                        <div class="w-40">
-                            @if ($resource->type() === 'service')
-                                <x-status.services :service="$resource" />
-                            @else
-                                <x-status.index :status="$resource->status" :showRefreshButton="false" :showIcon="false" />
-                            @endif
-                        </div>
-                        <div>{{ $resource->name }}<span
-                                class="px-2 text-xs">({{ str($resource->type())->headline() }})</span></div>
-                    </a>
-                @else
-                    <div class="flex gap-2 p-1 hover:bg-coolgray-100 hover:no-underline">
-                        <div class="w-40">
-                            @if ($resource->type() === 'service')
-                                <x-status.services :service="$resource" />
-                            @else
-                                <x-status.index :status="$resource->status" :showRefreshButton="false" :showIcon="false" />
-                            @endif
-                        </div>
-                        <div>{{ $resource->name }}<span
-                                class="px-2 text-xs">({{ str($resource->type())->headline() }})</span></div>
-                    </div>
-                @endif
-            @empty
-            @endforelse
-        </div>
-    @else
-
     @endif
 </div>
