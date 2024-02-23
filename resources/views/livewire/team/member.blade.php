f931ebece8257be208f76ebba0ab4398ed612e26
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-23 12:34:36 +0100

    feat: make user owner fix: ownership check

--- resources/views/livewire/team/member.blade.php
+++ resources/views/livewire/team/member.blade.php
@@ -1,25 +1,47 @@
-<tr>
-    <td>
-        {{ $member->name }}</th>
-    <td>{{ $member->email }}</td>
-    <td>
-        {{ data_get($member, 'pivot.role') }}</td>
-    <td>
-        {{-- TODO: This is not good --}}
+<tr @class([
+    'text-white bg-coolblack hover:bg-coolgray-100',
+    'bg-coolgray-100' => $member->id == auth()->user()->id,
+])>
+    <td class="px-5 py-4 text-sm whitespace-nowrap">
+        {{ $member->name }}
+    </td>
+    <td class="px-5 py-4 text-sm whitespace-nowrap">
+        {{ $member->email }}
+    </td>
+    <td class="px-5 py-4 text-sm whitespace-nowrap">
+        {{ data_get($member, 'pivot.role') }}
+    </td>
+    <td class="px-5 py-4 text-sm whitespace-nowrap">
         @if (auth()->user()->isAdminFromSession())
             @if ($member->id !== auth()->user()->id)
-                @if (data_get($member, 'pivot.role') !== 'owner')
-                    @if (data_get($member, 'pivot.role') !== 'admin')
-                        <x-forms.button wire:click="makeAdmin">Convert to Admin</x-forms.button>
-                    @else
-                        <x-forms.button wire:click="makeReadonly">Convert to Member</x-forms.button>
+                @if (auth()->user()->isOwner())
+                    @if (data_get($member, 'pivot.role') === 'owner')
+                        <x-forms.button wire:click="makeAdmin">To Admin</x-forms.button>
+                        <x-forms.button wire:click="makeReadonly">To Member</x-forms.button>
+                        <x-forms.button isError wire:click="remove">Remove</x-forms.button>
                     @endif
-                    <x-forms.button wire:click="remove">Remove</x-forms.button>
-                @else
-                    <x-forms.button disabled>Remove</x-forms.button>
+                    @if (data_get($member, 'pivot.role') === 'admin')
+                        <x-forms.button wire:click="makeOwner">To Owner</x-forms.button>
+                        <x-forms.button wire:click="makeReadonly">To Member</x-forms.button>
+                        <x-forms.button isError wire:click="remove">Remove</x-forms.button>
+                    @endif
+                    @if (data_get($member, 'pivot.role') === 'member')
+                        <x-forms.button wire:click="makeOwner">To Owner</x-forms.button>
+                        <x-forms.button wire:click="makeAdmin">To Admin</x-forms.button>
+                        <x-forms.button isError wire:click="remove">Remove</x-forms.button>
+                    @endif
+                @elseif (auth()->user()->isAdmin())
+                    @if (data_get($member, 'pivot.role') === 'admin')
+                        <x-forms.button wire:click="makeReadonly">To Member</x-forms.button>
+                        <x-forms.button isError wire:click="remove">Remove</x-forms.button>
+                    @endif
+                    @if (data_get($member, 'pivot.role') === 'member')
+                        <x-forms.button wire:click="makeAdmin">To Admin</x-forms.button>
+                        <x-forms.button isError wire:click="remove">Remove</x-forms.button>
+                    @endif
                 @endif
             @else
-                <x-forms.button disabled>Remove</x-forms.button>
+                <div class="text-neutral-500">(This is you)</div>
             @endif
         @endif
     </td>
--- resources/views/livewire/team/member/index.blade.php
+++ resources/views/livewire/team/member/index.blade.php
@@ -1,29 +1,39 @@
 <div>
     <x-team.navbar />
     <h2>Members</h2>
-    <div class="pt-4 overflow-hidden">
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach (currentTeam()->members->sortBy('name') as $member)
-                    <livewire:team.member :member="$member" :wire:key="$member->id" />
-                @endforeach
-            </tbody>
-        </table>
+
+    <div class="flex flex-col">
+        <div class="flex flex-col">
+            <div class="overflow-x-auto">
+                <div class="inline-block min-w-full">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full divide-y divide-coolgray-400">
+                            <thead>
+                                <tr class="text-neutral-500">
+                                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Name
+                                    </th>
+                                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Email</th>
+                                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Role</th>
+                                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-coolgray-400">
+                                @foreach (currentTeam()->members as $member)
+                                    <livewire:team.member :member="$member" :wire:key="$member->id" />
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     @if (auth()->user()->isAdminFromSession())
         <div class="py-4">
             @if (is_transactional_emails_active())
-                <h3 class="pb-4">Invite a new member</h3>
+                <h2 class="pb-4">Invite New Member</h2>
             @else
-                <h3>Invite a new member</h3>
+                <h2>Invite New Member</h2>
                 @if (isInstanceAdmin())
                     <div class="pb-4 text-xs text-warning">You need to configure (as root team) <a href="/settings#smtp"
                             class="underline text-warning">Transactional
