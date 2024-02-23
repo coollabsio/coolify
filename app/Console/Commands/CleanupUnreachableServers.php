61dbc81765a0e6a884e4161690626ee76cd86dc9
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-23 21:51:43 +0100

    feat: delay container/server jobs

--- app/Console/Commands/CleanupUnreachableServers.php
+++ app/Console/Commands/CleanupUnreachableServers.php
@@ -8,15 +8,16 @@ use Illuminate\Console\Command;
 class CleanupUnreachableServers extends Command
 {
     protected $signature = 'cleanup:unreachable-servers';
-    protected $description = 'Cleanup Unreachable Servers (3 days)';
+    protected $description = 'Cleanup Unreachable Servers (7 days)';
 
     public function handle()
     {
         echo "Running unreachable server cleanup...\n";
-        $servers = Server::where('unreachable_count', 3)->where('unreachable_notification_sent', true)->where('updated_at', '<', now()->subDays(3))->get();
+        $servers = Server::where('unreachable_count', 3)->where('unreachable_notification_sent', true)->where('updated_at', '<', now()->subDays(7))->get();
         if ($servers->count() > 0) {
             foreach ($servers as $server) {
                 echo "Cleanup unreachable server ($server->id) with name $server->name";
+                send_internal_notification("Server $server->name is unreachable for 7 days. Cleaning up...");
                 $server->update([
                     'ip' => '1.2.3.4'
                 ]);
