6fe791c1f1f2012427fa3c862c1737ad1ad98b58
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-01 11:43:42 +0100

    fix: pull request deployments + build servers

--- resources/views/livewire/project/application/general.blade.php
+++ resources/views/livewire/project/application/general.blade.php
@@ -8,7 +8,8 @@
             @if ($isConfigurationChanged && !is_null($application->config_hash) && !$application->isExited())
                 <div title="Configuration not applied to the running application. You need to redeploy.">
                     <svg class="w-6 h-6 text-warning" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
-                        <path fill="currentColor" d="M240.26 186.1L152.81 34.23a28.74 28.74 0 0 0-49.62 0L15.74 186.1a27.45 27.45 0 0 0 0 27.71A28.31 28.31 0 0 0 40.55 228h174.9a28.31 28.31 0 0 0 24.79-14.19a27.45 27.45 0 0 0 .02-27.71m-20.8 15.7a4.46 4.46 0 0 1-4 2.2H40.55a4.46 4.46 0 0 1-4-2.2a3.56 3.56 0 0 1 0-3.73L124 46.2a4.77 4.77 0 0 1 8 0l87.44 151.87a3.56 3.56 0 0 1 .02 3.73M116 136v-32a12 12 0 0 1 24 0v32a12 12 0 0 1-24 0m28 40a16 16 0 1 1-16-16a16 16 0 0 1 16 16"/>
+                        <path fill="currentColor"
+                            d="M240.26 186.1L152.81 34.23a28.74 28.74 0 0 0-49.62 0L15.74 186.1a27.45 27.45 0 0 0 0 27.71A28.31 28.31 0 0 0 40.55 228h174.9a28.31 28.31 0 0 0 24.79-14.19a27.45 27.45 0 0 0 .02-27.71m-20.8 15.7a4.46 4.46 0 0 1-4 2.2H40.55a4.46 4.46 0 0 1-4-2.2a3.56 3.56 0 0 1 0-3.73L124 46.2a4.77 4.77 0 0 1 8 0l87.44 151.87a3.56 3.56 0 0 1 .02 3.73M116 136v-32a12 12 0 0 1 24 0v32a12 12 0 0 1-24 0m28 40a16 16 0 1 1-16-16a16 16 0 0 1 16 16" />
                     </svg>
                 </div>
             @endif
@@ -100,11 +101,15 @@
                             <x-forms.input id="application.docker_registry_image_tag" label="Docker Image Tag" />
                         @endif
                     @else
-                        @if ($application->destination->server->isSwarm() || $application->additional_servers->count() > 0)
-                            <x-forms.input id="application.docker_registry_image_name" required label="Docker Image" />
+                        @if (
+                            $application->destination->server->isSwarm() ||
+                                $application->additional_servers->count() > 0 ||
+                                $application->settings->is_build_server_enabled)
+                            <x-forms.input id="application.docker_registry_image_name" required label="Docker Image"
+                                placeholder="Required!" />
                             <x-forms.input id="application.docker_registry_image_tag"
                                 helper="If set, it will tag the built image with this tag too. <br><br>Example: If you set it to 'latest', it will push the image with the commit sha tag + with the latest tag."
-                                label="Docker Image Tag" />
+                                placeholder="Empty means latest will be used." label="Docker Image Tag" />
                         @else
                             <x-forms.input id="application.docker_registry_image_name"
                                 helper="Empty means it won't push the image to a docker registry."
