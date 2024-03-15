657c7d8cff3775359bbe26fb9774d3887b0ce0c8
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-15 20:40:50 +0100

    Refactor form components
    Update Input and Textarea components to use nullable type declarations and remove unused imports.

--- app/View/Components/Forms/Textarea.php
+++ app/View/Components/Forms/Textarea.php
@@ -4,7 +4,6 @@ namespace App\View\Components\Forms;
 
 use Closure;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Str;
 use Illuminate\View\Component;
 use Visus\Cuid2\Cuid2;
 
@@ -14,18 +13,19 @@ class Textarea extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string|null $id = null,
-        public string|null $name = null,
-        public string|null $type = 'text',
-        public string|null $value = null,
-        public string|null $label = null,
-        public string|null $placeholder = null,
-        public bool        $required = false,
-        public bool        $disabled = false,
-        public bool        $readonly = false,
-        public string|null $helper = null,
-        public bool        $realtimeValidation = false,
-        public string      $defaultClass = "textarea leading-normal bg-coolgray-100 rounded text-white scrollbar disabled:bg-coolgray-200/50 disabled:border-none placeholder:text-coolgray-500 read-only:text-neutral-500 read-only:bg-coolgray-200/50"
+        public ?string $id = null,
+        public ?string $name = null,
+        public ?string $type = 'text',
+        public ?string $value = null,
+        public ?string $label = null,
+        public ?string $placeholder = null,
+        public bool    $required = false,
+        public bool    $disabled = false,
+        public bool    $readonly = false,
+        public ?string $helper = null,
+        public bool    $realtimeValidation = false,
+        // public bool    $allowToPeak = true,
+        public string  $defaultClass = "textarea leading-normal bg-coolgray-100 rounded text-white scrollbar disabled:bg-coolgray-200/50 disabled:border-none placeholder:text-coolgray-500 read-only:text-neutral-500 read-only:bg-coolgray-200/50"
     ) {
         //
     }
