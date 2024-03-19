289480c95478a28408159e5b50b82e08ddf9af0b
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-19 10:24:23 +0100

    feat: able to run scheduler/horizon programatically

---
+++ app/Console/Commands/Scheduler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Scheduler extends Command
+{
+    protected $signature = 'start:scheduler';
+    protected $description = 'Start Scheduler';
+    public function handle()
+    {
+        if (config('coolify.is_scheduler_enabled')) {
+            $this->info('Scheduler is enabled. Starting.');
+            $this->call('schedule:work');
+            exit(0);
+        } else {
+            exit(0);
+        }
+    }
+}
