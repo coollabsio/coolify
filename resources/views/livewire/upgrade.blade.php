8f66a41c096e64a09437215b6bd6ac016821f21f
Andras Bacsai <andras.bacsai@gmail.com>
2024-03-21 08:41:23 +0100

    Remove unnecessary button code in upgrade.blade.php

--- resources/views/livewire/upgrade.blade.php
+++ resources/views/livewire/upgrade.blade.php
@@ -25,18 +25,5 @@
                 Upgrade
             @endif
         </button>
-    @else
-        <button class="menu-item">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 transition-colors text-neutral-700"
-                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path
-                    d="M9 12h-3.586a1 1 0 0 1 -.707 -1.707l6.586 -6.586a1 1 0 0 1 1.414 0l6.586 6.586a1 1 0 0 1 -.707 1.707h-3.586v3h-6v-3z" />
-                <path d="M9 21h6" />
-                <path d="M9 18h6" />
-            </svg>
-            <div class="w-20"></div>
-        </button>
     @endif
 </div>
