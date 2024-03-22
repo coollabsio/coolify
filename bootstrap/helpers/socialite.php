cae9ae51ad52914b8ab4dd112a1258192999cb06
Pat Rocchio <procchio6@gmail.com>
2024-03-22 15:22:27 -0400

    Fix azure oauth not recognizing tenant ID
    Co-authored-by: Suraj Kumar <srjkmr1024@gmail.com>
    Co-authored-by: Michael Castanieto <mcastanieto@gmail.com>
    Co-authored-by: Mike Kim <m.kim4247@gmail.com>

--- bootstrap/helpers/socialite.php
+++ bootstrap/helpers/socialite.php
@@ -6,19 +6,30 @@ use Laravel\Socialite\Facades\Socialite;
 function get_socialite_provider(string $provider)
 {
     $oauth_setting = OauthSetting::firstWhere('provider', $provider);
+
+    if ($provider == 'azure') {
+        $azure_config = new \SocialiteProviders\Manager\Config(
+            $oauth_setting->client_id,
+            $oauth_setting->client_secret,
+            $oauth_setting->redirect_uri,
+            ['tenant' => $oauth_setting->tenant],
+          );
+        return Socialite::driver('azure')->setConfig($azure_config);
+    }
+
     $config = [
         'client_id' => $oauth_setting->client_id,
         'client_secret' => $oauth_setting->client_secret,
         'redirect' => $oauth_setting->redirect_uri,
-        'tenant' => $oauth_setting->tenant,
     ];
+
     $provider_class_map = [
-        'azure' => \SocialiteProviders\Azure\Provider::class,
         'bitbucket' => \Laravel\Socialite\Two\BitbucketProvider::class,
         'github' => \Laravel\Socialite\Two\GithubProvider::class,
         'gitlab' => \Laravel\Socialite\Two\GitlabProvider::class,
         'google' => \Laravel\Socialite\Two\GoogleProvider::class,
     ];
+
     return Socialite::buildProvider(
         $provider_class_map[$provider],
         $config
