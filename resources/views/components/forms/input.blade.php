2644efd9f724352a08492da3ce942eced062879e
Andras Bacsai <andras.bacsai@gmail.com>
2024-04-29 12:54:36 +0200

    Update input.blade.php and health-checks.blade.php

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -41,6 +41,7 @@
             wire:dirty.class.remove='dark:focus:ring-coolgray-300 dark:ring-coolgray-300'
             wire:dirty.class="dark:focus:ring-warning dark:ring-warning" wire:loading.attr="disabled"
             type="{{ $type }}" @disabled($disabled)
+            min="{{ $attributes->get('min') }}" max="{{ $attributes->get('max') }}"
             @if ($id !== 'null') id={{ $id }} @endif name="{{ $name }}"
             placeholder="{{ $attributes->get('placeholder') }}">
     @endif
--- resources/views/livewire/project/shared/health-checks.blade.php
+++ resources/views/livewire/project/shared/health-checks.blade.php
@@ -10,22 +10,24 @@
         </div>
         <div class="flex gap-2">
             <x-forms.input id="resource.health_check_method" placeholder="GET" label="Method" required />
-
             <x-forms.input id="resource.health_check_scheme" placeholder="http" label="Scheme" required />
             <x-forms.input id="resource.health_check_host" placeholder="localhost" label="Host" required />
-            <x-forms.input id="resource.health_check_port"
+            <x-forms.input type="number" id="resource.health_check_port"
                 helper="If no port is defined, the first exposed port will be used." placeholder="80" label="Port" />
             <x-forms.input id="resource.health_check_path" placeholder="/health" label="Path" required />
         </div>
         <div class="flex gap-2">
-            <x-forms.input id="resource.health_check_return_code" placeholder="200" label="Return Code" required />
+            <x-forms.input type="number" id="resource.health_check_return_code" placeholder="200" label="Return Code" required />
             <x-forms.input id="resource.health_check_response_text" placeholder="OK" label="Response Text" />
         </div>
         <div class="flex gap-2">
-            <x-forms.input id="resource.health_check_interval" placeholder="30" label="Interval" required />
-            <x-forms.input id="resource.health_check_timeout" placeholder="30" label="Timeout" required />
-            <x-forms.input id="resource.health_check_retries" placeholder="3" label="Retries" required />
-            <x-forms.input id="resource.health_check_start_period" placeholder="30" label="Start Period" required />
+            <x-forms.input min=1 type="number" id="resource.health_check_interval" placeholder="30" label="Interval"
+                required />
+            <x-forms.input type="number" id="resource.health_check_timeout" placeholder="30" label="Timeout"
+                required />
+            <x-forms.input type="number" id="resource.health_check_retries" placeholder="3" label="Retries" required />
+            <x-forms.input min=1 type="number" id="resource.health_check_start_period" placeholder="30"
+                label="Start Period" required />
         </div>
     </div>
 </form>
