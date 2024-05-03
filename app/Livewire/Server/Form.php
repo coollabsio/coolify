6b81eaa65d0904e2c698214d1b3b92469193d114
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-03 10:54:46 +0200

    Fix server form to dispatch 'proxyStatusUpdated' event when server is reachable

--- app/Livewire/Server/Form.php
+++ app/Livewire/Server/Form.php
@@ -82,6 +82,7 @@ class Form extends Component
             $this->server->settings->is_reachable = true;
             $this->server->settings->is_usable = true;
             $this->server->settings->save();
+            $this->dispatch('proxyStatusUpdated');
         } else {
             $this->dispatch('error', 'Server is not reachable.', 'Please validate your configuration and connection.<br><br>Check this <a target="_blank" class="underline" href="https://coolify.io/docs/knowledge-base/server/openssh">documentation</a> for further help. <br><br>Error: ' . $error);
             return;
