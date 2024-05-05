d52aac76c08015a397725757d44ce50899a6d5e3
Francesco Bruno <59353843+FrancescoBrunoDev@users.noreply.github.com>
2024-05-05 15:55:58 +0000

    Changed the breadcrumbs for let it using flex-wrap

--- resources/views/livewire/project/environment-edit.blade.php
+++ resources/views/livewire/project/environment-edit.blade.php
@@ -6,22 +6,30 @@
             <livewire:project.delete-environment :disabled="!$environment->isEmpty()" :environment_id="$environment->id" />
         </div>
         <nav class="flex pt-2 pb-10">
-            <ol class="flex items-center">
+            <ol class="flex items-center flex-wrap gap-y-1">
                 <li class="inline-flex items-center">
-                    <a class="text-xs truncate lg:text-sm"
-                        href="{{ route('project.show', ['project_uuid' => data_get($parameters, 'project_uuid')]) }}">
-                        {{ $project->name }}</a>
-                </li>
-                <li>
                     <div class="flex items-center">
+                        <a class="text-xs truncate lg:text-sm"
+                            href="{{ route('project.show', ['project_uuid' => data_get($parameters, 'project_uuid')]) }}">
+                            {{ $project->name }}</a>
                         <svg aria-hidden="true" class="w-4 h-4 mx-1 font-bold dark:text-warning" fill="currentColor"
                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
                                 d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                 clip-rule="evenodd"></path>
                         </svg>
+                    </div>
+                </li>
+                <li>
+                    <div class="flex items-center">
                         <a class="text-xs truncate lg:text-sm"
                             href="{{ route('project.resource.index', ['environment_name' => data_get($parameters, 'environment_name'), 'project_uuid' => data_get($parameters, 'project_uuid')]) }}">{{ data_get($parameters, 'environment_name') }}</a>
+                        <svg aria-hidden="true" class="w-4 h-4 mx-1 font-bold dark:text-warning" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"></path>
+                        </svg>
                     </div>
                 </li>
                 <li>
