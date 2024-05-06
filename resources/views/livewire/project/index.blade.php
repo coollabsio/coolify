5e44a61068391be7f558a1111bfc3669d07509f6
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-06 14:33:45 +0200

    chore: Improve menu item styling and spacing in project index and show views

--- resources/views/livewire/project/index.blade.php
+++ resources/views/livewire/project/index.blade.php
@@ -8,15 +8,14 @@
     <div class="subtitle">All your projects are here.</div>
     <div class="grid gap-2 lg:grid-cols-2">
         @forelse ($projects as $project)
-            <div class="box group" x-data
-                x-on:click="goto('{{ $project->uuid }}')">
+            <div class="box group" x-data x-on:click="goto('{{ $project->uuid }}')">
                 <a class="flex flex-col justify-center flex-1 mx-6"
                     href="{{ route('project.show', ['project_uuid' => data_get($project, 'uuid')]) }}">
                     <div class="box-title">{{ $project->name }}</div>
                     <div class="box-description ">
                         {{ $project->description }}</div>
                 </a>
-                <div class="flex items-center text-xs">
+                <div class="flex items-center justify-center gap-2 pt-4 pb-2 mr-4 text-xs lg:py-0 lg:justify-normal">
                     <a class="mx-4 font-bold hover:underline"
                         href="{{ route('project.edit', ['project_uuid' => data_get($project, 'uuid')]) }}">
                         Settings
