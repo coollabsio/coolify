5df443a0167cce3d74954ba42699caf44efa614e
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-08 14:48:04 +0200

    Refactor StartSentinel.php for improved readability and maintainability

--- app/Actions/Server/StartSentinel.php
+++ app/Actions/Server/StartSentinel.php
@@ -10,6 +10,6 @@ class StartSentinel
     use AsAction;
     public function handle(Server $server)
     {
-        return instant_remote_process(['docker run --pull always -d --name coolify-sentinel --restart always -p 127.0.0.1:8888:8888 -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify/metrics:/var/www/html/storage/app/metrics --pid host --health-cmd "curl --fail http://127.0.0.1:8888/api/health || exit 1" --health-interval 10s --health-retries 3 ghcr.io/coollabsio/sentinel:latest'], $server, false);
+        return instant_remote_process(['docker run --rm --pull always -d --name coolify-sentinel --restart always -p 127.0.0.1:8888:8888 -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify/metrics:/var/www/html/storage/app/metrics --pid host --health-cmd "curl --fail http://127.0.0.1:8888/api/health || exit 1" --health-interval 10s --health-retries 3 ghcr.io/coollabsio/sentinel:latest'], $server, false);
     }
 }
