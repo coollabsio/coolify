1026f1efa57db4531a8797938edefa7c1e56fc58
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-09 12:30:51 +0200

    refactor: add SCHEDULER environment variable to StartSentinel.php

--- app/Actions/Server/StartSentinel.php
+++ app/Actions/Server/StartSentinel.php
@@ -14,7 +14,7 @@ class StartSentinel
             instant_remote_process(['docker rm -f coolify-sentinel'], $server, false);
         }
         instant_remote_process([
-            "docker run --rm --pull always -d --name coolify-sentinel -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify/metrics:/app/metrics -v /data/coolify/logs:/app/logs --pid host --health-cmd \"curl --fail http://127.0.0.1:8888/api/health || exit 1\" --health-interval 10s --health-retries 3 ghcr.io/coollabsio/sentinel:$version",
+            "docker run --rm --pull always -d -e \"SCHEDULER=true\" --name coolify-sentinel -v /var/run/docker.sock:/var/run/docker.sock -v /data/coolify/metrics:/app/metrics -v /data/coolify/logs:/app/logs --pid host --health-cmd \"curl --fail http://127.0.0.1:8888/api/health || exit 1\" --health-interval 10s --health-retries 3 ghcr.io/coollabsio/sentinel:$version",
             "chown -R 9999:root /data/coolify/metrics /data/coolify/logs",
             "chmod -R 600 /data/coolify/metrics /data/coolify/logs"
         ], $server, false);
