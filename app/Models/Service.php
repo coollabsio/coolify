32ff3461541279fdc5db32dcdf460e99049e9d91
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-15 09:46:28 +0200

    chore: Refactor Service.php to handle missing admin user in extraFields() method

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -450,14 +450,16 @@ class Service extends BaseModel
                     $data = collect([]);
                     $admin_user = $this->environment_variables()->where('key', 'SERVICE_USER_ADMIN')->first();
                     $admin_password = $this->environment_variables()->where('key', 'SERVICE_PASSWORD_ADMIN')->first();
-                    $data = $data->merge([
-                        'User' => [
-                            'key' => 'SERVICE_USER_ADMIN',
-                            'value' => data_get($admin_user, 'value', 'admin'),
-                            'readonly' => true,
-                            'rules' => 'required',
-                        ],
-                    ]);
+                    if ($admin_user) {
+                        $data = $data->merge([
+                            'User' => [
+                                'key' => 'SERVICE_USER_ADMIN',
+                                'value' => data_get($admin_user, 'value', 'admin'),
+                                'readonly' => true,
+                                'rules' => 'required',
+                            ],
+                        ]);
+                    }
                     if ($admin_password) {
                         $data = $data->merge([
                             'Password' => [
