684745902241eaa49180d97636ed0db4293b8f5b
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-16 13:33:35 +0200

    chore: Limit commit message length to 50 characters in ApplicationDeploymentJob

--- app/Jobs/ApplicationDeploymentJob.php
+++ app/Jobs/ApplicationDeploymentJob.php
@@ -1179,8 +1179,11 @@ class ApplicationDeploymentJob implements ShouldQueue, ShouldBeEncrypted
             ]
         );
         if ($this->saved_outputs->get('commit_message')) {
-            $this->application_deployment_queue->commit_message = $this->saved_outputs->get('commit_message');
-            ApplicationDeploymentQueue::whereCommit($this->commit)->whereApplicationId($this->application->id)->update(['commit_message' => $this->saved_outputs->get('commit_message')]);
+            $commit_message = str($this->saved_outputs->get('commit_message'))->limit(50);
+            $this->application_deployment_queue->commit_message = $commit_message->value();
+            ApplicationDeploymentQueue::whereCommit($this->commit)->whereApplicationId($this->application->id)->update(
+                ['commit_message' => $commit_message->value()]
+            );
             $this->application_deployment_queue->save();
         }
     }
