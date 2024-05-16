57a026a7a14f93bf21026a6c3f06508d3bbd8ada
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-16 09:39:29 +0200

    Refactor modal input button title for dynamic configuration navbar

--- resources/views/livewire/server/proxy/dynamic-configuration-navbar.blade.php
+++ resources/views/livewire/server/proxy/dynamic-configuration-navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="flex gap-2">
     <h3 class="dark:text-white">File: {{ str_replace('|', '.', $fileName) }}</h3>
     <div class="flex gap-2">
-        <x-modal-input buttonTitle="+ Add" title="Edit Configuration">
+        <x-modal-input buttonTitle="Edit" title="Edit Configuration">
             <livewire:server.proxy.new-dynamic-configuration :server_id="$server_id" :fileName="$fileName" :value="$value"
                 :newFile="$newFile" wire:key="{{ $fileName }}" />
         </x-modal-input>
