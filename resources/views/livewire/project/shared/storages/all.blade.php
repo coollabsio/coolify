ad5c1639e85983e871c5325b0b590026dab01ac4
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-27 15:11:00 +0200

    fix: do not allow service storage mount point modifications

--- resources/views/livewire/project/shared/storages/all.blade.php
+++ resources/views/livewire/project/shared/storages/all.blade.php
@@ -3,10 +3,11 @@
         @foreach ($resource->persistentStorages as $storage)
             @if ($resource->type() === 'service')
                 <livewire:project.shared.storages.show wire:key="storage-{{ $storage->id }}" :storage="$storage"
-                    :isFirst="$loop->first" isReadOnly='true' />
+                    :isFirst="$loop->first" isReadOnly='true' isService='true' />
             @else
                 <livewire:project.shared.storages.show wire:key="storage-{{ $storage->id }}" :storage="$storage"
-                    isReadOnly="{{ data_get($storage, 'is_readonly') }}" />
+                    isReadOnly="{{ data_get($storage, 'is_readonly') }}"
+                    startedAt="{{ data_get($resource, 'started_at') }}" />
             @endif
         @endforeach
     </div>
