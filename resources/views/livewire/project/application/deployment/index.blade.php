e16bd194a3c2bea10cbeeef37ca78ed28584f3b8
Esteban Ecallard <100437198+EstebX@users.noreply.github.com>
2024-05-29 12:35:32 +0000

    fix: removal of the failed deployment condition, addition of since started instead of finished time

--- resources/views/livewire/project/application/deployment/index.blade.php
+++ resources/views/livewire/project/application/deployment/index.blade.php
@@ -92,15 +92,11 @@
                     <div>
                         @if ($deployment->status !== 'in_progress')
                             Finished <span x-text="measure_since_started()">0s</span> in
-                            @if ($deployment->status !== 'failed')
-                                <span class="font-bold" x-text="measure_finished_time()">0s</span>
-                            @else
-                                <span class="font-bold">0s</span>
-                            @endif
+                            <span class="font-bold" x-text="measure_finished_time()">0s</span>
                         @else
                             Running for
                         @endif
-                        <span class="font-bold" x-text="measure_finished_time()">0s</span>
+                        <span class="font-bold" x-text="measure_since_started()">0s</span>
                     </div>
                 </div>
             </div>
