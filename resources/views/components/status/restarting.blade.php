edacfcdec71b71e1bddf6591e7a96c399bc83722
Andras Bacsai <andras.bacsai@gmail.com>
2024-05-29 10:02:01 +0200

    Update status component links to open in a new tab

--- resources/views/components/status/restarting.blade.php
+++ resources/views/components/status/restarting.blade.php
@@ -9,7 +9,7 @@
         <div class="badge badge-warning "></div>
         <div class="pl-2 pr-1 text-xs font-bold tracking-wider dark:text-warning" @if($lastDeploymentInfo) title="{{$lastDeploymentInfo}}" @endif>
             @if ($lastDeploymentLink)
-              <a href="{{ $lastDeploymentLink }}" class="underline cursor-pointer">
+              <a href="{{ $lastDeploymentLink }}" target="_blank" class="underline cursor-pointer">
                   {{ str($status)->before(':')->headline() }}
               </a>
           @else
