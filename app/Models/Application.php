f332a73122888573ca83f33f85a67c5e1c5f413b
Andras Bacsai <andras.bacsai@gmail.com>
2024-06-12 12:05:08 +0200

    feat: cancelling a deployment will check if new could be started.

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -531,7 +531,7 @@ class Application extends BaseModel
 
     public function get_last_successful_deployment()
     {
-        return ApplicationDeploymentQueue::where('application_id', $this->id)->where('status', 'finished')->where('pull_request_id', 0)->orderBy('created_at', 'desc')->first();
+        return ApplicationDeploymentQueue::where('application_id', $this->id)->where('status', ApplicationDeploymentStatus::FINISHED)->where('pull_request_id', 0)->orderBy('created_at', 'desc')->first();
     }
 
     public function get_last_days_deployments()
