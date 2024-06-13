fc7ba76e6a2d18529580399b66ca748a6de60183
Andras Bacsai <andras.bacsai@gmail.com>
2024-06-13 10:18:35 +0200

    fix: in services should edit compose file for volumes and envs

--- resources/views/livewire/project/service/configuration.blade.php
+++ resources/views/livewire/project/service/configuration.blade.php
@@ -172,7 +172,8 @@
                     <h2>Storages</h2>
                 </div>
                 <div class="pb-4">Persistent storage to preserve data between deployments.</div>
-                <span class="dark:text-warning">Please modify storage layout in your Docker Compose file.</span>
+                <div class="pb-4 dark:text-warning text-coollabs">If you would like to add a volume, you must add it to
+                    your compose file (General tab).</div>
                 @foreach ($applications as $application)
                     <livewire:project.service.storage wire:key="application-{{ $application->id }}"
                         :resource="$application" />
