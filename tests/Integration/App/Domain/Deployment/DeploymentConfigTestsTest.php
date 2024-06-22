f91d4b50607c116141ff4caae5fbc7c36176f6f9
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-22 10:08:12 +0200

    WIP

--- tests/Integration/App/Domain/Deployment/DeploymentConfigTestsTest.php
+++ tests/Integration/App/Domain/Deployment/DeploymentConfigTestsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Domain\Deployment\DeploymentContext;
+use App\Models\Application;
 use App\Models\ApplicationDeploymentQueue;
 use App\Services\Deployment\DeploymentProvider;
 use App\Services\Docker\DockerProvider;
@@ -11,6 +12,33 @@ it('can create an instance of a deploymentcontext', function () {
     $context = getContextForApplicationDeployment($deploymentQueue);
 
     expect($context)->toBeInstanceOf(DeploymentContext::class);
+});
+
+it('can fetch the application instance from a deployment context', function () {
+    $deploymentQueue = ApplicationDeploymentQueue::factory()->create();
+
+    $context = getContextForApplicationDeployment($deploymentQueue);
+
+    expect($context->getApplication())->toBeInstanceOf(Application::class)
+        ->and($context->getApplication()->id)->toBe($deploymentQueue->application_id);
+
+});
+
+it('can fetch the generate git import commands', function () {
+    $deploymentQueue = ApplicationDeploymentQueue::factory()->create();
+
+    $context = getContextForApplicationDeployment($deploymentQueue);
+
+    $commands = $context->generateGitImportCommands();
+
+    $id = $deploymentQueue->deployment_uuid;
+    expect($commands['fullRepoUrl'])
+        ->toBe('coollabsio/coolify')
+        ->and($commands['branch'])
+        ->toBe('master');
+
+    // TODO: Check $commands['commands'] for the correct git commands
+
 });
 
 function getContextForApplicationDeployment(ApplicationDeploymentQueue $applicationDeploymentQueue): DeploymentContext
