bd3be8cff19eb62e0d4d679077074d1d9b24db96
Thijmen Stavenuiter <thijmenstavenuiter@gmail.com>
2024-06-23 13:15:33 +0200

    Add tests for simple docker deployment

--- tests/Integration/App/Jobs/ExperimentalDeploymentJob/SimpleDockerfileDeploymentTest.php
+++ tests/Integration/App/Jobs/ExperimentalDeploymentJob/SimpleDockerfileDeploymentTest.php
@@ -11,7 +11,7 @@ use App\Services\Deployment\DeploymentProvider;
 use App\Services\Docker\DockerProvider;
 use Illuminate\Support\Facades\Http;
 
-it('should be able to deploy a Dockerfile project', function () {
+it('should be able to deploy a Simple Dockerfile project', function () {
 
     $dockerHostIp = gethostbyname('host.docker.internal');
     // TODO: Ensure that no mails are being send
@@ -38,16 +38,13 @@ it('should be able to deploy a Dockerfile project', function () {
     $domainNameInDocker = "http://nginx-testing.{$dockerHostIp}.sslip.io";
 
     $application = Application::factory()->create([
-        'name' => 'NodeJS Fastify Example',
+        'name' => 'Simple Pure Dockerfile Deployment',
         'fqdn' => "{$domainNameInDocker},http://nginx-testing.127.0.0.1.sslip.io",
         'git_repository' => 'coollabsio/coolify-examples',
         'git_branch' => 'main',
         'git_commit_sha' => 'HEAD',
         'build_pack' => 'dockerfile',
         'ports_exposes' => '80',
-        'dockerfile' => null,
-        'source_type' => GithubApp::class,
-        'source_id' => $publicGitHub->id,
         'destination_type' => StandaloneDocker::class,
         'destination_id' => $destination->id,
     ]);
@@ -65,46 +62,44 @@ it('should be able to deploy a Dockerfile project', function () {
     // Add user to team, otherwise exception in app/Notifications/Channels/EmailChannel.php:18
     $user = User::factory()->create();
 
-    //    $team = $application->environment->project->team;
-    //
-    //    $user->teams()->attach($team, ['role' => 'admin']);
-    //
-    //    $applicationDeploymentQueue = ApplicationDeploymentQueue::factory()
-    //        ->create([
-    //            'application_id' => $application->id,
-    //            'application_name' => $application->name,
-    //            'server_id' => $server->id,
-    //            'server_name' => $server->name,
-    //            'destination_id' => $destination->id,
-    //            'force_rebuild' => true,
-    //            'is_webhook' => false,
-    //            'commit' => 'HEAD',
-    //        ]);
-    //
-    //    $job = new ExperimentalDeploymentJob($applicationDeploymentQueue->id);
-    //
-    //    expect($job)->toBeInstanceOf(ExperimentalDeploymentJob::class);
-    //
-    //    $dockerProvider = $this->app->make(DockerProvider::class);
-    //
-    //    expect($dockerProvider)
-    //        ->toBeInstanceOf(DockerProvider::class);
-    //
-    //    $deploymentProvider = $this->app->make(DeploymentProvider::class);
-    //
-    //    expect($deploymentProvider)
-    //        ->toBeInstanceOf(DeploymentProvider::class);
-    //
-    //    $job->handle($dockerProvider, $deploymentProvider);
-    //
-    //    assertUrlStatus($domainNameInDocker, 200);
-    //
-    //    $content = Http::get($domainNameInDocker)->json();
-    //    expect($content)
-    //        ->toHaveKey('hello')
-    //        ->and($content['hello'])
-    //        ->toBe('from nodejs');
+    $team = $application->environment->project->team;
 
+    $user->teams()->attach($team, ['role' => 'admin']);
+
+    $applicationDeploymentQueue = ApplicationDeploymentQueue::factory()
+        ->create([
+            'application_id' => $application->id,
+            'application_name' => $application->name,
+            'server_id' => $server->id,
+            'server_name' => $server->name,
+            'destination_id' => $destination->id,
+            'force_rebuild' => true,
+            'is_webhook' => false,
+            'commit' => 'HEAD',
+        ]);
+
+    $job = new ExperimentalDeploymentJob($applicationDeploymentQueue->id);
+
+    expect($job)->toBeInstanceOf(ExperimentalDeploymentJob::class);
+
+    $dockerProvider = $this->app->make(DockerProvider::class);
+
+    expect($dockerProvider)
+        ->toBeInstanceOf(DockerProvider::class);
+
+    $deploymentProvider = $this->app->make(DeploymentProvider::class);
+
+    expect($deploymentProvider)
+        ->toBeInstanceOf(DeploymentProvider::class);
+
+    $job->handle($dockerProvider, $deploymentProvider);
+
+    assertUrlStatus($domainNameInDocker, 200);
+
+    $content = Http::get($domainNameInDocker)->body();
+    expect($content)
+        ->toContain('Welcome to nginx!')
+        ->toContain('If you see this page, the nginx web server is successfully installed');
     // skip the test per default, but run it if a special env environment is set
 });
 //})->skip(! getenv('RUN_EXPENSIVE_TESTS'), 'This test is expensive and should only be run in special environments');
