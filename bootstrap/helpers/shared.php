2a52fb5872d9321dc89259f83de4af5da8425e8e
Andras Bacsai <andras.bacsai@gmail.com>
2024-06-26 13:32:36 +0200

    feat: bulk env update api endpoint

--- bootstrap/helpers/shared.php
+++ bootstrap/helpers/shared.php
@@ -40,6 +40,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Support\Stringable;
 use Lcobucci\JWT\Encoding\ChainedFormatter;
@@ -2318,4 +2319,14 @@ function generateSentinelToken()
 function isBase64Encoded($strValue)
 {
     return base64_encode(base64_decode($strValue, true)) === $strValue;
+}
+function customApiValidator(Collection|array $item, array $rules)
+{
+    if (is_array($item)) {
+        $item = collect($item);
+    }
+
+    return Validator::make($item->toArray(), $rules, [
+        'required' => 'This field is required.',
+    ]);
 }
