ec191af874b85f76ac92994caa8b297d22cf7964
Andras Bacsai <andras.bacsai@gmail.com>
2024-07-18 14:23:15 +0200

    chore: Handle JSON parsing errors in format_docker_command_output_to_json

--- bootstrap/helpers/docker.php
+++ bootstrap/helpers/docker.php
@@ -48,9 +48,13 @@ function format_docker_command_output_to_json($rawOutput): Collection
         $outputLines = collect($outputLines);
     }
 
-    return $outputLines
-        ->reject(fn ($line) => empty($line))
-        ->map(fn ($outputLine) => json_decode($outputLine, true, flags: JSON_THROW_ON_ERROR));
+    try {
+        return $outputLines
+            ->reject(fn ($line) => empty($line))
+            ->map(fn ($outputLine) => json_decode($outputLine, true, flags: JSON_THROW_ON_ERROR));
+    } catch (\Throwable $e) {
+        return collect([]);
+    }
 }
 
 function format_docker_labels_to_json(string|array $rawOutput): Collection
