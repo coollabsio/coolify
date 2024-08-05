7d6a8954498d46c6b5891dee59224a0936c42f2f
ayntk-ai <122374094+ayntk-ai@users.noreply.github.com>
2024-08-05 14:44:20 +0200

    Updated Configuration Settings

--- resources/views/livewire/settings/configuration.blade.php
+++ resources/views/livewire/settings/configuration.blade.php
@@ -10,23 +10,25 @@
 
         <div class="flex flex-col gap-2 pt-4">
             <div class="flex flex-wrap items-end gap-2">
+                <h3 class="pt-6">Instance Settings</h3>
                 <x-forms.input id="settings.fqdn" label="Instance's Domain" placeholder="https://coolify.io" />
                 <x-forms.input id="settings.instance_name" label="Instance's Name" placeholder="Coolify" />
+                <h3 class="pt-6 w-full">DNS Validation</h3>
+                <div class="flex flex-wrap items-end gap-2 md:w-96">
+                    <x-forms.checkbox instantSave id="is_dns_validation_enabled" label="Enable DNS validation" />
+                </div>
                 <x-forms.input id="settings.custom_dns_servers" label="DNS Servers"
                     helper="DNS servers for validation FQDNs againts. A comma separated list of DNS servers."
                     placeholder="1.1.1.1,8.8.8.8" />
-                <div class="md:w-96">
-                    <x-forms.checkbox instantSave id="is_dns_validation_enabled" label="Validate DNS settings?" />
-                </div>
             </div>
 
             {{-- <div class="flex gap-2 ">
                 <x-forms.input type="number" id="settings.public_port_min" label="Public Port Min" />
                 <x-forms.input type="number" id="settings.public_port_max" label="Public Port Max" />
             </div> --}}
-        </div>
-        <h2 class="pt-6">API</h2>
 
+        </div>
+        <h3 class="pt-6">API</h3>
         <div class="md:w-96">
             <x-forms.checkbox instantSave id="is_api_enabled" label="Enabled" />
         </div>
@@ -42,6 +44,10 @@
                 id="is_auto_update_enabled" label="Auto Update Coolify" />
         @else
             <x-forms.checkbox instantSave id="is_auto_update_enabled" label="Auto Update Coolify" />
+            @if($is_auto_update_enabled)
+                <x-forms.input id="auto_update_frequency" label="Auto Update Frequency" placeholder="0 0 * * *" helper="Cron expression for auto update frequency" />
+            @endif
+            <x-forms.input id="update_check_frequency" label="Update Check Frequency" placeholder="0 0 * * *" helper="Cron expression for update check frequency" />
         @endif
         <x-forms.checkbox instantSave id="is_registration_enabled" label="Registration Allowed" />
         <x-forms.checkbox instantSave id="do_not_track" label="Do Not Track" />
