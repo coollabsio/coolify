11619b1f1466977681a5432446b9f35f363befe0
ayntk-ai <122374094+ayntk-ai@users.noreply.github.com>
2024-08-13 22:29:44 +0200

    remove unused use

--- database/migrations/2024_08_13_194119_create_git_hosts_table.php
+++ database/migrations/2024_08_13_194119_create_git_hosts_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
