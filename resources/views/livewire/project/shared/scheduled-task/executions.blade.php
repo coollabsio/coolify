165275cb6805b2b53602649aacedb99ae3385893
ayntk-ai <122374094+ayntk-ai@users.noreply.github.com>
2024-08-16 18:01:55 +0200

    formatting

--- resources/views/livewire/project/shared/scheduled-task/executions.blade.php
+++ resources/views/livewire/project/shared/scheduled-task/executions.blade.php
@@ -1,34 +1,32 @@
 <div class="flex flex-col-reverse gap-2">
     @forelse($executions as $execution)
-        @if (data_get($execution, 'id') == $selectedKey)
-            <div class="p-2">
-                @if (data_get($execution, 'message'))
-                    <div>
-                        <pre>{{ data_get($execution, 'message') }}</pre>
-                    </div>
-                @else
-                    <div>No output was recorded for this execution.</div>
-                @endif
-            </div>
+    @if (data_get($execution, 'id') == $selectedKey)
+    <div class="p-2">
+        @if (data_get($execution, 'message'))
+        <div>
+            <pre>{{ data_get($execution, 'message') }}</pre>
+        </div>
+        @else
+        <div>No output was recorded for this execution.</div>
         @endif
-        <a wire:click="selectTask({{ data_get($execution, 'id') }})" @class([
-            'flex flex-col border-l  transition-colors box-without-bg bg-coolgray-100 hover:bg-coolgray-200 cursor-pointer',
-            'bg-coolgray-200 dark:text-white hover:bg-coolgray-200' =>
-                data_get($execution, 'id') == $selectedKey,
-            'border-green-500' => data_get($execution, 'status') === 'success',
-            'border-red-500' => data_get($execution, 'status') === 'failed',
+    </div>
+    @endif
+    <a wire:click="selectTask({{ data_get($execution, 'id') }})" @class([ 'flex flex-col border-l  transition-colors box-without-bg bg-coolgray-100 hover:bg-coolgray-200 cursor-pointer' , 'bg-coolgray-200 dark:text-white hover:bg-coolgray-200'=>
+        data_get($execution, 'id') == $selectedKey,
+        'border-green-500' => data_get($execution, 'status') === 'success',
+        'border-red-500' => data_get($execution, 'status') === 'failed',
         ])>
-            @if (data_get($execution, 'status') === 'running')
-                <div class="absolute top-2 right-2">
-                    <x-loading />
-                </div>
-            @endif
-            <div>Status: {{ data_get($execution, 'status') }}</div>
-            <div>Started At: 
-                {{ $this->formatDateInServerTimezone(data_get($execution, 'created_at', now())) }}
-            </div>
-        </a>
+        @if (data_get($execution, 'status') === 'running')
+        <div class="absolute top-2 right-2">
+            <x-loading />
+        </div>
+        @endif
+        <div>Status: {{ data_get($execution, 'status') }}</div>
+        <div>Started At:
+            {{ $this->formatDateInServerTimezone(data_get($execution, 'created_at', now())) }}
+        </div>
+    </a>
     @empty
-        <div>No executions found.</div>
+    <div>No executions found.</div>
     @endforelse
 </div>
