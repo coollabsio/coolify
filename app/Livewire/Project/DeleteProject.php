b656cabb33fc260d92fa2bf363402cd327d93edb
ayntk-ai <122374094+ayntk-ai@users.noreply.github.com>
2024-08-31 15:07:50 +0200

    delete project confirmation

--- app/Livewire/Project/DeleteProject.php
+++ app/Livewire/Project/DeleteProject.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Project;
 
 use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class DeleteProject extends Component
@@ -21,8 +23,12 @@ class DeleteProject extends Component
         $this->projectName = Project::findOrFail($this->project_id)->name;
     }
 
-    public function delete()
+    public function delete($password)
     {
+        if (!Hash::check($password, Auth::user()->password)) {
+            $this->addError('password', 'The provided password is incorrect.');
+            return;
+        }
         $this->validate([
             'project_id' => 'required|int',
         ]);
--- resources/views/livewire/project/delete-project.blade.php
+++ resources/views/livewire/project/delete-project.blade.php
@@ -2,12 +2,11 @@
     title="Confirm Project Deletion?"
     buttonTitle="Delete Project"
     isErrorButton
-    action="delete_project"
+    submitAction="delete"
     :actions="['This will delete the selected project.']"
     confirmationLabel="Please confirm the execution of the actions by entering the Project Name below"
     shortConfirmationLabel="Project Name"
-    submitAction="delete_project"
     buttonTitle="Delete Project"
-    confirmText="{{ $projectName }}"
+    confirmationText="{{ $projectName }}"
     step3ButtonText="Permanently Delete Project"
 />
