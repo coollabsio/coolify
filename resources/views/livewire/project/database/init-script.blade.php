a97ccd206c2d66f73e605d533ed1998f8b5d00cf
ayntk-ai <122374094+ayntk-ai@users.noreply.github.com>
2024-08-31 20:55:56 +0200

    confirm init script

--- resources/views/livewire/project/database/init-script.blade.php
+++ resources/views/livewire/project/database/init-script.blade.php
@@ -2,9 +2,20 @@
     <div class="flex items-end gap-2">
         <x-forms.input id="filename" label="Filename" />
         <x-forms.button type="submit">Save</x-forms.button>
-        <x-modal-confirmation isErrorButton buttonTitle="Delete">
-            This script will be deleted. It is not reversible. <br>Please think again.
-        </x-modal-confirmation>
+        <x-modal-confirmation 
+        isErrorButton
+        title="Confirm init-script deletion?"
+        buttonTitle="Delete"
+        submitAction="delete"
+        :actions="[
+            'The init-script of this database will be permanently deleted.'
+        ]"
+        confirmationText="{{ $filename }}"
+        confirmationLabel="Please confirm the execution of the actions by entering the init-script name below"
+        shortConfirmationLabel="Init-script Name"
+        :confirmWithPassword=false
+        step2ButtonText="Permanently Delete Init-script"
+        />
     </div>
     <x-forms.textarea id="content" label="Content" />
 </form>
