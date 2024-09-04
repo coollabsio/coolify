04ce622465edfdaff306809712f9d3cb188e729f
Andras Bacsai <andras.bacsai@gmail.com>
2024-09-04 11:34:31 +0200

    refactor: Update build_args property type in ApplicationDeploymentJob

--- app/Jobs/ApplicationDeploymentJob.php
+++ app/Jobs/ApplicationDeploymentJob.php
@@ -109,7 +109,7 @@ class ApplicationDeploymentJob implements ShouldBeEncrypted, ShouldQueue
 
     private bool $is_debug_enabled;
 
-    private $build_args;
+    private Collection|string $build_args;
 
     private $env_args;
 
@@ -168,6 +168,7 @@ class ApplicationDeploymentJob implements ShouldBeEncrypted, ShouldQueue
         $this->application_deployment_queue = ApplicationDeploymentQueue::find($application_deployment_queue_id);
         $this->application = Application::find($this->application_deployment_queue->application_id);
         $this->build_pack = data_get($this->application, 'build_pack');
+        $this->build_args = collect([]);
 
         $this->application_deployment_queue_id = $application_deployment_queue_id;
         $this->deployment_uuid = $this->application_deployment_queue->deployment_uuid;
