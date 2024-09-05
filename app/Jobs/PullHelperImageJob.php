d211362ab3e84aa893f44de43fbb1817fdef1581
Andras Bacsai <andras.bacsai@gmail.com>
2024-09-05 11:04:58 +0200

    improvement: only pull helper image if the version is newer than the one

--- app/Jobs/PullHelperImageJob.php
+++ app/Jobs/PullHelperImageJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\InstanceSettings;
 use App\Models\Server;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeEncrypted;
@@ -10,6 +11,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class PullHelperImageJob implements ShouldBeEncrypted, ShouldQueue
 {
@@ -32,10 +35,25 @@ class PullHelperImageJob implements ShouldBeEncrypted, ShouldQueue
     public function handle(): void
     {
         try {
-            $helperImage = config('coolify.helper_image');
-            ray("Pulling {$helperImage}");
-            instant_remote_process(["docker pull -q {$helperImage}"], $this->server, false);
-            ray('PullHelperImageJob done');
+            $response = Http::retry(3, 1000)->get('https://cdn.coollabs.io/coolify/versions.json');
+            if ($response->successful()) {
+                $versions = $response->json();
+                $settings = InstanceSettings::get();
+                $latest_version = data_get($versions, 'coolify.helper.version');
+                $current_version = $settings->helper_version;
+                Log::info('Latest version', $latest_version);
+                Log::info('Current version', $current_version);
+                if (version_compare($latest_version, $current_version, '>')) {
+                    // New version available
+                    Log::info('New version available', $latest_version);
+                    $helperImage = config('coolify.helper_image');
+                    // REMOVE -next
+                    instant_remote_process(["docker pull -q {$helperImage}:{$latest_version}-next"], $this->server);
+                    $settings->update(['helper_version' => $latest_version]);
+                    Log::info('Pulled helper image', $helperImage, $latest_version);
+                }
+            }
+
         } catch (\Throwable $e) {
             send_internal_notification('PullHelperImageJob failed with: '.$e->getMessage());
             ray($e->getMessage());
