aa30e83f4af4ef4248819c949dbab388672ecb50
Andras Bacsai <andras.bacsai@gmail.com>
2024-09-10 11:33:52 +0200

    fix: delete older versions of the helper image other than the latest one

--- app/Actions/Server/CleanupDocker.php
+++ app/Actions/Server/CleanupDocker.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Server;
 
+use App\Models\InstanceSettings;
 use App\Models\Server;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -21,10 +22,16 @@ class CleanupDocker
 
     private function getCommands(): array
     {
+        $settings = InstanceSettings::get();
+        $helperImageVersion = data_get($settings, 'helper_version');
+        $helperImage = config('coolify.helper_image');
+        $helperImageWithVersion = config('coolify.helper_image').':'.$helperImageVersion;
+
         $commonCommands = [
             'docker container prune -f --filter "label=coolify.managed=true"',
             'docker image prune -af --filter "label!=coolify.managed=true"',
             'docker builder prune -af',
+            "docker images --filter before=$helperImageWithVersion --filter reference=$helperImage | grep $helperImage | awk '{print $3}' | xargs -r docker rmi",
         ];
 
         return $commonCommands;
