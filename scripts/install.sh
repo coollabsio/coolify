72f01185067201f34345ec1d21609562a0ff4e10
Andras Bacsai <andras.bacsai@gmail.com>
2024-09-11 14:26:10 +0200

    chore: Add reminder to backup .env file during installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -406,5 +406,6 @@ sleep 20
 echo "Please visit http://$(curl -4s https://ifconfig.io):8000 to get started."
 echo -e "\nCongratulations! Your Coolify instance is ready to use.\n"
 
+echo -e "Make sure you backup your /data/coolify/source/.env file to a safe location, outside of this server.\n"
 cp /data/coolify/source/.env /data/coolify/source/.env.backup
 echo -e "Your .env file has been copied to /data/coolify/source/.env.backup\n"
