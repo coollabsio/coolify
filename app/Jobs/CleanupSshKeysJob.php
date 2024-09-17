95070ab48d371c75effac2636b13e1a21bb220fc
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-09-17 12:57:06 +0200

    Feat: SSH Key cleanup job

---
+++ app/Jobs/CleanupSshKeysJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\PrivateKey;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
+
+class CleanupSshKeysJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        $oneWeekAgo = Carbon::now()->subWeek();
+
+        PrivateKey::where('created_at', '<', $oneWeekAgo)
+            ->whereDoesntHave('gitSources')
+            ->whereDoesntHave('servers')
+            ->delete();
+    }
+}
