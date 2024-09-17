d13e2c086541f8e2005ee5c2bc14da49baf419a2
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-09-17 15:57:57 +0200

    Fix: Clear mux directory

--- database/migrations/2024_09_16_170001_populate_ssh_keys_and_clear_mux_directory.php
+++ database/migrations/2024_09_16_170001_populate_ssh_keys_and_clear_mux_directory.php
@@ -4,12 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Storage;
 use App\Models\PrivateKey;
 
-class PopulateSshKeysDirectory extends Migration
+class PopulateSshKeysAndClearMuxDirectory extends Migration
 {
     public function up()
     {
         Storage::disk('ssh-keys')->deleteDirectory('');
         Storage::disk('ssh-keys')->makeDirectory('');
+
+        Storage::disk('ssh-mux')->deleteDirectory('');
+        Storage::disk('ssh-mux')->makeDirectory('');
 
         PrivateKey::chunk(100, function ($keys) {
             foreach ($keys as $key) {
