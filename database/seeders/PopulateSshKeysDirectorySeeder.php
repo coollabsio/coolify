62459f9a95b11bf21c8cac77ece1028e8f620a85
Andras Bacsai <andras.bacsai@gmail.com>
2024-09-24 11:57:13 +0200

    refactor: Remove unused code in DatabaseBackupStatusJob and PopulateSshKeysDirectorySeeder

--- database/seeders/PopulateSshKeysDirectorySeeder.php
+++ database/seeders/PopulateSshKeysDirectorySeeder.php
@@ -29,8 +29,8 @@ class PopulateSshKeysDirectorySeeder extends Seeder
                 Process::run("chown -R $user ".storage_path('app/ssh/keys'));
                 Process::run("chown -R $user ".storage_path('app/ssh/mux'));
             } else {
-                Process::run('chown -R 9999:9999 '.storage_path('app/ssh/keys'));
-                Process::run('chown -R 9999:9999 '.storage_path('app/ssh/mux'));
+                Process::run('chown -R 9999:root '.storage_path('app/ssh/keys'));
+                Process::run('chown -R 9999:root '.storage_path('app/ssh/mux'));
             }
         } catch (\Throwable $e) {
             echo "Error: {$e->getMessage()}\n";
