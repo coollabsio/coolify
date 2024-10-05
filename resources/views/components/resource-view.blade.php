4624a381b1ccac0f413ebfa308518ff172c448f5
Andras Bacsai <andras.bacsai@gmail.com>
2024-10-05 20:55:23 +0200

    new search input on "new resource" view

--- resources/views/components/resource-view.blade.php
+++ resources/views/components/resource-view.blade.php
@@ -2,8 +2,9 @@
     'transition-all duration-150 box-without-bg dark:bg-coolgray-100 bg-white group',
     'hover:border-l-coollabs cursor-pointer' => !$upgrade,
     'hover:border-l-red-500 cursor-not-allowed' => $upgrade,
-]) @if (!$upgrade) wire:click={{ $wire }} @endif>
+])>
     <div class="flex items-center">
+
         {{ $logo }}
         <div class="flex flex-col pl-2 ">
             <div class="dark:text-white text-md">
@@ -18,13 +19,4 @@
             @endif
         </div>
     </div>
-    @isset($documentation)
-        <div class="flex-1"></div>
-        <div class="flex items-center px-2 " title="Read the documentation.">
-            <a class="p-2 rounded hover:bg-coolgray-200 hover:no-underline group-hover:dark:text-white text-neutral-600"
-                onclick="event.stopPropagation()" href="{{ $documentation }}" target="_blank">
-                Docs
-            </a>
-        </div>
-    @endisset
 </div>
