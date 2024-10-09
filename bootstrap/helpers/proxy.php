a094eceb624b024ef46efd1e98a6769003555c9e
Vladimír Smitka <vladimir.smitka@lynt.cz>
2024-10-09 18:34:17 +0200

    Expose port 443/udp with Caddy proxy

--- bootstrap/helpers/proxy.php
+++ bootstrap/helpers/proxy.php
@@ -239,6 +239,7 @@ function generate_default_proxy_configuration(Server $server)
                     'ports' => [
                         '80:80',
                         '443:443',
+                        '443:443/udp',
                     ],
                     'labels' => [
                         'coolify.managed=true',
