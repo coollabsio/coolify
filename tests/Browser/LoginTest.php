becaf92fd8992745fcef6bd649cc988c9615bfc8
Andras Bacsai <andras.bacsai@gmail.com>
2024-10-17 21:15:48 +0200

    test dusk gh

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -13,18 +13,20 @@ class LoginTest extends DuskTestCase
      * Login with the test user and assert that the user is redirected to the dashboard.
      *
      * @return void
+     *
      * @throws Throwable
      */
     public function testLogin()
     {
-        $email = config('testing.dusk_test_email');
-        $password = config('testing.dusk_test_password');
+        $email = 'test@example.com';
+        $password = 'password';
         $this->browse(function (Browser $browser) use ($password, $email) {
             $browser->visit('/login')
                 ->type('email', $email)
                 ->type('password', $password)
                 ->press('Login')
-                ->assertPathIs('/');
+                ->assertPathIs('/')
+                ->screenshot('login');
         });
     }
 }
