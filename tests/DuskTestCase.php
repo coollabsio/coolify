8bb021e62ad653fe470bc9bb18f6f87c57757075
Andras Bacsai <andras.bacsai@gmail.com>
2024-10-17 21:18:37 +0200

    Refactor DuskTestCase to support headless Chrome and maximize window size

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,24 +2,59 @@
 
 namespace Tests;
 
+use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Support\Collection;
 use Laravel\Dusk\TestCase as BaseTestCase;
 
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     */
+    public static function prepare(): void
+    {
+        if (! static::runningInSail()) {
+            static::startChromeDriver();
+        }
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     */
     protected function driver(): RemoteWebDriver
     {
+        $options = (new ChromeOptions)->addArguments(collect([
+            $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
+        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
+            return $items->merge([
+                '--disable-gpu',
+                '--headless=new',
+            ]);
+        })->all());
+
         return RemoteWebDriver::create(
-            env('DUSK_DRIVER_URL'),
-            DesiredCapabilities::chrome()
+            'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY,
+                $options
+            )
         );
     }
 
+    /**
+     * Determine if the browser window should start maximized.
+     */
     protected function baseUrl()
     {
-        return 'https://staging.heyandras.dev';
+        $app_url = config('app.url');
+        $port = config('app.port');
+
+        return $app_url.':'.$port;
     }
 }
