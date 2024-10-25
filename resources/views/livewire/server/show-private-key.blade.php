d26a0f194b97dd14525d4cf88cf47fb720935df5
Andras Bacsai <andras.bacsai@gmail.com>
2024-10-25 11:17:17 +0200

    Refactor flexbox layout and button styles in show-private-key.blade.php

--- resources/views/livewire/server/show-private-key.blade.php
+++ resources/views/livewire/server/show-private-key.blade.php
@@ -14,17 +14,17 @@
     </div>
     <div class="grid xl:grid-cols-2 grid-cols-1 gap-2">
         @forelse ($privateKeys as $private_key)
-            <div class="box-without-bg justify-between dark:bg-coolgray-100 bg-white items-center">
-                <div class="flex flex-col ">
+            <div class="box-without-bg justify-between dark:bg-coolgray-100 bg-white items-center flex flex-col gap-2">
+                <div class="flex flex-col w-full">
                     <div class="box-title">{{ $private_key->name }}</div>
                     <div class="box-description">{{ $private_key->description }}</div>
                 </div>
                 @if (data_get($server, 'privateKey.uuid') !== $private_key->uuid)
-                <x-forms.button wire:click='setPrivateKey({{ $private_key->id }})'>
-                    Use this key
+                    <x-forms.button class="w-full" wire:click='setPrivateKey({{ $private_key->id }})'>
+                        Use this key
                     </x-forms.button>
                 @else
-                    <x-forms.button disabled>
+                    <x-forms.button class="w-full" disabled>
                         Currently used
                     </x-forms.button>
                 @endif
