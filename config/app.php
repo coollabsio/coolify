e45e5e2a224cb12ad6397e5b7447ed89d851a028
Andras Bacsai <andras.bacsai@gmail.com>
2024-10-29 11:32:50 +0100

    disable dusk

--- config/app.php
+++ config/app.php
@@ -200,7 +200,6 @@ return [
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
-        App\Providers\DuskServiceProvider::class,
 
     ],
 
