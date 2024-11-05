f24f749b0ead0583052e6717c5c496c25487637d
Evert Arias <evert.arias@treve.dev>
2024-11-05 17:07:30 -0500

    fix: make "Update" heading visible in light mode

--- resources/views/livewire/settings/index.blade.php
+++ resources/views/livewire/settings/index.blade.php
@@ -95,7 +95,7 @@
             <x-forms.checkbox instantSave id="is_registration_enabled" label="Registration Allowed" />
             <x-forms.checkbox instantSave id="do_not_track" label="Do Not Track" />
         </div>
-        <h5 class="pt-4 font-bold text-white">Update</h5>
+        <h5 class="pt-4 font-bold">Update</h5>
         <div class="text-right md:w-96">
             @if (!is_null(env('AUTOUPDATE', null)))
                 <div class="text-right md:w-96">
