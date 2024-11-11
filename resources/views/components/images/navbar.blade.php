1fd873758886d1a97dde5e06c803aa3cb80c3967
David Buday <david.buday@gauss.dev>
2024-11-11 15:38:23 +0100

    fix: navbar link color

--- resources/views/components/images/navbar.blade.php
+++ resources/views/components/images/navbar.blade.php
@@ -3,10 +3,12 @@
     <div class="subtitle">Images and container management.</div>
     <div class="navbar-main">
         <nav class="flex items-center gap-6 scrollbar min-h-10">
-            <a href="{{ route('images.images.index') }}">
+            <a href="{{ route('images.images.index') }}"
+                class="{{ request()->routeIs('images.images.index') ? 'dark:text-white ' : '' }}">
                 <button>Images</button>
             </a>
-            <a href="{{ route('images.registries.index') }}">
+            <a href="{{ route('images.registries.index') }}"
+                class="{{ request()->routeIs('images.registries.index') ? 'dark:text-white' : '' }}">
                 <button>Registries</button>
             </a>
         </nav>
