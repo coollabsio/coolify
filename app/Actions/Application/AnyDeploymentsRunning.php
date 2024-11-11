d7cde676d544f78b99d1fc623f5d9fa855f340cd
Andras Bacsai <andras.bacsai@gmail.com>
2024-11-11 23:35:48 +0100

    feat: check local horizon scheduler deployments

---
+++ app/Actions/Application/AnyDeploymentsRunning.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Actions\Application;
+
+use Laravel\Horizon\Contracts\JobRepository;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class AnyDeploymentsRunning
+{
+    use AsAction;
+
+    public function handle()
+    {
+        $hostname = gethostname();
+        $recent = app(JobRepository::class)->getRecent();
+        if ($recent) {
+            $running = $recent->filter(function ($job) use ($hostname) {
+                $payload = json_decode($job->payload);
+                $tags = data_get($payload, 'tags');
+
+                return $job->status != 'completed' &&
+                       $job->status != 'failed' &&
+                       isset($tags) &&
+                       is_array($tags) &&
+                       in_array('server:'.$hostname, $tags);
+            });
+            if ($running->count() > 0) {
+                dump($running);
+                echo 'true';
+
+                return true;
+            }
+        }
+
+        echo 'false';
+
+        return false;
+    }
+}
