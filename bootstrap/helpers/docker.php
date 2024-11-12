85d07283222934682bf5c988754f954798e1c870
konstchri <79702630+konstchri@users.noreply.github.com>
2024-11-12 16:21:45 +0200

    [Fix]: Caddy docker labels do not honor "strip prefix" option

--- bootstrap/helpers/docker.php
+++ bootstrap/helpers/docker.php
@@ -283,6 +283,10 @@ function fqdnLabelsForCaddy(string $network, string $uuid, Collection $domains, 
         $host_without_www = str($host)->replace('www.', '');
         $schema = $url->getScheme();
         $port = $url->getPort();
+        $handle = "handle_path"
+        if ( ! $is_stripprefix_enabled){
+            $handle = "handle"
+        }
         if (is_null($port) && ! is_null($onlyPort)) {
             $port = $onlyPort;
         }
@@ -293,12 +297,13 @@ function fqdnLabelsForCaddy(string $network, string $uuid, Collection $domains, 
         $labels->push("caddy_{$loop}.header=-Server");
         $labels->push("caddy_{$loop}.try_files={path} /index.html /index.php");
 
+
         if ($port) {
-            $labels->push("caddy_{$loop}.handle_path.{$loop}_reverse_proxy={{upstreams $port}}");
+            $labels->push("caddy_{$loop}.{$handle}.{$loop}_reverse_proxy={{upstreams $port}}");
         } else {
-            $labels->push("caddy_{$loop}.handle_path.{$loop}_reverse_proxy={{upstreams}}");
+            $labels->push("caddy_{$loop}.{$handle}.{$loop}_reverse_proxy={{upstreams}}");
         }
-        $labels->push("caddy_{$loop}.handle_path={$path}*");
+        $labels->push("caddy_{$loop}.{$handle}={$path}*");
         if ($is_gzip_enabled) {
             $labels->push("caddy_{$loop}.encode=zstd gzip");
         }
