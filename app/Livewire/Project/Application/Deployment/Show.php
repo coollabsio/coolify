9630efce67a78e2418afdf065c517a6ade43fd2f
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-11-22 15:52:36 +0100

    uuid deplyment

--- app/Livewire/Project/Application/Deployment/Show.php
+++ app/Livewire/Project/Application/Deployment/Show.php
@@ -26,7 +26,7 @@ class Show extends Component
         if (! $project) {
             return redirect()->route('dashboard');
         }
-        $environment = $project->load(['environments'])->environments->where('name', request()->route('environment_name'))->first()->load(['applications']);
+        $environment = $project->load(['environments'])->environments->where('uuid', request()->route('environment_uuid'))->first()->load(['applications']);
         if (! $environment) {
             return redirect()->route('dashboard');
         }
@@ -46,7 +46,7 @@ class Show extends Component
         if (! $application_deployment_queue) {
             return redirect()->route('project.application.deployment.index', [
                 'project_uuid' => $project->uuid,
-                'environment_name' => $environment->name,
+                'environment_uuid' => $environment->uuid,
                 'application_uuid' => $application->uuid,
             ]);
         }
