f535ed2359f9400ded0eed439360e059bb28f41b
Andras Bacsai <andras.bacsai@gmail.com>
2024-12-04 14:00:49 +0100

    Enhance modal input component and storage forms
    - Added 'minWidth' attribute to modal input component for better responsiveness.
    - Updated modal input instantiation in storage view to set a minimum width of 64rem.
    - Improved layout and user guidance in the storage add forms by adding descriptive text for volume, file, and directory mounts.
    - Changed button labels from 'Save' to 'Add' for clarity in storage forms.

--- resources/views/livewire/project/shared/storages/add.blade.php
+++ resources/views/livewire/project/shared/storages/add.blade.php
@@ -1,13 +1,15 @@
-<div class="flex flex-col w-full gap-2 rounded max-h-[80vh] overflow-y-auto scrollbar">
-    <div class="p-4">
-        You can add Volumes, Files and Directories to your resources here.
-        <form class="flex flex-col w-full gap-2 rounded" wire:submit='submitPersistentVolume'>
+<div class="flex flex-col w-full gap-2 max-h-[80vh] overflow-y-auto scrollbar">
+    <form class="flex flex-col w-full gap-2 rounded " wire:submit='submitPersistentVolume'>
+        <div class="flex flex-col">
             <h3>Volume Mount</h3>
-            @if ($isSwarm)
-                <h5>Swarm Mode detected: You need to set a shared volume (EFS/NFS/etc) on all the worker nodes if you
-                    would
-                    like to use a persistent volumes.</h5>
-            @endif
+            <div>Docker Volumes mounted to the container.</div>
+        </div>
+        @if ($isSwarm)
+            <h5>Swarm Mode detected: You need to set a shared volume (EFS/NFS/etc) on all the worker nodes if you
+                would
+                like to use a persistent volumes.</h5>
+        @endif
+        <div class="flex flex-col gap-2 px-2">
             <x-forms.input placeholder="pv-name" id="name" label="Name" required helper="Volume name." />
             @if ($isSwarm)
                 <x-forms.input placeholder="/root" id="host_path" label="Source Path" required
@@ -19,29 +21,39 @@
             <x-forms.input placeholder="/tmp/root" id="mount_path" label="Destination Path" required
                 helper="Directory inside the container." />
             <x-forms.button type="submit" @click="modalOpen=false">
-                Save
+                Add
             </x-forms.button>
+        </div>
 
-        </form>
-        <form class="flex flex-col w-full gap-2 rounded" wire:submit='submitFileStorage'>
+    </form>
+    <form class="flex flex-col w-full gap-2 rounded py-4" wire:submit='submitFileStorage'>
+        <div class="flex flex-col">
             <h3>File Mount</h3>
+            <div>Actual file mounted from the host system to the container.</div>
+        </div>
+        <div class="flex flex-col gap-2 px-2">
             <x-forms.input placeholder="/etc/nginx/nginx.conf" id="file_storage_path" label="Destination Path" required
-                helper="File inside the container" />
+                helper="File location inside the container" />
             <x-forms.textarea label="Content" id="file_storage_content"></x-forms.textarea>
             <x-forms.button type="submit" @click="modalOpen=false">
-                Save
+                Add
             </x-forms.button>
-        </form>
-        <form class="flex flex-col w-full gap-2 rounded" wire:submit='submitFileStorageDirectory'>
+        </div>
+    </form>
+    <form class="flex flex-col w-full gap-2 rounded" wire:submit='submitFileStorageDirectory'>
+        <div class="flex flex-col">
             <h3>Directory Mount</h3>
+            <div>Directory mounted from the host system to the container.</div>
+        </div>
+        <div class="flex flex-col gap-2 px-2">
             <x-forms.input placeholder="{{ application_configuration_dir() }}/{{ $resource->uuid }}/etc/nginx"
                 id="file_storage_directory_source" label="Source Directory" required
                 helper="Directory on the host system." />
             <x-forms.input placeholder="/etc/nginx" id="file_storage_directory_destination"
                 label="Destination Directory" required helper="Directory inside the container." />
             <x-forms.button type="submit" @click="modalOpen=false">
-                Save
+                Add
             </x-forms.button>
-        </form>
-    </div>
+        </div>
+    </form>
 </div>
