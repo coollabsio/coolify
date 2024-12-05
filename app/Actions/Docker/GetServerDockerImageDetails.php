40bf9e22b1fcfab68336c13a77eca01b58536005
davidhorvat <david.horvat@gauss.dev>
2024-12-05 19:42:41 +0100

    refactor: list docker images

---
+++ app/Actions/Docker/GetServerDockerImageDetails.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Actions\Docker;
+
+use Illuminate\Database\Eloquent\Collection;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class GetServerDockerImageDetails
+{
+    use AsAction;
+
+    public static function run()
+    {
+
+    }
+}
