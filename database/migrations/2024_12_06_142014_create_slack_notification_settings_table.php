cc894defc5939915cc7c6dec47d5f68284b1acd4
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-12-06 15:25:37 +0100

    feat: add slack notifications to a new table

---
+++ database/migrations/2024_12_06_142014_create_slack_notification_settings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('slack_notification_settings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained()->cascadeOnDelete();
+
+            $table->boolean('slack_enabled')->default(false);
+            $table->string('slack_webhook_url')->nullable();
+
+            $table->boolean('deployment_success_slack_notifications')->default(false);
+            $table->boolean('deployment_failure_slack_notifications')->default(true);
+            $table->boolean('backup_success_slack_notifications')->default(false);
+            $table->boolean('backup_failure_slack_notifications')->default(true);
+            $table->boolean('scheduled_task_success_slack_notifications')->default(false);
+            $table->boolean('scheduled_task_failure_slack_notifications')->default(true);
+            $table->boolean('status_change_slack_notifications')->default(false);
+            $table->boolean('server_disk_usage_slack_notifications')->default(true);
+
+            $table->unique(['team_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('slack_notification_settings');
+    }
+};
