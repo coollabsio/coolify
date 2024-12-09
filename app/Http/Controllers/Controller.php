5515f07f14915b8cadab824cee748aa32504e57e
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-12-09 12:05:18 +0100

    chore: remove commented out notification

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -44,7 +44,6 @@ class Controller extends BaseController
         $request->fulfill();
         $name = request()->user()?->name;
 
-        // send_internal_notification("User {$name} verified their email address.");
         return redirect(RouteServiceProvider::HOME);
     }
 
