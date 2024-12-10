78d07f00f86f814840aa76e88c7da88f0310e724
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-12-10 16:11:58 +0100

    feat: docker cleanup email views

---
+++ resources/views/emails/docker-cleanup-failed.blade.php
@@ -0,0 +1,8 @@
+<x-emails.layout>
+Docker Cleanup on {{ $serverName }} FAILED with the following error:
+
+<pre>
+{{ $message }}
+</pre>
+
+</x-emails.layout>
---
+++ resources/views/emails/docker-cleanup-success.blade.php
@@ -0,0 +1,8 @@
+<x-emails.layout>
+Docker Cleanup on "{{ $serverName }}" succeeded with the following message:
+
+<pre>
+"{{ $message }}"
+</pre>
+
+</x-emails.layout>
