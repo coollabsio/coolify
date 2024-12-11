0483f98ad01917ebb56af71bc679b97fc2deb85d
davidhorvat <david.horvat@gauss.dev>
2024-12-11 19:34:21 +0100

    refactor: GetServerDockerImageDetails

--- app/Actions/Docker/GetServerDockerImageDetails.php
+++ app/Actions/Docker/GetServerDockerImageDetails.php
@@ -10,7 +10,7 @@ class GetServerDockerImageDetails
 
     public static function run($server, $imageId)
     {
-        $imageDetailsRaw = instant_remote_process(["docker inspect --type=image {$imageId}"], $server);
+        $imageDetailsRaw = instant_remote_process(["docker inspect --type=image {$imageId} --format '{{json .}}'"], $server);
         $imageDetails = json_decode($imageDetailsRaw, true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
@@ -18,10 +18,10 @@ class GetServerDockerImageDetails
         }
 
         $containerCountRaw = instant_remote_process(["docker ps -q --filter ancestor={$imageId} | wc -l"], $server);
-        $containerCount = intval(trim($containerCountRaw)); // Convert to an integer
+        $containerCount = intval(trim($containerCountRaw));
 
-        $imageDetails[0]['ContainerCount'] = $containerCount;
+        $imageDetails['ContainerCount'] = $containerCount;
 
-        return $imageDetails[0];
+        return $imageDetails;
     }
 }
