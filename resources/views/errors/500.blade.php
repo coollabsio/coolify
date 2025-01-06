099189885350a4bf57c5f9fde4179fba994b5618
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-01-06 18:16:30 +0100

    chore: final css touches

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.base')
-<div class="h-full p-4">
-    <div>
-        <p class="font-mono font-semibold text-red-500 text-9xl">500</p>
-        <h1 class="mt-4 font-bold tracking-tight dark:text-white">Wait, this is not cool...</h1>
-        <p class="text-base leading-7 text-neutral-300">There has been an error, we are working on it.</p>
+<div class="flex items-center justify-center min-h-screen">
+    <div class="w-full max-w-3xl px-8">
+        <p class="font-mono font-semibold text-red-500 text-[200px] leading-none">500</p>
+        <h1 class="text-3xl font-bold tracking-tight dark:text-white">Wait, this is not cool...</h1>
+        <p class="mt-2 text-lg leading-7 text-neutral-300">There has been an error, we are working on it.</p>
         @if ($exception->getMessage() !== '')
-            <div class="mt-6 text-xs text-red-500 whitespace-pre-wrap">
+            <div class="mt-6 text-sm text-red-500 whitespace-pre-wrap leading-none">
                 Error: {!! $exception->getMessage() !!}
             </div>
         @endif
-        <div class="mt-10 space-x-6">
+        <div class="flex items-center mt-10 gap-6">
             <a href="/">
                 <x-forms.button>Go back home</x-forms.button>
             </a>
-            <a target="_blank" class="text-xs" href="{{ config('constants.urls.contact') }}">Contact
-                support
+            <a target="_blank" class="text-sm hover:text-neutral-300 flex items-center gap-1" href="{{ config('constants.urls.contact') }}">
+                Contact support
                 <x-external-link />
             </a>
         </div>
