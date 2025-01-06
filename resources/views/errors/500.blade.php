cb75ca8bea7709bccef5a7c1bb2bd41635faa5c7
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-01-06 18:11:04 +0100

    chore: more css as it still looks like shit

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.base')
-<div class="flex flex-col items-center justify-center h-full">
-    <div class="text-center">
-        <p class="font-mono font-semibold text-red-500 text-7xl">500</p>
+<div class="h-full p-4">
+    <div>
+        <p class="font-mono font-semibold text-red-500 text-9xl">500</p>
         <h1 class="mt-4 font-bold tracking-tight dark:text-white">Wait, this is not cool...</h1>
         <p class="text-base leading-7 text-neutral-300">There has been an error, we are working on it.</p>
         @if ($exception->getMessage() !== '')
-            <div class="mt-6 text-xs text-red-500 whitespace-pre-wrap text-right">
+            <div class="mt-6 text-xs text-red-500 whitespace-pre-wrap">
                 Error: {!! $exception->getMessage() !!}
             </div>
         @endif
-        <div class="flex items-center justify-center mt-10 gap-x-6">
+        <div class="mt-10 space-x-6">
             <a href="/">
                 <x-forms.button>Go back home</x-forms.button>
             </a>
