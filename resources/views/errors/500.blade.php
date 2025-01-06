d0c84bc6fa785a085a1af6524dbe764cfb8a71d1
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-01-06 18:22:12 +0100

    fix: indent

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,10 +3,10 @@
     <div class="w-full max-w-3xl px-8">
         <p class="font-mono font-semibold text-red-500 text-[200px] leading-none">500</p>
         <h1 class="text-3xl font-bold tracking-tight dark:text-white">Wait, this is not cool...</h1>
-        <p class="mt-2 text-lg leading-7 text-neutral-300">There has been an error, we are working on it.</p>
+        <p class="mt-2 text-lg leading-7 text-neutral-300">There has been an error with the following error message:</p>
         @if ($exception->getMessage() !== '')
             <div class="mt-6 text-sm text-red-500 whitespace-pre-wrap leading-none">
-                Error: {!! $exception->getMessage() !!}
+                {!! $exception->getMessage() !!}
             </div>
         @endif
         <div class="flex items-center mt-10 gap-6">
