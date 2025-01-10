925af53d3dc9e2d5b0d45ab5c44b1bd64d35b8c8
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-10 19:00:18 +0100

    horizon gate

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Contracts\CustomJobRepositoryInterface;
 use App\Models\ApplicationDeploymentQueue;
+use App\Models\User;
 use App\Repositories\CustomJobRepository;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Horizon\Contracts\JobRepository;
 use Laravel\Horizon\Events\JobReserved;
@@ -42,6 +44,16 @@ class HorizonServiceProvider extends ServiceProvider
                 ]);
             }
         });
+    }
 
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function ($user) {
+            $root_user = User::find(0);
+
+            return in_array($user->email, [
+                $root_user->email,
+            ]);
+        });
     }
 }
