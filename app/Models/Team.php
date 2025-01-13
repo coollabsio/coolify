db5786efc9aeedf082a2804f6c1f00da6e7869b0
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-01-13 12:14:36 +0100

    fix: instance wide GitHub apps are not available on other teams then the source team

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -247,8 +247,17 @@ class Team extends Model implements SendsDiscord, SendsEmail, SendsPushover, Sen
     public function sources()
     {
         $sources = collect([]);
-        $github_apps = $this->hasMany(GithubApp::class)->whereisPublic(false)->get();
-        $gitlab_apps = $this->hasMany(GitlabApp::class)->whereisPublic(false)->get();
+        $github_apps = GithubApp::where(function ($query) {
+            $query->where('team_id', $this->id)
+                ->Where('is_public', false)
+                ->orWhere('is_system_wide', true);
+        })->get();
+
+        $gitlab_apps = GitlabApp::where(function ($query) {
+            $query->where('team_id', $this->id)
+                ->Where('is_public', false)
+                ->orWhere('is_system_wide', true);
+        })->get();
 
         return $sources->merge($github_apps)->merge($gitlab_apps);
     }
