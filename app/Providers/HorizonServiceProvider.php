aab5398b80148f0e2f62677d6016da1f19736ec9
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-14 08:49:03 +0100

    fix: monaco editor disabled state

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -4,30 +4,15 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         parent::boot();
-
-        // Horizon::routeSmsNotificationsTo('15556667777');
-        // Horizon::routeMailNotificationsTo('example@example.com');
-        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
-
-        Horizon::night();
     }
 
-    /**
-     * Register the Horizon gate.
-     *
-     * This gate determines who can access Horizon in non-local environments.
-     */
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
--- resources/views/livewire/project/application/general.blade.php
+++ resources/views/livewire/project/application/general.blade.php
@@ -283,9 +283,9 @@
                     <x-forms.checkbox label="Escape special characters in labels?"
                         helper="By default, $ (and other chars) is escaped. So if you write $ in the labels, it will be saved as $$.<br><br>If you want to use env variables inside the labels, turn this off."
                         id="application.settings.is_container_label_escape_enabled" instantSave></x-forms.checkbox>
-                    <x-forms.checkbox label="Readonly labels"
+                    {{-- <x-forms.checkbox label="Readonly labels"
                         helper="Labels are readonly by default. Readonly means that edits you do to the labels could be lost and Coolify will autogenrate the labels for you. If you want to edit the labels directly, disable this option. <br><br>Be careful, it could break the proxy configuration after you restart the container as Coolify will now NOT autogenrate the labels for you (ofc you can alway reset the labels to the coolify defaults manually)."
-                        id="application.settings.is_container_label_readonly_enabled" instantSave></x-forms.checkbox>
+                        id="application.settings.is_container_label_readonly_enabled" instantSave></x-forms.checkbox> --}}
                 </div>
             @endif
             @if ($application->dockerfile)
@@ -308,15 +308,20 @@
                     @endif
                 </div>
 
-                <x-forms.textarea label="Container Labels" rows="15" id="customLabels"
-                    monacoEditorLanguage="ini" useMonacoEditor></x-forms.textarea>
+                @if ($application->settings->is_container_label_readonly_enabled)
+                    <x-forms.textarea readonly disabled label="Container Labels" rows="15" id="customLabels"
+                        monacoEditorLanguage="ini" useMonacoEditor></x-forms.textarea>
+                @else
+                    <x-forms.textarea label="Container Labels" rows="15" id="customLabels"
+                        monacoEditorLanguage="ini" useMonacoEditor></x-forms.textarea>
+                @endif
                 <div class="w-96">
-                    <x-forms.checkbox label="Escape special characters in labels?"
-                        helper="By default, $ (and other chars) is escaped. So if you write $ in the labels, it will be saved as $$.<br><br>If you want to use env variables inside the labels, turn this off."
-                        id="application.settings.is_container_label_escape_enabled" instantSave></x-forms.checkbox>
                     <x-forms.checkbox label="Readonly labels"
                         helper="Labels are readonly by default. Readonly means that edits you do to the labels could be lost and Coolify will autogenrate the labels for you. If you want to edit the labels directly, disable this option. <br><br>Be careful, it could break the proxy configuration after you restart the container as Coolify will now NOT autogenrate the labels for you (ofc you can alway reset the labels to the coolify defaults manually)."
                         id="application.settings.is_container_label_readonly_enabled" instantSave></x-forms.checkbox>
+                    <x-forms.checkbox label="Escape special characters in labels?"
+                        helper="By default, $ (and other chars) is escaped. So if you write $ in the labels, it will be saved as $$.<br><br>If you want to use env variables inside the labels, turn this off."
+                        id="application.settings.is_container_label_escape_enabled" instantSave></x-forms.checkbox>
                 </div>
                 <x-modal-confirmation title="Confirm Labels Reset to Coolify Defaults?"
                     buttonTitle="Reset Labels to Defaults" buttonFullWidth submitAction="resetDefaultLabels(true)"
