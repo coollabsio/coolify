4b6690e5999a56f4fea7c3636bb096a10bd26e3f
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-01-16 17:51:26 +0100

    fix: RootUserSeeder
    - ensure that the existing root user is not overwritten
    - ensure that the seeder can only be run once - creating the initial root user

--- database/seeders/RootUserSeeder.php
+++ database/seeders/RootUserSeeder.php
@@ -11,22 +11,26 @@ class RootUserSeeder extends Seeder
 {
     public function run(): void
     {
+        if (User::where('id', 0)->exists()) {
+            echo "  Root user already exists. Skipping creation.\n";
+
+            return;
+        }
+
         if (env('ROOT_USER_EMAIL') && env('ROOT_USER_PASSWORD')) {
-            User::updateOrCreate(
-                ['id' => 0],
-                [
-                    'name' => env('ROOT_USER_NAME', 'Root User'),
-                    'email' => env('ROOT_USER_EMAIL'),
-                    'password' => Hash::make(env('ROOT_USER_PASSWORD')),
-                ]
-            );
+            User::create([
+                'id' => 0,
+                'name' => env('ROOT_USERNAME', 'Root User'),
+                'email' => env('ROOT_USER_EMAIL'),
+                'password' => Hash::make(env('ROOT_USER_PASSWORD')),
+            ]);
 
             InstanceSettings::updateOrCreate(
                 ['id' => 0],
                 ['is_registration_enabled' => false]
             );
 
-            echo "  Root user created/updated successfully.\n";
+            echo "  Root user created successfully.\n";
             echo "  Registration has been disabled.\n";
         } else {
             echo "  Warning: ROOT_USER_EMAIL and ROOT_USER_PASSWORD environment variables are required for root user creation.\n";
