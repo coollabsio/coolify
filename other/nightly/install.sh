3957bb31ce0d556a5d1f04c5b1b6ce2dafc40b2c
Andras Bacsai <andras.bacsai@gmail.com>
2025-01-20 14:01:14 +0100

    fix: update CDN URL in install script to point to nightly version

--- other/nightly/install.sh
+++ other/nightly/install.sh
@@ -5,7 +5,7 @@ set -e # Exit immediately if a command exits with a non-zero status
 ## $1 could be empty, so we need to disable this check
 #set -u # Treat unset variables as an error and exit
 set -o pipefail # Cause a pipeline to return the status of the last command that exited with a non-zero status
-CDN="https://cdn.coollabs.io/coolify"
+CDN="https://cdn.coollabs.io/coolify-nightly"
 DATE=$(date +"%Y%m%d-%H%M%S")
 
 VERSION="1.7"
