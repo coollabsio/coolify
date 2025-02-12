2fa247b744cf06a1a6096783594d0e112686cbe6
Andras Bacsai <andras.bacsai@gmail.com>
2025-02-12 10:00:01 +0100

    chore: Remove commented-out config download functionality

--- resources/views/livewire/project/application/general.blade.php
+++ resources/views/livewire/project/application/general.blade.php
@@ -6,9 +6,9 @@
                 Save
             </x-forms.button>
 
-            <x-forms.button wire:click="downloadConfig">
+            {{-- <x-forms.button wire:click="downloadConfig">
                 Download Config
-            </x-forms.button>
+            </x-forms.button> --}}
         </div>
         <div>General configuration for your application.</div>
         <div class="flex flex-col gap-2 py-4">
