ae4ae22ad499a882f5f4fe10d11f23824fdff7f3
Andras Bacsai <andras.bacsai@gmail.com>
2025-03-04 08:47:06 +0100

    fix

--- resources/js/route.js
+++ resources/js/route.js
@@ -4,15 +4,22 @@ import { route as ziggyRoute } from 'ziggy-js';
 export function route(name, params, absolute = true, config) {
   const route = ziggyRoute(name, params, absolute, config);
   try {
+    if (absolute === false) {
+      return route
+    }
+
     const origin = window.location.origin;
     const routeUrl = new URL(route);
     const originUrl = new URL(origin);
+    console.log(routeUrl.protocol)
+    console.log(originUrl.protocol)
     if (routeUrl.protocol !== originUrl.protocol) {
       routeUrl.protocol = originUrl.protocol;
       route = routeUrl.toString();
     }
     return route
   } catch (error) {
+    console.error(error)
     return route
   } finally {
     console.log(route)
