a55ad5a1da086179385713db81c7dd0b6891443e
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-03-26 20:28:30 +0100

    chore(config): remove unused auth config option

--- config/auth.php
+++ config/auth.php
@@ -64,11 +64,6 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
