967409c32c90ebe499f5fde85f0c5512a8926cbf
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-03-29 14:50:51 +0100

    chore(core): remove css file
    - CSS file is not needed for Inertia

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
