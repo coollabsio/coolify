03b0ee59f6919b412b3163bf7b09cabd25ad9d06
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-03-31 18:50:42 +0200

    feat(dep): bump all dependencies

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
     'coolify' => [
         'version' => '4.0.0-beta.402',
-        'helper_version' => '1.0.7',
+        'helper_version' => '1.0.8',
         'realtime_version' => '1.0.6',
         'self_hosted' => env('SELF_HOSTED', true),
         'autoupdate' => env('AUTOUPDATE'),
