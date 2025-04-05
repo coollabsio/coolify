68c2c7fb81b80a9c69bbac29039f4b1e796fadcf
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-04-05 14:41:34 +0200

    fix(Status): conditionally check proxy status and refresh button based on force_stop state

--- app/Livewire/Server/Proxy/Status.php
+++ app/Livewire/Server/Proxy/Status.php
@@ -34,9 +34,6 @@ class Status extends Component
     public function checkProxy(bool $notification = false)
     {
         try {
-            if ($this->server->proxy->force_stop) {
-                return;
-            }
             if ($this->polling) {
                 if ($this->numberOfPolls >= 10) {
                     $this->polling = false;
