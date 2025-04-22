ae4ce2ba65e457e7c5eebc2491cdb355a8709123
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-04-22 22:17:34 +0200

    fix(types): make types more strict in config files

--- config/session.php
+++ config/session.php
@@ -130,7 +130,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session',
+        Str::slug((string) env('APP_NAME', 'laravel'), '_').'_session',
     ),
 
     /*
