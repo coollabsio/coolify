54f38926040d48b135bbaa9eb890e70083d3c8ac
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-04-22 21:39:09 +0200

    style: use single quotes in the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
     ],
