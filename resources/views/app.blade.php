675bdf79f4f319b892aab97f25e99aedb98a7759
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-04-22 21:07:24 +0200

    chore(setup): add tailwind v4

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="noindex">
+    <title>Coolify</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold">Simple Heading</h1>
+</body>
+
+</html>
