2083ec1c0d5712b984b7531255689a9685d83a12
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-04-25 10:25:54 +0200

    chore(versions): update coolify version to 4.0.0-beta.413 and nightly version to 4.0.0-beta.414 in configuration files

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.0.0-beta.412',
+        'version' => '4.0.0-beta.413',
         'helper_version' => '1.0.8',
         'realtime_version' => '1.0.7',
         'self_hosted' => env('SELF_HOSTED', true),
