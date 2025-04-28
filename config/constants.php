0c0bdbf2fe21875e8804f2e23e2a3c2295187dba
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-04-28 10:45:02 +0200

    v4.0.0-beta.413 (#5711)
    * feat(README): add InterviewPal sponsorship link and corresponding SVG icon
    
    * chore(versions): update coolify version to 4.0.0-beta.413 and nightly version to 4.0.0-beta.414 in configuration files
    
    * fix(terminal): enhance WebSocket client verification with authorized IPs in terminal server
    
    * chore(versions): update realtime version to 1.0.8 in versions.json
    
    * chore(versions): update realtime version to 1.0.8 in versions.json

--- config/constants.php
+++ config/constants.php
@@ -2,9 +2,9 @@
 
 return [
     'coolify' => [
-        'version' => '4.0.0-beta.412',
+        'version' => '4.0.0-beta.413',
         'helper_version' => '1.0.8',
-        'realtime_version' => '1.0.7',
+        'realtime_version' => '1.0.8',
         'self_hosted' => env('SELF_HOSTED', true),
         'autoupdate' => env('AUTOUPDATE'),
         'base_config_path' => env('BASE_CONFIG_PATH', '/data/coolify'),
