a96154db41c66ca96e59d3ead2910a9256646f99
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-05-13 22:08:03 +0200

    refactor(s6): improve ssh-init script
    - the command ssh-keygen -A checks on its own if host keys already exist so we can remove the manual check

--- docker/testing-host/etc/s6-overlay/scripts/ssh-init.sh
+++ docker/testing-host/etc/s6-overlay/scripts/ssh-init.sh
@@ -3,9 +3,7 @@
 set -euo pipefail
 
 main() {
-    if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
-        ssh-keygen -A
-    fi
+    ssh-keygen -A
 
     echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuGmoeGq/pojrsyP1pszcNVuZx9iFkCELtxrh31QJ68 root@coolify-dev" >>/root/.ssh/authorized_keys
     chmod 600 /root/.ssh/authorized_keys
