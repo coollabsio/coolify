64305b79921d3f875cb3111e2c6a3bb68369e258
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-05-20 10:54:15 +0200

    refactor(sidebar): simplify server patching link by removing button element

--- resources/views/components/server/sidebar-security.blade.php
+++ resources/views/components/server/sidebar-security.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col items-start gap-2 min-w-fit">
     <a class="{{ request()->routeIs('server.security.patches') ? 'menu-item menu-item-active' : 'menu-item' }}"
         href="{{ route('server.security.patches', $parameters) }}">
-        <button>Server Patching</button>
+        Server Patching
     </a>
 </div>
