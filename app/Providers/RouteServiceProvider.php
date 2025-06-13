f3d54f2c869e3ebd4ad2f3c02a42641d533da8a0
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2025-06-13 11:12:19 -0400

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -49,7 +49,7 @@ class RouteServiceProvider extends ServiceProvider
                 return Limit::perMinute(1000)->by($request->user()?->id ?: $request->ip());
             }
 
-            return Limit::perMinute(200)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(config('api.throttle'))->by($request->user()?->id ?: $request->ip());
         });
         RateLimiter::for('5', function (Request $request) {
             return Limit::perMinute(5)->by($request->user()?->id ?: $request->ip());
