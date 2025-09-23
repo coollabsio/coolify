487b15a6e96ebfec4cd240af4895ce7de041cb43
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-09-23 16:49:59 +0200

    feat(private-key-refresh): add refresh dispatch on private key update and connection check
    - Implemented a dispatch for 'refreshServerShow' after successfully updating the private key and validating the server connection.
    - This enhancement improves the user experience by ensuring the server display is updated immediately following key changes and connection checks.

--- app/Livewire/Server/PrivateKey/Show.php
+++ app/Livewire/Server/PrivateKey/Show.php
@@ -47,6 +47,7 @@ class Show extends Component
                 }
             });
             $this->dispatch('success', 'Private key updated successfully.');
+            $this->dispatch('refreshServerShow');
         } catch (\Exception $e) {
             $this->server->refresh();
             $this->server->validateConnection();
@@ -60,6 +61,7 @@ class Show extends Component
             ['uptime' => $uptime, 'error' => $error] = $this->server->validateConnection();
             if ($uptime) {
                 $this->dispatch('success', 'Server is reachable.');
+                $this->dispatch('refreshServerShow');
             } else {
                 $this->dispatch('error', 'Server is not reachable.<br><br>Check this <a target="_blank" class="underline" href="https://coolify.io/docs/knowledge-base/server/openssh">documentation</a> for further help.<br><br>Error: '.$error);
 
