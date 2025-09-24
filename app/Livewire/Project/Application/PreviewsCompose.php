83feaa5f2065e4a9a41bec90bcaaef8e28fce32f
Michael Engel <michael_engel@posteo.de>
2025-09-24 10:52:01 +0200

    fix(PreviewCompose): adds port to preview urls

--- app/Livewire/Project/Application/PreviewsCompose.php
+++ app/Livewire/Project/Application/PreviewsCompose.php
@@ -72,10 +72,12 @@ class PreviewsCompose extends Component
                 $template = $this->preview->application->preview_url_template;
                 $host = $url->getHost();
                 $schema = $url->getScheme();
+                $port = ":" . $url->getPort();
                 $random = new Cuid2;
                 $preview_fqdn = str_replace('{{random}}', $random, $template);
                 $preview_fqdn = str_replace('{{domain}}', $host, $preview_fqdn);
                 $preview_fqdn = str_replace('{{pr_id}}', $this->preview->pull_request_id, $preview_fqdn);
+                $preview_fqdn = str_replace('{{port}}', $port, $preview_fqdn);
                 $preview_fqdn = "$schema://$preview_fqdn";
             }
 
