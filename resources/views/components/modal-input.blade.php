1123dcfe722a0a392969f2e952e58f55beb04de1
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-10-01 18:45:58 +0200

    ui(components): enhance close button accessibility in modal
    - Added focus-visible styles to the close button in the modal component for improved accessibility and user experience.
    - Ensured consistent visual feedback when the button is focused, aligning with recent accessibility enhancements.

--- resources/views/components/modal-input.blade.php
+++ resources/views/components/modal-input.blade.php
@@ -46,7 +46,7 @@
                 <div class="flex items-center justify-between pb-3">
                     <h3 class="text-2xl font-bold">{{ $title }}</h3>
                     <button @click="modalOpen=false"
-                        class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 rounded-full dark:text-white hover:bg-neutral-100 dark:hover:bg-coolgray-300 outline-0">
+                        class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 rounded-full dark:text-white hover:bg-neutral-100 dark:hover:bg-coolgray-300 outline-0 focus-visible:ring-2 focus-visible:ring-coollabs dark:focus-visible:ring-warning">
                         <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
