fbbc32ad74d85274208c532ddd8572a0bfce607d
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-10-09 16:38:40 +0200

    fix(css): remove unnecessary tracking classes from status components for consistency

--- resources/views/components/status/stopped.blade.php
+++ resources/views/components/status/stopped.blade.php
@@ -8,6 +8,6 @@
     @endif
     <span wire:loading.remove.delay.longer class="flex items-center">
         <div class="badge badge-error "></div>
-        <div class="pl-2 pr-1 text-xs font-bold tracking-wider text-error">{{ str($status)->before(':')->headline() }}</div>
+        <div class="pl-2 pr-1 text-xs font-bold text-error">{{ str($status)->before(':')->headline() }}</div>
     </span>
 </div>
