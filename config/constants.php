e28c926b9e5df1bc18dcd119360a7c38bf6e6214
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-11-07 08:26:58 +0100

    fix: update version numbers to 4.0.0-beta.443 and 4.0.0-beta.444

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.0.0-beta.442',
+        'version' => '4.0.0-beta.443',
         'helper_version' => '1.0.12',
         'realtime_version' => '1.0.10',
         'self_hosted' => env('SELF_HOSTED', true),
