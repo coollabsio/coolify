12eabaacad0c48a2c60b1e86abf462a1632c1d5e
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-11-25 09:34:43 +0100

    fix: update coolify version to 4.0.0-beta.447 and nightly version to 4.0.0-beta.448

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.0.0-beta.446',
+        'version' => '4.0.0-beta.447',
         'helper_version' => '1.0.12',
         'realtime_version' => '1.0.10',
         'self_hosted' => env('SELF_HOSTED', true),
