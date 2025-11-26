bfe6202edd22efbefdc87a006d3934b61823f03a
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-11-26 10:50:03 +0100

    fix: update version number to 4.0.0-beta.450

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.0.0-beta.449',
+        'version' => '4.0.0-beta.450',
         'helper_version' => '1.0.12',
         'realtime_version' => '1.0.10',
         'self_hosted' => env('SELF_HOSTED', true),
