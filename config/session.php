09ca5040ecea4c04a451fa47d42de5dcd0702b11
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-11-27 17:08:14 +0100

    chore(config): encrypt sessions

--- config/session.php
+++ config/session.php
@@ -48,7 +48,7 @@ return [
     |
     */
 
-    'encrypt' => env('SESSION_ENCRYPT', false),
+    'encrypt' => env('SESSION_ENCRYPT', true),
 
     /*
     |--------------------------------------------------------------------------
