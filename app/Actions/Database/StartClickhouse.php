d9d16c58ccb76f1b86321db054c083a80785a1e5
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-11-28 11:15:55 +0100

    Revert "fix(docker): migrate database start actions from --time to -t flag"

--- app/Actions/Database/StartClickhouse.php
+++ app/Actions/Database/StartClickhouse.php
@@ -105,7 +105,7 @@ class StartClickhouse
         $this->commands[] = "echo '{$readme}' > $this->configuration_dir/README.md";
         $this->commands[] = "echo 'Pulling {$database->image} image.'";
         $this->commands[] = "docker compose -f $this->configuration_dir/docker-compose.yml pull";
-        $this->commands[] = "docker stop -t 10 $container_name 2>/dev/null || true";
+        $this->commands[] = "docker stop --time=10 $container_name 2>/dev/null || true";
         $this->commands[] = "docker rm -f $container_name 2>/dev/null || true";
         $this->commands[] = "docker compose -f $this->configuration_dir/docker-compose.yml up -d";
         $this->commands[] = "echo 'Database started.'";
