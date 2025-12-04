e10096bffef2e242316cc202b2be2eb821cefea0
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-12-04 15:42:09 +0100

    chore(config): use redis for caching

--- config/cache.php
+++ config/cache.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_STORE', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
@@ -116,5 +116,5 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
+    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'coolify')).'-cache-'),
 ];
