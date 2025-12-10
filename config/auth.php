285c6dd10aa9de5efec7fab4d48e16a6e8047446
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-12-10 15:44:56 +0100

    chore(config): expire password reset tokens after 10min

--- config/auth.php
+++ config/auth.php
@@ -90,7 +90,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
+            'expire' => 10,
             'throttle' => 60,
         ],
     ],
