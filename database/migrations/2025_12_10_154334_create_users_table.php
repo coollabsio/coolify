b77d404673993e19a258265d7f8d30984d508f2f
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-12-10 16:59:33 +0100

    feat(auth): create users and password_reset_tokens table

---
+++ database/migrations/2025_12_10_154334_create_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+};
