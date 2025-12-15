26a4244095926f55b9ec612cbecb62fa985d893d
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-12-15 23:43:19 +0100

    chore(setup): configure svelte

---
+++ svelte.config.ts
@@ -0,0 +1,23 @@
+import { vitePreprocess, type SvelteConfig } from '@sveltejs/vite-plugin-svelte';
+
+const config: SvelteConfig = {
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		// TODO: Enforce runes mode on all files once Inertia v3 https://github.com/inertiajs/inertia/tree/3.x is released
+		// runes: true,
+		discloseVersion: false,
+		modernAst: true,
+		dev: true,
+		experimental: {
+			async: true,
+		}
+	},
+	vitePlugin: {
+		inspector: {
+			showToggleButton: 'always',
+			toggleButtonPos: 'top-right'
+		}
+	}
+};
+
+export default config;
