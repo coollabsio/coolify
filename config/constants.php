19eff02adf035993be28cd65fe9593b22cfeeb6b
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-12-17 11:05:01 +0100

    Bump version to 4.0.0-beta.456 and update nightly version to 4.0.0-beta.457

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.0.0-beta.455',
+        'version' => '4.0.0-beta.456',
         'helper_version' => '1.0.12',
         'realtime_version' => '1.0.10',
         'self_hosted' => env('SELF_HOSTED', true),
