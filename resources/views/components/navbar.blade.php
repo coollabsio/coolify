afd7f1531dbb156d0766e9df7882b8ee261905d4
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-01-12 23:07:00 +0100

    fix(ui): logout button indentation

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -398,7 +398,7 @@
                                 <path fill="currentColor"
                                     d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2a9.985 9.985 0 0 1 8 4h-2.71a8 8 0 1 0 .001 12h2.71A9.985 9.985 0 0 1 12 22m7-6v-3h-8v-2h8V8l5 4z" />
                             </svg>
-                            <span class="menu-item-label">Logout</span>
+                            <span>Logout</span>
                         </button>
                     </form>
                 </li>
