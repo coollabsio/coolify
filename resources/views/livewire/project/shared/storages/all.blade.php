ea950781cf2093a07871ddbf2ab9904ed99df30b
ShadowArcanist <162910371+ShadowArcanist@users.noreply.github.com>
2026-01-14 00:51:49 +0530

    fix(ui): correct volume mount instructions for compose based application

--- resources/views/livewire/project/shared/storages/all.blade.php
+++ resources/views/livewire/project/shared/storages/all.blade.php
@@ -2,8 +2,7 @@
     <div class="flex flex-col gap-4">
         @if ($resource->type() === 'service' || data_get($resource, 'build_pack') === 'dockercompose')
             <div class="w-full p-2 text-sm rounded bg-warning/10 text-warning">
-                Volume mounts are read-only. If you would like to add or modify a volume, you must edit your Docker
-                Compose file and reload the compose file.
+                For docker compose based applications Volume mounts are read-only in the Coolify dashboard. To add, modify, or manage volumes, you must edit your Docker Compose file and reload the compose file.
             </div>
         @endif
         @foreach ($resource->persistentStorages as $storage)
