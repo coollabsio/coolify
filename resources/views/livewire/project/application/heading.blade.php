c5dd45380f1bd674f50c47d14bbc2b39c06bbadd
Mailo <oliam2002@hotmail.com>
2026-01-16 13:32:18 +0100

    Reduce navbar gap size in project application and service headings. This fixes the horizontal overflow issue.

--- resources/views/livewire/project/application/heading.blade.php
+++ resources/views/livewire/project/application/heading.blade.php
@@ -1,7 +1,7 @@
 <nav wire:poll.10000ms="checkStatus" class="pb-6">
     <x-resources.breadcrumbs :resource="$application" :parameters="$parameters" :title="$lastDeploymentInfo" :lastDeploymentLink="$lastDeploymentLink" />
     <div class="navbar-main">
-        <nav class="flex shrink-0 gap-6 items-center whitespace-nowrap scrollbar min-h-10">
+        <nav class="flex shrink-0 gap-4 items-center whitespace-nowrap scrollbar min-h-10">
             <a class="{{ request()->routeIs('project.application.configuration') ? 'dark:text-white' : '' }}" {{ wireNavigate() }}
                 href="{{ route('project.application.configuration', $parameters) }}">
                 Configuration
