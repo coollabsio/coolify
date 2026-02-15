b5e0a28889da67cacefacc5a689a12623ffe0cb0
TweedlePure <kees@thepurefoodco.com>
2026-02-14 16:07:40 -0800

    feat: add OAuth registration setting to Advanced settings

--- app/Livewire/Settings/Advanced.php
+++ app/Livewire/Settings/Advanced.php
@@ -1,2 +1,98 @@
-public bool $is_registration_enabled;
+<?php
+
+
+
+namespace App\\Livewire\\Settings;
+
+
+
+use App\\Models\\InstanceSettings;
+
+use Livewire\\Component;
+
+
+
+class Advanced extends Component
+    
+{
+    
+    public InstanceSettings $settings;
+    
+    public bool $is_registration_enabled;
+    
+    public bool $is_oauth_registration_enabled;
+    
+
+    
+    public function updatedIsRegistrationEnabled()
+    
+    {
+        
+        $this->settings->is_registration_enabled = $this->is_registration_enabled;
+        
+        $this->settings->save();
+        
+    }
+    
+
+    
+    public function updatedIsOauthRegistrationEnabled()
+    
+    {
+        
+        $this->settings->is_oauth_registration_enabled = $this->is_oauth_registration_enabled;
+        
+        $this->settings->save();
+        
+    }
+    
+
+    
+    public function mount()
+    
+    {
+        
+        $this->is_registration_enabled = $this->settings->is_registration_enabled;
+        
+        $this->is_oauth_registration_enabled = $this->settings->is_oauth_registration_enabled;
+        
+    }
+    
+
+    
+    public function render()
+    
+    {
+        
+        return view('livewire.settings.advanced');
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
