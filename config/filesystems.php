48b63f950f04eaf9538c37a3f19c2541dc263af4
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-02-24 16:27:04 +0100

    chore(config): update configs to the latest skeleton

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => mb_rtrim((string) env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
