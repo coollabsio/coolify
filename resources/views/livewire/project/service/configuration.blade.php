71cccdd13853c0be89e6ba2ca02bb4e547793572
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-03-04 12:26:34 +0100

    Update configuration.blade.php

--- resources/views/livewire/project/service/configuration.blade.php
+++ resources/views/livewire/project/service/configuration.blade.php
@@ -158,6 +158,15 @@
                                         Settings
                                     </a>
                                     @if (str($database->status)->contains('running'))
+                                        @can('canAccessTerminal')
+                                            <a href="{{ route('project.service.files', ['project_uuid' => $project->uuid, 'environment_uuid' => $environment->uuid, 'service_uuid' => $service->uuid]) }}"
+                                                class="button bg-green-600">
+                                                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"></path>
+                                                </svg>
+                                                Database Panel
+                                            </a>
+                                        @endcan
                                         @can('update', $service)
                                             <x-modal-confirmation title="Confirm Service Database Restart?"
                                                 buttonTitle="Restart" submitAction="restartDatabase({{ $database->id }})"
