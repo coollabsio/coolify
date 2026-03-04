612219efa93b42b8265bdd8b15b34abfaedd2566
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-04 17:11:12 +0100

    chore(setup): add global inertia type declarations

---
+++ resources/js/global.d.ts
@@ -0,0 +1,10 @@
+declare module "@inertiajs/core" {
+    export interface InertiaConfig {
+        sharedPageProps: {
+        };
+        // flashDataType: {
+        //     toast?: { type: "success" | "error"; message: string };
+        // };
+        errorValueType: string[];
+    }
+}
