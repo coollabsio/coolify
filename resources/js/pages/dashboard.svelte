a93b2beccdc7034f0e1ea9126b2fd852c266df87
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-10 15:13:12 +0100

    chore(ui): use layout on dashboard page

--- resources/js/pages/dashboard.svelte
+++ resources/js/pages/dashboard.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import { page } from "@inertiajs/svelte";
-</script>
+	import { setLayoutProps } from "@inertiajs/svelte";
 
-<svelte:head>
-	<title>Dashboard - {$page.props.appName}</title>
-	<!-- the TS type of appName is currently incorrect, which is likely an Inertia bug. -->
-	<meta name="description" content="The {$page.props.appName} dashboard" />
-</svelte:head>
+	setLayoutProps({
+		title: "Dashboard",
+	});
+</script>
 
 <div class="bg-background text-foreground min-h-screen p-8">
 	<h1 class="mb-6 text-3xl font-bold">Dashboard</h1>
