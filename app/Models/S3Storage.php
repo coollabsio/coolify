52d9727d706ab86f77c885dfb1b208617510cee3
deepshekhardas <deepshekharadas1234@gmail.com>
2026-03-11 06:26:22 +0530

    fix: update email link in S3 connection error notification

--- app/Models/S3Storage.php
+++ app/Models/S3Storage.php
@@ -129,8 +129,7 @@ class S3Storage extends BaseModel
             if ($this->unusable_email_sent === false && is_transactional_emails_enabled()) {
                 $mail = new MailMessage;
                 $mail->subject('Coolify: S3 Storage Connection Error');
-                $mail->view('emails.s3-connection-error', ['name' => $this->name, 'reason' => $e->getMessage(), 'url' => route('storage.show', ['storage_uuid' => $this->uuid])]);
-
+                $mail->view('emails.s3-connection-error', ['name' => $this->name, 'reason' => $e->getMessage(), 'url' => base_url().'/storages/'.$this->uuid]);
                 // Load the team with its members and their roles explicitly
                 $team = $this->team()->with(['members' => function ($query) {
                     $query->withPivot('role');
