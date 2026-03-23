ab594045062877575410940e62a4bcec8abbdf32
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-03-23 09:54:11 +0100

    Borrar05

--- resources/views/livewire/project/shared/file-explorer.blade.php
+++ resources/views/livewire/project/shared/file-explorer.blade.php
@@ -408,9 +408,13 @@
                                                         </x-forms.button>
                                                         <x-modal-confirmation title="Delete File?" buttonTitle="Delete" submitAction="deleteFile('{{ $filePathEscaped }}')" :confirmWithText="false" :confirmWithPassword="false" step1ButtonText="Delete" step2ButtonText="Confirm">
                                             <x-slot:content>
-                                                <svg class="w-4 h-4 text-error" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-                                                </svg>
+                                                <div class="cursor-pointer" title="Delete">
+                                                    <x-forms.button class="!text-xs !px-2 !py-1" type="button">
+                                                        <svg class="w-4 h-4 text-error" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                                                        </svg>
+                                                    </x-forms.button>
+                                                </div>
                                             </x-slot:content>
                                         </x-modal-confirmation>
                                                     </div>
