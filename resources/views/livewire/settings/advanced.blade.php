496b60ff528aa44497d19e948eb8d9d78ef8c72d
GPradaT <gptclawd@gmail.com>
2026-03-23 11:53:18 +0100

    feat: add OAuth self-registration setting
    Allow admins to enable OAuth-based registration independently of
    general registration. New toggle in Admin > Advanced settings.
    
    Closes #8042

--- resources/views/livewire/settings/advanced.blade.php
+++ resources/views/livewire/settings/advanced.blade.php
@@ -22,6 +22,11 @@
                             label="Registration Allowed" />
                     </div>
                     <div class="md:w-96">
+                        <x-forms.checkbox instantSave id="is_oauth_registration_enabled"
+                            helper="Allow new users to register via OAuth providers even when general registration is disabled. Existing OAuth users can always log in."
+                            label="OAuth Registration Allowed" />
+                    </div>
+                    <div class="md:w-96">
                         <x-forms.checkbox instantSave id="do_not_track"
                             helper="Opt out of anonymous usage tracking. When enabled, this instance will not report to coolify.io's installation count and will not send error reports to help improve Coolify."
                             label="Do Not Track" />
