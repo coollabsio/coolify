89ec1b1c4f2227cc7c80c2fab36442b5f072c32b
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-03-24 16:08:34 +0100

    quitarbotonrecursos

--- resources/views/livewire/project/resource/index.blade.php
+++ resources/views/livewire/project/resource/index.blade.php
@@ -29,18 +29,6 @@
                 <livewire:project.delete-environment :disabled="!$environment->isEmpty()" :environment_id="$environment->id" />
             @endcan
         </div>
-        @can('createAnyResource')
-            <div class="pt-2">
-                <x-modal-confirmation title="Regenerate Environment Resources?" buttonTitle="Regenerate Resources"
-                    submitAction="regenerateEnvironmentResources" :confirmWithText="false" :confirmWithPassword="false"
-                    :actions="[
-                        'This will re-parse service stacks in the current environment.',
-                        'Soft-deleted services in this environment will be restored.',
-                        'Only this project and environment are affected.',
-                    ]"
-                    step2ButtonText="Regenerate" />
-            </div>
-        @endcan
         <nav class="flex pt-2 pb-6">
             <ol class="flex items-center">
                 <li class="inline-flex items-center" x-data="{ projectOpen: false, toggle() { this.projectOpen = !this.projectOpen }, open() { this.projectOpen = true }, close() { this.projectOpen = false } }">
