2811a8814268b64611cdff36985cfceeb0d7e401
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-26 18:00:34 +0100

    refactor(job): use camelCase for queue enum values

--- app/Enums/ProcessingQueue.php
+++ app/Enums/ProcessingQueue.php
@@ -8,11 +8,11 @@ enum ProcessingQueue: string
 {
     case Default = 'default';
     case High = 'high';
-    case StandardDeployment = 'standard-deployment';
-    case ProductionDeployment = 'production-deployment';
+    case StandardDeployment = 'standardDeployment';
+    case ProductionDeployment = 'productionDeployment';
 
-    case WorkerDefault = 'worker-default';
-    case WorkerHigh = 'worker-high';
-    case WorkerStandardDeployment = 'worker-standard-deployment';
-    case WorkerProductionDeployment = 'worker-production-deployment';
+    case WorkerDefault = 'workerDefault';
+    case WorkerHigh = 'workerHigh';
+    case WorkerStandardDeployment = 'workerStandardDeployment';
+    case WorkerProductionDeployment = 'workerProductionDeployment';
 }
