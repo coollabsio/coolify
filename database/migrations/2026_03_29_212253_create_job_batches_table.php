ea6d3d322953197cdb7e51f0720de95e11f635ef
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-30 15:10:02 +0200

    chore(job): remove comment on job batches migration

--- database/migrations/2026_03_29_212253_create_job_batches_table.php
+++ database/migrations/2026_03_29_212253_create_job_batches_table.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('job_batches', function (Blueprint $table): void {
