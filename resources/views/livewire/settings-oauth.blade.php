5c7aea5f2f56525a6cdfe09a6f8b28f8c7731b21
tristanwork <tristan@jovohintl.com>
2026-05-31 10:47:42 +0800

    feat: allow oauth provider registration

--- resources/views/livewire/settings-oauth.blade.php
+++ resources/views/livewire/settings-oauth.blade.php
@@ -21,6 +21,12 @@
                         <x-forms.checkbox instantSave="instantSave('{{ $oauth_setting['provider'] }}')"
                             id="oauth_settings_map.{{ $oauth_setting['provider'] }}.enabled" label="Enabled" />
                     </div>
+                    <div class="w-64">
+                        <x-forms.checkbox
+                            helper="Allow new users to create accounts with this provider even when password registration is disabled."
+                            id="oauth_settings_map.{{ $oauth_setting['provider'] }}.allow_registration"
+                            label="Allow Registration" />
+                    </div>
                     <div class="flex flex-col w-full gap-2 xl:flex-row">
                         <x-forms.input id="oauth_settings_map.{{ $oauth_setting['provider'] }}.client_id"
                             label="Client ID" />
