05438e18f269a4115cb8084e554387513d5c7643
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2026-08-03 23:18:26 +0200

    chore(release): bump Coolify to 4.3.0 and nightly to 4.3.1

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => '4.2.0',
+        'version' => '4.3.0',
         'helper_version' => '1.0.14',
         'realtime_version' => '1.0.16',
         'railpack_version' => '0.23.0',
