595b72df6ad938829fbbde660ee16127ce9b0788
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-08-18 18:42:31 +0200

    feat(ui): add some animations to the copy button

--- resources/views/components/copy-button.blade.php
+++ resources/views/components/copy-button.blade.php
@@ -9,8 +9,12 @@
 @endphp
 
 <button type="button" title="{{ $label }}" aria-label="{{ $label }}"
-    {{ $attributes->class(['icon-button shrink-0']) }} @disabled($resolve === null && blank($value))
+    {{ $attributes->class(['icon-button group shrink-0']) }} @disabled($resolve === null && blank($value))
     x-data="copyButton" @click="copy(await ({{ $valueExpression }}))">
-    <x-reicon name="copy" x-show="!copied" class="size-3.5" />
-    <x-reicon name="check" x-cloak x-show="copied" class="size-3.5 text-success" />
+    <span class="inline-flex transition-transform duration-150 ease-out group-active:scale-75">
+        <x-reicon name="copy" x-show="!copied" class="size-3.5" />
+        <x-reicon name="check" x-cloak x-show="copied" class="size-3.5 text-success"
+            x-transition:enter="transition-transform duration-200 ease-out"
+            x-transition:enter-start="scale-50" x-transition:enter-end="scale-100" />
+    </span>
 </button>
