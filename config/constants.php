29c7b043f39c65a23d146c9013aad630cb6c80d9
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2026-08-26 17:28:54 +0200

    chore(release): bump Coolify version to 4.3.12

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     'coolify' => [
-        'version' => env('COOLIFY_VERSION') ?: '4.3.11',
+        'version' => env('COOLIFY_VERSION') ?: '4.3.12',
         'helper_version' => '1.0.16',
         'realtime_version' => '1.0.17',
         'railpack_version' => '0.23.0',
