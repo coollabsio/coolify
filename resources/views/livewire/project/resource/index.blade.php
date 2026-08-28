bf003c6d6ed3ac9a448b15ec9de27d1432b2860f
Aditya Tripathi <aditya@climactic.co>
2026-08-28 17:14:35 +0000

    feat(resource-list): show template update dot on services

--- resources/views/livewire/project/resource/index.blade.php
+++ resources/views/livewire/project/resource/index.blade.php
@@ -202,6 +202,9 @@
                                         {{ wireNavigate() }}
                                         class="relative block truncate text-[13px] font-semibold text-black hover:underline dark:text-fg"
                                         x-text="item.name"></a>
+                                    <template x-if="item.type === 'service' && item.updateAvailable">
+                                        <span class="size-2 shrink-0 rounded-full bg-warning" title="Template update available"></span>
+                                    </template>
                                 </div>
                                 <p class="min-h-4 truncate text-[11px] text-neutral-500 dark:text-fg-faint">
                                     <span x-show="item.description" x-text="item.description"></span>
@@ -295,6 +298,9 @@
                                         <h2
                                             class="truncate text-[13px]! leading-4! font-semibold! text-black dark:text-fg"
                                             x-text="item.name"></h2>
+                                        <template x-if="item.type === 'service' && item.updateAvailable">
+                                            <span class="size-2 shrink-0 rounded-full bg-warning" title="Template update available"></span>
+                                        </template>
                                     </div>
                                     <p class="mt-0.5 text-[11px] text-neutral-500 dark:text-fg-faint"
                                         x-text="item.typeLabel"></p>
